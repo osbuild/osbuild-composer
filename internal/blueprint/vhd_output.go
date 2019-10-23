75cc7f8b96c173e266fb5d88d507e26b9ac797d0
Martin Sehnoutka <msehnout@redhat.com>
2019-10-23 11:15:47 +0200

    Adjust VHD output to the Lorax one

--- internal/blueprint/vhd_output.go
+++ internal/blueprint/vhd_output.go
@@ -5,10 +5,24 @@ import "github.com/osbuild/osbuild-composer/internal/pipeline"
 type vhdOutput struct{}
 
 func (t *vhdOutput) translate(b *Blueprint) *pipeline.Pipeline {
-	p := getF30Pipeline()
+	packages := [...]string{
+		"@Core",
+		"chrony",
+		"kernel",
+		"selinux-policy-targeted",
+		"grub2-pc",
+		"langpacks-en",
+		"net-tools",
+		"ntfsprogs",
+		"WALinuxAgent",
+		"libxcrypt-compat",
+	}
+	p := getCustomF30PackageSet(packages[:], []string{})
+	addF30LocaleStage(p)
 	addF30FSTabStage(p)
 	addF30GRUB2Stage(p)
 	addF30SELinuxStage(p)
+	addF30FixBlsStage(p)
 	addF30QemuAssembler(p, "qcow2", t.getName())
 	return p
 }
