8356702263eeb22d717cc57443902cbb1dbcd277
Lars Karlitski <lars@karlitski.net>
2019-10-29 03:12:37 +0100

    blueprint: move partitioned-disk towards lorax parity
    The main difference (according to image-info) is an additional package
    containing a gpg key which was used to verify packages. The one
    generated by lorax-composer doesn't have this, because it doesn't verify
    signatures.
    
    Also, lorax generates an image with a separate /boot partition. This is
    not yet addressed here, because osbuild doesn't support it yet.

--- internal/blueprint/disk_output.go
+++ internal/blueprint/disk_output.go
@@ -5,16 +5,30 @@ import "github.com/osbuild/osbuild-composer/internal/pipeline"
 type diskOutput struct{}
 
 func (t *diskOutput) translate(b *Blueprint) *pipeline.Pipeline {
-	p := getF30Pipeline()
+	packages := [...]string{
+		"@core",
+		"chrony",
+		"firewalld",
+		"grub2-pc",
+		"kernel",
+		"langpacks-en",
+		"selinux-policy-targeted",
+	}
+	excludedPackages := [...]string{
+		"dracut-config-rescue",
+	}
+	p := getCustomF30PackageSet(packages[:], excludedPackages[:])
+	addF30LocaleStage(p)
 	addF30FSTabStage(p)
 	addF30GRUB2Stage(p, b.getKernelCustomization())
+	addF30FixBlsStage(p)
 	addF30SELinuxStage(p)
 	addF30QemuAssembler(p, "raw", t.getName())
 	return p
 }
 
 func (t *diskOutput) getName() string {
-	return "image.img"
+	return "disk.img"
 }
 
 func (t *diskOutput) getMime() string {
