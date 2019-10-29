3f4e503112d39aeb569d61abe940b1d3f3287db0
Lars Karlitski <lars@karlitski.net>
2019-10-29 02:12:43 +0100

    blueprint: move ext4-filesystem towards lorax parity
    The main difference (according to image-info) is an additional package
    containing a gpg key which was used to verify packages. The one
    generated by lorax-composer doesn't have this, because it doesn't verify
    signatures.

--- internal/blueprint/ext4_output.go
+++ internal/blueprint/ext4_output.go
@@ -5,14 +5,28 @@ import "github.com/osbuild/osbuild-composer/internal/pipeline"
 type ext4Output struct{}
 
 func (t *ext4Output) translate(b *Blueprint) *pipeline.Pipeline {
-	p := getF30Pipeline()
+	packages := [...]string{
+		"policycoreutils",
+		"selinux-policy-targeted",
+		"kernel",
+		"firewalld",
+		"chrony",
+		"langpacks-en",
+	}
+	excludedPackages := [...]string{
+		"dracut-config-rescue",
+	}
+	p := getCustomF30PackageSet(packages[:], excludedPackages[:])
+	addF30LocaleStage(p)
+	addF30GRUB2Stage(p, b.getKernelCustomization())
+	addF30FixBlsStage(p)
 	addF30SELinuxStage(p)
 	addF30RawFSAssembler(p, t.getName())
 	return p
 }
 
 func (t *ext4Output) getName() string {
-	return "image.img"
+	return "filesystem.img"
 }
 
 func (t *ext4Output) getMime() string {
