7fe3929aee7baab4b3dfd1151dfaeb15011e6cea
Martin Sehnoutka <msehnout@redhat.com>
2019-11-04 14:12:31 +0100

    enable passing of a blueprint to osbuild-pipeline
    It is enabled via a command-line argument `-blueprint`.

--- cmd/osbuild-pipeline/main.go
+++ cmd/osbuild-pipeline/main.go
@@ -3,16 +3,30 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"github.com/osbuild/osbuild-composer/internal/blueprint"
+	"io/ioutil"
 	"os"
+
+	"github.com/osbuild/osbuild-composer/internal/blueprint"
 )
 
 func main() {
 	var format string
+	var blueprintArg string
 	flag.StringVar(&format, "output-format", "qcow2", "output format")
+	flag.StringVar(&blueprintArg, "blueprint", "", "blueprint to translate")
 	flag.Parse()
 
 	blueprint := &blueprint.Blueprint{}
+	if blueprintArg != "" {
+		file, err := ioutil.ReadFile(blueprintArg)
+		if err != nil {
+			panic("Colud not find blueprint")
+		}
+		err = json.Unmarshal([]byte(file), &blueprint)
+		if err != nil {
+			panic("Colud not parse blueprint")
+		}
+	}
 	pipeline, err := blueprint.ToPipeline(format)
 	if err != nil {
 		panic(err.Error())
