df7e14d5eb40ac96c6b898ebd069d54d117a92a0
Ondřej Budai <obudai@redhat.com>
2019-11-26 20:07:44 +0100

    target: add new fields to target and cleanup
    We need some more fields in target struct to support weldr API. This commit
    introduces them. Also a bit of cleanup is done.

--- internal/target/target.go
+++ internal/target/target.go
@@ -3,12 +3,27 @@ package target
 import (
 	"encoding/json"
 	"errors"
+	"github.com/google/uuid"
+	"time"
 )
 
 type Target struct {
+	Uuid      uuid.UUID     `json:"uuid"`
 	ImageName string        `json:"image_name"`
 	Name      string        `json:"name"`
+	Created   time.Time     `json:"created"`
+	Status    string        `json:"status"`
 	Options   TargetOptions `json:"options"`
+}
+
+func newTarget(name string, options TargetOptions) *Target {
+	return &Target{
+		Uuid:    uuid.New(),
+		Name:    name,
+		Created: time.Now(),
+		Status:  "WAITING",
+		Options: options,
+	}
 }
 
 type TargetOptions interface {
@@ -16,8 +31,12 @@ type TargetOptions interface {
 }
 
 type rawTarget struct {
-	Name    string          `json:"name"`
-	Options json.RawMessage `json:"options"`
+	Uuid      uuid.UUID       `json:"uuid"`
+	ImageName string          `json:"image_name"`
+	Name      string          `json:"name"`
+	Created   time.Time       `json:"created"`
+	Status    string          `json:"status"`
+	Options   json.RawMessage `json:"options"`
 }
 
 func (target *Target) UnmarshalJSON(data []byte) error {
@@ -26,8 +45,24 @@ func (target *Target) UnmarshalJSON(data []byte) error {
 	if err != nil {
 		return err
 	}
+	options, err := UnmarshalTargetOptions(rawTarget.Name, rawTarget.Options)
+	if err != nil {
+		return err
+	}
+
+	target.Uuid = rawTarget.Uuid
+	target.ImageName = rawTarget.ImageName
+	target.Name = rawTarget.Name
+	target.Created = rawTarget.Created
+	target.Status = rawTarget.Status
+	target.Options = options
+
+	return nil
+}
+
+func UnmarshalTargetOptions(targetName string, rawOptions json.RawMessage) (TargetOptions, error) {
 	var options TargetOptions
-	switch rawTarget.Name {
+	switch targetName {
 	case "org.osbuild.azure":
 		options = new(AzureTargetOptions)
 	case "org.osbuild.aws":
@@ -35,15 +70,9 @@ func (target *Target) UnmarshalJSON(data []byte) error {
 	case "org.osbuild.local":
 		options = new(LocalTargetOptions)
 	default:
-		return errors.New("unexpected target name")
+		return nil, errors.New("unexpected target name")
 	}
-	err = json.Unmarshal(rawTarget.Options, options)
-	if err != nil {
-		return err
-	}
+	err := json.Unmarshal(rawOptions, options)
 
-	target.Name = rawTarget.Name
-	target.Options = options
-
-	return nil
+	return options, err
 }
