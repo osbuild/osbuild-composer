66c57a05b7e250e2203f63085e6bf43b7135be58
Ondřej Budai <obudai@redhat.com>
2019-11-27 20:26:28 +0100

    api tests: add store fixtures
    Prior this commit there wasn't an easy to populate the store. The only way
    was to call the weldr API or store methods. This design made testing of
    various edges quite hard.
    
    This commit adds store fixtures - an easy way how to define store state
    before each test case.
    
    In addition, the fixtures were refactored so that new instances are created
    prior each test. Before this change the tests were in some cases dependant
    on each other.

--- internal/mocks/rpmmd/fixtures.go
+++ internal/mocks/rpmmd/fixtures.go
@@ -2,9 +2,15 @@ package rpmmd_mock
 
 import (
 	"fmt"
+	"github.com/google/uuid"
+	"github.com/osbuild/osbuild-composer/internal/blueprint"
 	"github.com/osbuild/osbuild-composer/internal/rpmmd"
+	"github.com/osbuild/osbuild-composer/internal/store"
+	"sort"
 	"time"
 )
+
+type FixtureGenerator func() Fixture
 
 func generatePackageList() rpmmd.PackageList {
 	baseTime, err := time.Parse(time.RFC3339, "2006-01-02T15:04:05Z")
@@ -37,72 +43,140 @@ func generatePackageList() rpmmd.PackageList {
 		packageList = append(packageList, basePackage, secondBuild)
 	}
 
+	sort.Slice(packageList, func(i, j int) bool {
+		return packageList[i].Name < packageList[j].Name
+	})
+
 	return packageList
 }
 
-var basePackageList = fetchPackageList{
-	generatePackageList(),
-	nil,
+func createBaseStoreFixture() *store.Store {
+	var bName = "test"
+	var b = blueprint.Blueprint{Name: bName, Version: "0.0.0"}
+
+	var date = time.Date(2019, 11, 27, 13, 19, 0, 0, time.FixedZone("UTC+1", 60*60))
+
+	s := store.New(nil)
+
+	s.Blueprints[bName] = b
+	s.Composes = map[uuid.UUID]store.Compose{
+		uuid.MustParse("e65f76f8-b0d9-4974-9dd7-745ae80b4721"): store.Compose{
+			QueueStatus: "WAITING",
+			Blueprint:   &b,
+			OutputType:  "tar",
+			Targets:     nil,
+			JobCreated:  date,
+		},
+		uuid.MustParse("e65f76f8-b0d9-4974-9dd7-745ae80b4722"): store.Compose{
+			QueueStatus: "RUNNING",
+			Blueprint:   &b,
+			OutputType:  "tar",
+			Targets:     nil,
+			JobCreated:  date,
+			JobStarted:  date,
+		},
+		uuid.MustParse("e65f76f8-b0d9-4974-9dd7-745ae80b4723"): store.Compose{
+			QueueStatus: "FINISHED",
+			Blueprint:   &b,
+			OutputType:  "tar",
+			Targets:     nil,
+			JobCreated:  date,
+			JobStarted:  date,
+			JobFinished: date,
+		},
+		uuid.MustParse("e65f76f8-b0d9-4974-9dd7-745ae80b4724"): store.Compose{
+			QueueStatus: "FAILED",
+			Blueprint:   &b,
+			OutputType:  "tar",
+			Targets:     nil,
+			JobCreated:  date,
+			JobStarted:  date,
+			JobFinished: date,
+		},
+	}
+
+	return s
 }
 
-var BaseFixture = Fixture{
-	basePackageList,
-	depsolve{
-		[]rpmmd.PackageSpec{
-			{
-				Name:    "dep-package1",
-				Epoch:   0,
-				Version: "1.33",
-				Release: "2.fc30",
-				Arch:    "x86_64",
+func BaseFixture() Fixture {
+	return Fixture{
+		fetchPackageList{
+			generatePackageList(),
+			nil,
+		},
+		depsolve{
+			[]rpmmd.PackageSpec{
+				{
+					Name:    "dep-package1",
+					Epoch:   0,
+					Version: "1.33",
+					Release: "2.fc30",
+					Arch:    "x86_64",
+				},
+				{
+					Name:    "dep-package2",
+					Epoch:   0,
+					Version: "2.9",
+					Release: "1.fc30",
+					Arch:    "x86_64",
+				},
 			},
-			{
-				Name:    "dep-package2",
-				Epoch:   0,
-				Version: "2.9",
-				Release: "1.fc30",
-				Arch:    "x86_64",
+			nil,
+		},
+		createBaseStoreFixture(),
+	}
+}
+
+func NonExistingPackage() Fixture {
+	return Fixture{
+		fetchPackageList{
+			generatePackageList(),
+			nil,
+		},
+		depsolve{
+			nil,
+			&rpmmd.DNFError{
+				Kind:   "MarkingErrors",
+				Reason: "Error occurred when marking packages for installation: Problems in request:\nmissing packages: fash",
 			},
 		},
-		nil,
-	},
+		createBaseStoreFixture(),
+	}
 }
 
-var NonExistingPackage = Fixture{
-	basePackageList,
-	depsolve{
-		nil,
-		&rpmmd.DNFError{
-			Kind:   "MarkingErrors",
-			Reason: "Error occurred when marking packages for installation: Problems in request:\nmissing packages: fash",
+func BadDepsolve() Fixture {
+	return Fixture{
+		fetchPackageList{
+			generatePackageList(),
+			nil,
 		},
-	},
+		depsolve{
+			nil,
+			&rpmmd.DNFError{
+				Kind:   "DepsolveError",
+				Reason: "There was a problem depsolving ['go2rpm']: \n Problem: conflicting requests\n  - nothing provides askalono-cli needed by go2rpm-1-4.fc31.noarch",
+			},
+		},
+		createBaseStoreFixture(),
+	}
 }
 
-var BadDepsolve = Fixture{
-	basePackageList,
-	depsolve{
-		nil,
-		&rpmmd.DNFError{
-			Kind:   "DepsolveError",
-			Reason: "There was a problem depsolving ['go2rpm']: \n Problem: conflicting requests\n  - nothing provides askalono-cli needed by go2rpm-1-4.fc31.noarch",
+func BadFetch() Fixture {
+	return Fixture{
+		fetchPackageList{
+			ret: nil,
+			err: &rpmmd.DNFError{
+				Kind:   "FetchError",
+				Reason: "There was a problem when fetching packages.",
+			},
 		},
-	},
-}
-
-var BadFetch = Fixture{
-	fetchPackageList{
-		ret: nil,
-		err: &rpmmd.DNFError{
-			Kind:   "FetchError",
-			Reason: "There was a problem when fetching packages.",
+		depsolve{
+			nil,
+			&rpmmd.DNFError{
+				Kind:   "DepsolveError",
+				Reason: "There was a problem depsolving ['go2rpm']: \n Problem: conflicting requests\n  - nothing provides askalono-cli needed by go2rpm-1-4.fc31.noarch",
+			},
 		},
-	},
-	depsolve{
-		nil,
-		&rpmmd.DNFError{
-			Kind:   "DepsolveError",
-			Reason: "There was a problem depsolving ['go2rpm']: \n Problem: conflicting requests\n  - nothing provides askalono-cli needed by go2rpm-1-4.fc31.noarch",
-		},
-	},
+		createBaseStoreFixture(),
+	}
 }
