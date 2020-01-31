642b90c97757ae67d8e7697344060aff32af00c6
Brian C. Lane <bcl@redhat.com>
2020-01-31 14:21:20 -0800

    Switch blueprint commit to use a random SHA-1
    Generating a SHA-1 based on time is not safe. A collision can easily be
    generated, and if parallel operations are used they will eventually
    collide. This reads random bytes and uses them for the SHA-1 hash. It
    will return an error if the rand.Read() fails.

--- internal/store/store_test.go
+++ internal/store/store_test.go
@@ -28,3 +28,13 @@ func TestBumpVersion(t *testing.T) {
 		}
 	}
 }
+
+func TestRandomSHA1String(t *testing.T) {
+	hash, err := randomSHA1String()
+	if err != nil {
+		t.Fatalf("RandomSHA1String failed: %s", err)
+	}
+	if len(hash) != 40 {
+		t.Fatalf("RandomSHA1String failed: hash is not 40 characters")
+	}
+}
