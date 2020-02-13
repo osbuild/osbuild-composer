6b957f01a0de1598d5e2fcc548f2cb36a9714073
Martin Sehnoutka <msehnout@redhat.com>
2020-02-13 15:13:07 +0100

    common: fix method signature for UnmarshalJSON
    There was a bug in the previous implementation which used to pass the
    argument as a value but that does not work because we need to change the
    value of it. The new implementation uses pass by reference.
    
    Create a test to cover this scenario.

--- internal/common/types_test.go
+++ internal/common/types_test.go
@@ -129,5 +129,21 @@ func TestJSONConversionsComposeRequest(t *testing.T) {
 			t.Error("Marshaled compose request is not the one expected")
 		}
 	}
+}
 
+func TestImageType_UnmarshalJSON(t *testing.T) {
+	dict := struct {
+		ImageTypes []ImageType `json:"image_types"`
+	}{}
+	input := `{"image_types":["qcow2", "Alibaba"]}`
+	err := json.Unmarshal([]byte(input), &dict)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if dict.ImageTypes[0] != Qcow2Generic {
+		t.Fatal("failed to umarshal image type qcow2; got tag:", dict.ImageTypes[0])
+	}
+	if dict.ImageTypes[1] != Alibaba {
+		t.Fatal("failed to umarshal image type Alibaba; got tag:", dict.ImageTypes[0])
+	}
 }
