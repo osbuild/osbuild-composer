osbuild/osbuild-composer

commit d7cbc22da4ece981c85af4d5177920a5bdeb175f

lint: fix unhandled errors

cmd/osbuild-composer/main.go
@@ -91,7 +91,10 @@ func main() {
 	jobAPI := jobqueue.New(logger, store)
 	weldrAPI := weldr.New(rpm, common.CurrentArch(), distribution, logger, store)
 
-	go jobAPI.Serve(jobListener)
+	go func() {
+		err := jobAPI.Serve(jobListener)
+		common.PanicOnError(err)
+	}()
 
 	// Optionally run RCM API as well as Weldr API
 	if rcmApiListeners, exists := listeners["osbuild-rcm.socket"]; exists {
@@ -124,9 +127,16 @@ func main() {
 			}
 
 			listener := tls.NewListener(listener, tlsConfig)
-			go jobAPI.Serve(listener)
+			go func() {
+				err := jobAPI.Serve(listener)
+				common.PanicOnError(err)
+			}()
 		}
 	}
 
-	weldrAPI.Serve(weldrListener)
+	go func() {
+		err := weldrAPI.Serve(weldrListener)
+		common.PanicOnError(err)
+	}()
+
 }

cmd/osbuild-tests/main.go
@@ -205,7 +205,10 @@ func runComposerCLI(quiet bool, command ...string) json.RawMessage {
 		log.Printf("$ composer-cli %s\n", strings.Join(command, " "))
 	}
 
-	cmd.Start()
+	err = cmd.Start()
+	if err != nil {
+		log.Fatalf("Could not start command: %v", err)
+	}
 
 	var result json.RawMessage
 

cmd/osbuild-worker/main.go
@@ -100,7 +100,10 @@ func (c *ComposerClient) AddJob() (*jobqueue.Job, error) {
 	}
 
 	var b bytes.Buffer
-	json.NewEncoder(&b).Encode(request{})
+	err := json.NewEncoder(&b).Encode(request{})
+	if err != nil {
+		return nil, err
+	}
 	response, err := c.client.Post(c.createURL("/job-queue/v1/jobs"), "application/json", &b)
 	if err != nil {
 		return nil, err
@@ -124,7 +127,10 @@ func (c *ComposerClient) AddJob() (*jobqueue.Job, error) {
 
 func (c *ComposerClient) UpdateJob(job *jobqueue.Job, status common.ImageBuildState, result *common.ComposeResult) error {
 	var b bytes.Buffer
-	json.NewEncoder(&b).Encode(&jobqueue.JobStatus{status, result})
+	err := json.NewEncoder(&b).Encode(&jobqueue.JobStatus{status, result})
+	if err != nil {
+		return err
+	}
 	urlPath := fmt.Sprintf("/job-queue/v1/jobs/%s/builds/%d", job.ID.String(), job.ImageBuildID)
 	url := c.createURL(urlPath)
 	req, err := http.NewRequest("PATCH", url, &b)

internal/blueprint/blueprint_test.go
@@ -78,7 +78,10 @@ func TestBumpVersion(t *testing.T) {
 
 	for _, c := range cases {
 		bp := c.NewBlueprint
-		bp.Initialize()
+		err := bp.Initialize()
+		if err != nil {
+			panic(err)
+		}
 
 		bp.BumpVersion(c.OldVersion)
 		if bp.Version != c.ExpectedVersion {

internal/common/compose_result.go
@@ -34,7 +34,7 @@ type ComposeResult struct {
 	Success bool `json:"success"`
 }
 
-func (cr *ComposeResult) Write(writer io.Writer) {
+func (cr *ComposeResult) Write(writer io.Writer) error {
 	if cr.Build == nil && len(cr.Stages) == 0 && cr.Assembler == nil {
 		fmt.Fprintf(writer, "The compose result is empty.\n")
 	}
@@ -46,7 +46,10 @@ func (cr *ComposeResult) Write(writer io.Writer) {
 			fmt.Fprintf(writer, "Stage %s\n", stage.Name)
 			enc := json.NewEncoder(writer)
 			enc.SetIndent("", "  ")
-			enc.Encode(stage.Options)
+			err := enc.Encode(stage.Options)
+			if err != nil {
+				return err
+			}
 			fmt.Fprintf(writer, "\nOutput:\n%s\n", stage.Output)
 		}
 	}
@@ -57,7 +60,10 @@ func (cr *ComposeResult) Write(writer io.Writer) {
 			fmt.Fprintf(writer, "Stage: %s\n", stage.Name)
 			enc := json.NewEncoder(writer)
 			enc.SetIndent("", "  ")
-			enc.Encode(stage.Options)
+			err := enc.Encode(stage.Options)
+			if err != nil {
+				return err
+			}
 			fmt.Fprintf(writer, "\nOutput:\n%s\n", stage.Output)
 		}
 	}
@@ -66,7 +72,12 @@ func (cr *ComposeResult) Write(writer io.Writer) {
 		fmt.Fprintf(writer, "Assembler %s:\n", cr.Assembler.Name)
 		enc := json.NewEncoder(writer)
 		enc.SetIndent("", "  ")
-		enc.Encode(cr.Assembler.Options)
+		err := enc.Encode(cr.Assembler.Options)
+		if err != nil {
+			return err
+		}
 		fmt.Fprintf(writer, "\nOutput:\n%s\n", cr.Assembler.Output)
 	}
+
+	return nil
 }

internal/common/helpers.go
@@ -15,3 +15,9 @@ func CurrentArch() string {
 		panic("unsupported architecture")
 	}
 }
+
+func PanicOnError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}

internal/jobqueue/api.go
@@ -74,7 +74,10 @@ func statusResponseError(writer http.ResponseWriter, code int, errors ...string)
 	writer.WriteHeader(code)
 
 	for _, err := range errors {
-		writer.Write([]byte(err))
+		_, e := writer.Write([]byte(err))
+		if e != nil {
+			panic(e)
+		}
 	}
 }
 

internal/store/store.go
@@ -243,7 +243,10 @@ func randomSHA1String() (string, error) {
 	} else if n != 20 {
 		return "", errors.New("randomSHA1String: short read from rand")
 	}
-	hash.Write(data)
+	_, err = hash.Write(data)
+	if err != nil {
+		return "", err
+	}
 	return hex.EncodeToString(hash.Sum(nil)), nil
 }
 

commit 2583b3ae6ae5493593ebd2edd4a30ecbbf647e93

fix composer immediately exiting
I broke it in d7cbc22d, there shouldn't be a goroutine.

cmd/osbuild-composer/main.go
@@ -134,9 +134,7 @@ func main() {
 		}
 	}
 
-	go func() {
-		err := weldrAPI.Serve(weldrListener)
-		common.PanicOnError(err)
-	}()
+	err = weldrAPI.Serve(weldrListener)
+	common.PanicOnError(err)
 
 }