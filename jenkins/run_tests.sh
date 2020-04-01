0e5e41117ac5e6b3f23f7ce68d54b331cd5edee7
Major Hayden <major@redhat.com>
2020-04-01 16:32:42 -0500

    🔌 Initial Jenkins test
    Signed-off-by: Major Hayden <major@redhat.com>

---
+++ jenkins/run_tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -euxo pipefail
+
+# Ensure Ansible is installed.
+sudo dnf -y install ansible
+
+# Clone the latest version of ansible-osbuild.
+git clone https://github.com/osbuild/ansible-osbuild.git ansible-osbuild
+
+# Get the current SHA of osbuild-composer.
+OSBUILD_COMPOSER_VERSION=$(git rev-parse HEAD)
+
+# Run the deployment.
+pushd ansible-osbuild
+  echo -e "[test_instances]\nlocalhost ansible_connection=local" > hosts.ini
+  ansible-playbook \
+    -i hosts.ini \
+    -e osbuild_composer_repo=${WORKSPACE} \
+    -e osbuild_composer_version=${OSBUILD_COMPOSER_VERSION} \
+    playbook.yml
+popd
+
+# Run the integration tests.
+# /usr/libexec/tests/osbuild-composer/osbuild-dnf-json-tests
+# /usr/libexec/tests/osbuild-composer/osbuild-image-tests
+/usr/libexec/tests/osbuild-composer/osbuild-rcm-tests -test.v
+# /usr/libexec/tests/osbuild-composer/osbuild-tests -test.v
+# /usr/libexec/tests/osbuild-composer/osbuild-weldr-tests -test.v
