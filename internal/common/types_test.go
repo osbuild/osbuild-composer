8797cd47047c9fd04174360b726fc5136017621b
Tom Gundersen <teg@jklm.no>
2020-04-03 02:25:47 +0200

    common: drop unused image types
    The Alibaba and GCE image types were not used, and they were
    causing problems when translated from and to strings as they were
    mapping to the same internal image type. The end result was that
    AWS jobs were shown as Alibaba or GCE in the UI.
    
    This fixes #369.
    
    Signed-off-by: Tom Gundersen <teg@jklm.no>

--- internal/common/types_test.go
+++ internal/common/types_test.go
@@ -9,7 +9,7 @@ func TestImageType_UnmarshalJSON(t *testing.T) {
 	dict := struct {
 		ImageTypes []ImageType `json:"image_types"`
 	}{}
-	input := `{"image_types":["qcow2", "Alibaba"]}`
+	input := `{"image_types":["qcow2", "Azure"]}`
 	err := json.Unmarshal([]byte(input), &dict)
 	if err != nil {
 		t.Fatal(err)
@@ -17,7 +17,7 @@ func TestImageType_UnmarshalJSON(t *testing.T) {
 	if dict.ImageTypes[0] != Qcow2Generic {
 		t.Fatal("failed to umarshal image type qcow2; got tag:", dict.ImageTypes[0])
 	}
-	if dict.ImageTypes[1] != Alibaba {
-		t.Fatal("failed to umarshal image type Alibaba; got tag:", dict.ImageTypes[0])
+	if dict.ImageTypes[1] != Azure {
+		t.Fatal("failed to umarshal image type Azure; got tag:", dict.ImageTypes[0])
 	}
 }
