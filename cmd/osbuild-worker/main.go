osbuild/osbuild-composer

commit 2435163fc94de40d5cf5b6c7bf1e73e15089eaa0

worker: move running osbuild into separate function
Setting up a command to run is quite involved. Separate that from the
logic of running it.

cmd/osbuild-worker/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"crypto/tls"
 	"crypto/x509"
-	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
@@ -11,7 +10,6 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
 	"path"
 
 	"github.com/osbuild/osbuild-composer/internal/common"
@@ -71,44 +69,9 @@ func RunJob(job *jobqueue.Job, uploadFunc func(*jobqueue.Job, io.Reader) error) 
 	// FIXME: how to handle errors in defer?
 	defer os.RemoveAll(tmpStore)
 
-	cmd := exec.Command(
-		"osbuild",
-		"--store", tmpStore,
-		"--json", "-",
-	)
-	cmd.Stderr = os.Stderr
-
-	stdin, err := cmd.StdinPipe()
+	result, err := RunOSBuild(job.Manifest, tmpStore, os.Stderr)
 	if err != nil {
-		return nil, fmt.Errorf("error setting up stdin for osbuild: %v", err)
-	}
-
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return nil, fmt.Errorf("error setting up stdout for osbuild: %v", err)
-	}
-
-	err = cmd.Start()
-	if err != nil {
-		return nil, fmt.Errorf("error starting osbuild: %v", err)
-	}
-
-	err = json.NewEncoder(stdin).Encode(job.Manifest)
-	if err != nil {
-		return nil, fmt.Errorf("error encoding osbuild pipeline: %v", err)
-	}
-	// FIXME: handle or comment this possible error
-	_ = stdin.Close()
-
-	var result common.ComposeResult
-	err = json.NewDecoder(stdout).Decode(&result)
-	if err != nil {
-		return nil, fmt.Errorf("error decoding osbuild output: %#v", err)
-	}
-
-	err = cmd.Wait()
-	if err != nil {
-		return &result, err
+		return nil, fmt.Errorf("osbuild error: %v", err)
 	}
 
 	var r []error
@@ -159,10 +122,10 @@ func RunJob(job *jobqueue.Job, uploadFunc func(*jobqueue.Job, io.Reader) error) 
 	}
 
 	if len(r) > 0 {
-		return &result, &TargetsError{r}
+		return result, &TargetsError{r}
 	}
 
-	return &result, nil
+	return result, nil
 }
 
 func main() {

cmd/osbuild-worker/osbuild.go
@@ -1,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os/exec"
+
+	"github.com/osbuild/osbuild-composer/internal/common"
+	"github.com/osbuild/osbuild-composer/internal/osbuild"
+)
+
+func RunOSBuild(manifest *osbuild.Manifest, store string, errorWriter io.Writer) (*common.ComposeResult, error) {
+	cmd := exec.Command(
+		"osbuild",
+		"--store", store,
+		"--json", "-",
+	)
+	cmd.Stderr = errorWriter
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return nil, fmt.Errorf("error setting up stdin for osbuild: %v", err)
+	}
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, fmt.Errorf("error setting up stdout for osbuild: %v", err)
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return nil, fmt.Errorf("error starting osbuild: %v", err)
+	}
+
+	err = json.NewEncoder(stdin).Encode(manifest)
+	if err != nil {
+		return nil, fmt.Errorf("error encoding osbuild pipeline: %v", err)
+	}
+	// FIXME: handle or comment this possible error
+	_ = stdin.Close()
+
+	var result common.ComposeResult
+	err = json.NewDecoder(stdout).Decode(&result)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding osbuild output: %#v", err)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		return &result, err
+	}
+
+	return &result, nil
+}

commit f4bb9fecbbaed53a8498fa08317e2945a4844375

worker: fix missing logs when osbuild fails
The commit 2435163f broke sending the logs to osbuild-composer. This was
partly because of unusual error handling in the RunOSBuild function.

This commit fixes that by creating a custom error and properly propagating
the result from it.

cmd/osbuild-worker/main.go
@@ -71,7 +71,7 @@ func RunJob(job *worker.Job, uploadFunc func(*worker.Job, io.Reader) error) (*co
 
 	result, err := RunOSBuild(job.Manifest, tmpStore, os.Stderr)
 	if err != nil {
-		return nil, fmt.Errorf("osbuild error: %v", err)
+		return result, fmt.Errorf("osbuild error: %v", err)
 	}
 
 	var r []error
@@ -175,6 +175,11 @@ func main() {
 		if err != nil {
 			log.Printf("  Job failed: %v", err)
 			status = common.IBFailed
+
+			// If the error comes from osbuild, retrieve the result
+			if osbuildError, ok := err.(*OSBuildError); ok {
+				result = osbuildError.Result
+			}
 		} else {
 			status = common.IBFinished
 		}

cmd/osbuild-worker/osbuild.go
@@ -10,6 +10,15 @@ import (
 	"github.com/osbuild/osbuild-composer/internal/osbuild"
 )
 
+type OSBuildError struct {
+	Message string
+	Result  *common.ComposeResult
+}
+
+func (e *OSBuildError) Error() string {
+	return e.Message
+}
+
 func RunOSBuild(manifest *osbuild.Manifest, store string, errorWriter io.Writer) (*common.ComposeResult, error) {
 	cmd := exec.Command(
 		"osbuild",
@@ -48,7 +57,10 @@ func RunOSBuild(manifest *osbuild.Manifest, store string, errorWriter io.Writer)
 
 	err = cmd.Wait()
 	if err != nil {
-		return &result, err
+		return nil, &OSBuildError{
+			Message: fmt.Sprintf("running osbuild failed: %v", err),
+			Result:  &result,
+		}
 	}
 
 	return &result, nil