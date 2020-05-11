d87a04f02fc5988170fd51f8b12374c6eaeed004
Tom Gundersen <teg@jklm.no>
2020-05-11 23:51:52 +0200

    store/json: simplify ImageType handling
    Serialize to a plain string, and do the translation for compatibility
    from string to string.
    
    This removes a lot of code, and if we change the store format, we should
    just skip the conversion altogether.
    
    Signed-off-by: Tom Gundersen <teg@jklm.no>

--- internal/store/json_test.go
+++ internal/store/json_test.go
@@ -1,23 +1,72 @@
 package store
 
 import (
-	"encoding/json"
 	"testing"
+
+	"github.com/osbuild/osbuild-composer/internal/distro"
+	"github.com/osbuild/osbuild-composer/internal/distro/test_distro"
 )
 
-func TestImageType_UnmarshalJSON(t *testing.T) {
-	dict := struct {
-		ImageTypes []imageType `json:"image_types"`
-	}{}
-	input := `{"image_types":["qcow2", "Azure"]}`
-	err := json.Unmarshal([]byte(input), &dict)
-	if err != nil {
-		t.Fatal(err)
+func Test_imageTypeToCompatString(t *testing.T) {
+	type args struct {
+		input distro.ImageType
 	}
-	if dict.ImageTypes[0] != Qcow2Generic {
-		t.Fatal("failed to umarshal image type qcow2; got tag:", dict.ImageTypes[0])
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "valid",
+			args: args{
+				input: &test_distro.TestImageType{},
+			},
+			want: "test_type",
+		},
 	}
-	if dict.ImageTypes[1] != Azure {
-		t.Fatal("failed to umarshal image type Azure; got tag:", dict.ImageTypes[0])
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := imageTypeToCompatString(tt.args.input)
+			if got != tt.want {
+				t.Errorf("imageTypeStringToCompatString() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_imageTypeFromCompatString(t *testing.T) {
+	type args struct {
+		input string
+		arch  distro.Arch
+	}
+	tests := []struct {
+		name string
+		args args
+		want distro.ImageType
+	}{
+		{
+			name: "valid",
+			args: args{
+				input: "test_type",
+				arch:  &test_distro.TestArch{},
+			},
+			want: &test_distro.TestImageType{},
+		},
+		{
+			name: "invalid",
+			args: args{
+				input: "foo",
+				arch:  &test_distro.TestArch{},
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := imageTypeFromCompatString(tt.args.input, tt.args.arch)
+			if got != tt.want {
+				t.Errorf("imageTypeStringFromCompatString() got = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
