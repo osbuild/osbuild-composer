682f60943f63b1f8732622613b187fa125967552
Tom Gundersen <teg@jklm.no>
2020-05-27 11:37:04 +0200

    distro: drop fedora-30
    We never shipped into fedora-30 and it is now EOL.
    
    Signed-off-by: Tom Gundersen <teg@jklm.no>

--- internal/distro/distro_test.go
+++ internal/distro/distro_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/osbuild/osbuild-composer/internal/distro"
 	"github.com/osbuild/osbuild-composer/internal/distro/distro_test_common"
-	"github.com/osbuild/osbuild-composer/internal/distro/fedora30"
 	"github.com/osbuild/osbuild-composer/internal/distro/fedora31"
 	"github.com/osbuild/osbuild-composer/internal/distro/fedora32"
 	"github.com/osbuild/osbuild-composer/internal/distro/rhel8"
@@ -18,20 +17,19 @@ func TestDistro_Manifest(t *testing.T) {
 		t,
 		"../../test/cases/",
 		"*",
-		fedora30.New(), fedora31.New(), fedora32.New(), rhel8.New(),
+		fedora31.New(), fedora32.New(), rhel8.New(),
 	)
 }
 
 // Test that all distros are registered properly and that Registry.List() works.
 func TestDistro_RegistryList(t *testing.T) {
 	expected := []string{
-		"fedora-30",
 		"fedora-31",
 		"fedora-32",
 		"rhel-8",
 	}
 
-	distros, err := distro.NewRegistry(fedora30.New(), fedora31.New(), fedora32.New(), rhel8.New())
+	distros, err := distro.NewRegistry(fedora31.New(), fedora32.New(), rhel8.New())
 	require.NoError(t, err)
 
 	require.Equalf(t, expected, distros.List(), "unexpected list of distros")
