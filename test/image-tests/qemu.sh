989e15e2fb71651d69c58c8e62b1f509ceac8cae
Major Hayden <major@redhat.com>
2020-06-15 15:03:42 -0500

    CI: Fix copy/paste errors in log gathering
    Perhaps my ability to copy and paste code should be taken away. 🤦🏻‍♂️
    
    Signed-off-by: Major Hayden <major@redhat.com>

--- test/image-tests/qemu.sh
+++ test/image-tests/qemu.sh
@@ -95,7 +95,7 @@ smoke_test_check () {
 # Get the compose log.
 get_compose_log () {
     COMPOSE_ID=$1
-    LOG_FILE=${WORKSPACE}/osbuild-${ID}-${VERSION_ID}-aws.log
+    LOG_FILE=${WORKSPACE}/osbuild-${ID}-${VERSION_ID}-${IMAGE_TYPE}.log
 
     # Download the logs.
     sudo composer-cli compose log $COMPOSE_ID | tee $LOG_FILE > /dev/null
@@ -104,7 +104,7 @@ get_compose_log () {
 # Get the compose metadata.
 get_compose_metadata () {
     COMPOSE_ID=$1
-    METADATA_FILE=${WORKSPACE}/osbuild-${ID}-${VERSION_ID}-aws.log
+    METADATA_FILE=${WORKSPACE}/osbuild-${ID}-${VERSION_ID}-${IMAGE_TYPE}.json
 
     # Download the metadata.
     sudo composer-cli compose metadata $COMPOSE_ID > /dev/null
