e4b839b31f6fefd62653e7d62ae4678f8eee1002
Ondřej Budai <obudai@redhat.com>
2020-08-28 13:19:55 +0200

    kojiapi: add koji target to composes
    So far, composes created by kojiapi didn't have any targets. This commit
    adds the koji target to them.
    
    This is the last piece of the puzzle. From now on, osbuild-composer has
    a koji API, which is actually able to upload images to Koji! Yay!

--- internal/target/koji_target.go
+++ internal/target/koji_target.go
@@ -1,6 +1,8 @@
 package target
 
 type KojiTargetOptions struct {
+	BuildID         uint64 `json:"build_id"`
+	Token           string `json:"token"`
 	Filename        string `json:"filename"`
 	UploadDirectory string `json:"upload_directory"`
 	Server          string `json:"server"`
