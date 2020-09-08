c07674e1f44b367933b08cf7d8030c0b1d7c0dbb
Lars Karlitski <lars@karlitski.net>
2020-09-08 17:09:16 +0200

    tools/prepare-source: also call go generate ./...
    To ensure that all generated code is up to date.

--- tools/prepare-source.sh
+++ tools/prepare-source.sh
@@ -10,7 +10,12 @@ GO_BINARY=$(go env GOPATH)/bin/go$GO_VERSION
 go get golang.org/dl/go$GO_VERSION
 $GO_BINARY download
 
-# prepare the sources
+# ensure that the code is formatted correctly, ...
 $GO_BINARY fmt ./...
+
+# ... go.mod and go.sum are up to date, ...
 $GO_BINARY mod tidy
 $GO_BINARY mod vendor
+
+# ... and all code has been regenerated from its sources.
+$GO_BINARY generate ./...
