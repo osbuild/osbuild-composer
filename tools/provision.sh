20fb5ac854bea121b07ce5e2f6d8de9a1208629e
Ondřej Budai <obudai@redhat.com>
2020-10-13 11:11:30 +0200

    test: generate certificates on fly instead of shipping them in the test RPM
    I have several reasons to do this:
    
    1) The pre-generated certificates are not documented. If someone wants
       to inspect them, he must know the right openssl incantation. This way,
       you are able to see what's inside the certificates in a plain text.
    
    2) The pre-generated certificates are going to expire at one point and
       someone will be surprised.
    
    3) Shipping private keys in RPMs is iffy. I know, it's just for testing but
       still...
    
    4) Auth tests are generating their own certificates. To achieve consistency,
       we have two options:
    
       a) Ship also all certificates for auth tests. That's extra 8 ones or
       something like that.
    
       b) Generate all certificates on fly. This commit does that.
    
    tl;dr: I think this is a good step forward.

--- tools/provision.sh
+++ tools/provision.sh
@@ -12,13 +12,57 @@ sudo mkdir -p /etc/osbuild-composer/repositories
 sudo cp -a /usr/share/tests/osbuild-composer/repositories/fedora-*.json \
     /etc/osbuild-composer/repositories/
 
-sudo cp -a /usr/share/tests/osbuild-composer/ca/* \
-    /etc/osbuild-composer/
-sudo chown _osbuild-composer /etc/osbuild-composer/composer-*.pem
+# Generate all X.509 certificates for the tests
+CERTDIR=/etc/osbuild-composer
+
+# Generate a CA.
+sudo /usr/libexec/osbuild-composer-test/x509/generate-certificate \
+    -selfsigned \
+    -out "$CERTDIR"/ca-crt.pem \
+    -keyout "$CERTDIR"/ca-key.pem \
+    -cn ca.osbuild.org \
+    -san ca.osbuild.org
+
+# Generate a composer certificate.
+sudo /usr/libexec/osbuild-composer-test/x509/generate-certificate \
+    -CA "$CERTDIR"/ca-crt.pem \
+    -CAkey "$CERTDIR"/ca-key.pem \
+    -out "$CERTDIR"/composer-crt.pem \
+    -keyout "$CERTDIR"/composer-key.pem \
+    -cn localhost \
+    -san localhost
+sudo chown _osbuild-composer "$CERTDIR"/composer-*.pem
+
+# Generate a worker certificate.
+sudo /usr/libexec/osbuild-composer-test/x509/generate-certificate \
+    -CA "$CERTDIR"/ca-crt.pem \
+    -CAkey "$CERTDIR"/ca-key.pem \
+    -out "$CERTDIR"/worker-crt.pem \
+    -keyout "$CERTDIR"/worker-key.pem \
+    -cn localhost \
+    -san localhost
+
+# Generate a client certificate.
+sudo /usr/libexec/osbuild-composer-test/x509/generate-certificate \
+    -CA "$CERTDIR"/ca-crt.pem \
+    -CAkey "$CERTDIR"/ca-key.pem \
+    -out "$CERTDIR"/client-crt.pem \
+    -keyout "$CERTDIR"/client-key.pem \
+    -cn client.osbuild.org \
+    -san client.osbuild.org
+
+# Client keys are used by tests to access the composer APIs. Allow all users access.
+sudo chmod 644 "$CERTDIR"/client-key.pem
 
 sudo systemctl start osbuild-remote-worker.socket
 sudo systemctl start osbuild-composer.socket
 sudo systemctl start osbuild-composer-api.socket
+
+# The keys were regenerated but osbuild-composer might be already running.
+# Let's try to restart it. In ideal world, this shouldn't be needed as every
+# test case is supposed to run on a pristine machine. However, this is
+# currently not true on Schutzbot
+sudo systemctl try-restart osbuild-composer
 
 # Basic verification
 sudo composer-cli status show
