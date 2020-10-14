6badebef4f494d648878375cb5f50d672972698e
Martin Sehnoutka <sehnoutka.martin@gmail.com>
2020-10-14 15:21:09 +0200

    schutzbot/run_cloud_cleaner: pass BRANCH_NAME instead of CHANGE_ID
    CHANGE_ID is unset in master branch. Use BRANCH_NAME instead.

--- schutzbot/run_cloud_cleaner.sh
+++ schutzbot/run_cloud_cleaner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-CLEANER_CMD="env $(cat "$AZURE_CREDS") CHANGE_ID=$CHANGE_ID BUILD_ID=$BUILD_ID DISTRO_CODE=$DISTRO_CODE /usr/libexec/osbuild-composer/cloud-cleaner"
+CLEANER_CMD="env $(cat "$AZURE_CREDS") BRANCH_NAME=$BRANCH_NAME BUILD_ID=$BUILD_ID DISTRO_CODE=$DISTRO_CODE /usr/libexec/osbuild-composer/cloud-cleaner"
 
 echo "🧹 Running the cloud cleaner"
 $CLEANER_CMD
