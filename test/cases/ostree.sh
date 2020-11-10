4b19095b208ac1e2ed876178c73f5672f1a0d704
Ondřej Budai <obudai@redhat.com>
2020-11-10 09:45:22 +0100

    test/ostree: add support for Fedora 33
    Let's use the same repository as for Fedora 32, it proved to be very stable.

--- test/cases/ostree.sh
+++ test/cases/ostree.sh
@@ -17,6 +17,11 @@ case "${ID}-${VERSION_ID}" in
         OSTREE_REF="fedora/32/${ARCH}/iot"
         OS_VARIANT="fedora32"
         BOOT_LOCATION="https://mirrors.rit.edu/fedora/fedora/linux/releases/32/Everything/x86_64/os/";;
+    "fedora-33")
+        IMAGE_TYPE=fedora-iot-commit
+        OSTREE_REF="fedora/33/${ARCH}/iot"
+        OS_VARIANT="fedora33"
+        BOOT_LOCATION="https://mirrors.rit.edu/fedora/fedora/linux/releases/33/Everything/x86_64/os/";;
     "rhel-8.3")
         IMAGE_TYPE=rhel-edge-commit
         OSTREE_REF="rhel/8/${ARCH}/edge"
