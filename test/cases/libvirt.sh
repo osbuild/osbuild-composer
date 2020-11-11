9e81f1df641447f84e1cdb073f5cbdc6ad60ea80
Ondřej Budai <obudai@redhat.com>
2020-11-11 10:00:49 +0100

    test/qemu: rename to libvirt
    This test doesn't use qemu directly , therefore imho libvirt is a more
    appropriate name.

---
+++ test/cases/libvirt.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+# Colorful output.
+function greenprint {
+    echo -e "\033[1;32m${1}\033[0m"
+}
+
+greenprint "Running libvirt integration tests for qcow2"
+/usr/libexec/osbuild-composer/test/libvirt-integration-tests.sh qcow2
+
+greenprint "Running libvirt integration tests for openstack"
+/usr/libexec/osbuild-composer/test/libvirt-integration-tests.sh openstack
+
+greenprint "Running libvirt integration tests for vhd"
+/usr/libexec/osbuild-composer/test/libvirt-integration-tests.sh vhd
