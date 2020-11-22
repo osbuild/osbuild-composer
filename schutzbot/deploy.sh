9bf3e957f68fde0e0f849ffd379b85168fd80040
Lars Karlitski <lars@karlitski.net>
2020-11-22 13:39:57 +0100

    Schutzfile: add koji-osbuild as dependant
    Generalize deploy.sh to allow setting up reverse dependency tests. Those
    need the same setup for osbuild and osbuild-composer (i.e., the ones
    under test), and in addition need the -tests package of the dependant
    project.
    
    Add test runs for koji-osbuild for all operating systems in Jenkinsfile.
    
    Use a commit in koji-osbuild for which a repository was pushed to s3 in
    the right format.

--- schutzbot/deploy.sh
+++ schutzbot/deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -euxo pipefail
 
+# The project whose -tests package is installed.
+#
+# If it is osbuild-composer (the default), it is pulled from the same
+# repository as the osbuild-composer under test. For all other projects, the
+# "dependants" key in Schutzfile is consulted to determine the repository to
+# pull the -test package from.
+PROJECT=${1:-osbuild-composer}
+
 # Colorful output.
 function greenprint {
     echo -e "\033[1;32m${1}\033[0m"
@@ -64,12 +72,17 @@ if [[ "${OSBUILD_GIT_COMMIT}" != "null" ]]; then
   setup_repo osbuild "${OSBUILD_GIT_COMMIT}" 10
 fi
 
+if [[ "$PROJECT" != "osbuild-composer" ]]; then
+  PROJECT_COMMIT=$(jq -r ".[\"${ID}-${VERSION_ID}\"].dependants[\"${PROJECT}\"].commit" Schutzfile)
+  setup_repo "${PROJECT}" "${PROJECT_COMMIT}" 10
+fi
+
 if [[ $ID == rhel ]]; then
     greenprint "Setting up EPEL repository"
     # we need this for ansible and koji
     sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 fi
 
-greenprint "Installing the Image Builder packages"
+greenprint "Installing test packages for ${PROJECT}"
 # Note: installing only -tests to catch missing dependencies
-retry sudo dnf -y install osbuild-composer-tests
+retry sudo dnf -y install "${PROJECT}-tests"
