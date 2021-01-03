9d101715dc59adcfe35edd07ec9fe82c447e5244
Lars Karlitski <lars@karlitski.net>
2021-01-03 21:07:46 +0100

    jobqueue: move fsjobqueue's generic tests into new package
    fsjobqueue_test contained tests that are generically testing the
    JobQueue interface. Split those out into its own package `jobqueuetest`.
    
    These tests will be useful when implementing a new package that conforms
    to the JobQueue interface.

--- internal/jobqueue/jobqueuetest/jobqueuetest.go
+++ internal/jobqueue/jobqueuetest/jobqueuetest.go
@@ -1,69 +1,41 @@
-package fsjobqueue_test
+// Package jobqueuetest provides test functions to verify a JobQueue
+// implementation satisfies the interface in package jobqueue.
+
+package jobqueuetest
 
 import (
-	"io/ioutil"
-	"os"
+	"context"
+	"encoding/json"
 	"testing"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 
 	"github.com/osbuild/osbuild-composer/internal/jobqueue"
-	"github.com/osbuild/osbuild-composer/internal/jobqueue/fsjobqueue"
-	"github.com/osbuild/osbuild-composer/internal/jobqueue/jobqueuetest"
 )
 
-type testResult struct {
+type MakeJobQueue func() (q jobqueue.JobQueue, stop func(), err error)
+
+func TestJobQueue(t *testing.T, makeJobQueue MakeJobQueue) {
+	wrap := func(f func(t *testing.T, q jobqueue.JobQueue)) func(*testing.T) {
+		q, stop, err := makeJobQueue()
+		require.NoError(t, err)
+		return func(t *testing.T) {
+			defer stop() // use defer because f() might call testing.T.FailNow()
+			f(t, q)
+		}
+	}
+
+	t.Run("errors", wrap(testErrors))
+	t.Run("args", wrap(testArgs))
+	t.Run("cancel", wrap(testCancel))
+	t.Run("job-types", wrap(testJobTypes))
+	t.Run("dependencies", wrap(testDependencies))
+	t.Run("multiple-workers", wrap(testMultipleWorkers))
 }
 
-func cleanupTempDir(t *testing.T, dir string) {
-	err := os.RemoveAll(dir)
-	require.NoError(t, err)
-}
-
-func newTemporaryQueue(t *testing.T) (jobqueue.JobQueue, string) {
-	dir, err := ioutil.TempDir("", "jobqueue-test-")
-	require.NoError(t, err)
-
-	q, err := fsjobqueue.New(dir)
-	require.NoError(t, err)
-	require.NotNil(t, q)
-
-	return q, dir
-}
-
-func pushTestJob(t *testing.T, q jobqueue.JobQueue, jobType string, args interface{}, dependencies []uuid.UUID) uuid.UUID {
-	t.Helper()
-	id, err := q.Enqueue(jobType, args, dependencies)
-	require.NoError(t, err)
-	require.NotEmpty(t, id)
-	return id
-}
-
-func finishNextTestJob(t *testing.T, q jobqueue.JobQueue, jobType string, result interface{}, deps []uuid.UUID) uuid.UUID {
-	id, tok, d, typ, args, err := q.Dequeue(context.Background(), []string{jobType})
-	require.NoError(t, err)
-	require.NotEmpty(t, id)
-	require.NotEmpty(t, tok)
-	require.ElementsMatch(t, deps, d)
-	require.Equal(t, jobType, typ)
-	require.NotNil(t, args)
-
-	err = q.FinishJob(id, result)
-	require.NoError(t, err)
-
-	return id
-}
-
-func TestNonExistant(t *testing.T) {
-	q, err := fsjobqueue.New("/non-existant-directory")
-	require.Error(t, err)
-	require.Nil(t, q)
-}
-
-func TestErrors(t *testing.T) {
-	q, dir := newTemporaryQueue(t)
-	defer cleanupTempDir(t, dir)
-
+func testErrors(t *testing.T, q jobqueue.JobQueue) {
 	// not serializable to JSON
 	id, err := q.Enqueue("test", make(chan string), nil)
 	require.Error(t, err)
@@ -73,34 +45,13 @@ func TestErrors(t *testing.T) {
 	id, err = q.Enqueue("test", "arg0", []uuid.UUID{uuid.New()})
 	require.Error(t, err)
 	require.Equal(t, uuid.Nil, id)
-
-	// token gets removed
-	pushTestJob(t, q, "octopus", nil, nil)
-	id, tok, _, _, _, err := q.Dequeue(context.Background(), []string{"octopus"})
-	require.NoError(t, err)
-	require.NotEmpty(t, tok)
-
-	idFromT, err := q.IdFromToken(tok)
-	require.NoError(t, err)
-	require.Equal(t, id, idFromT)
-
-	err = q.FinishJob(id, nil)
-	require.NoError(t, err)
-
-	// Make sure the token gets removed
-	id, err = q.IdFromToken(tok)
-	require.Equal(t, uuid.Nil, id)
-	require.Equal(t, jobqueue.ErrNotExist, err)
 }
 
-func TestArgs(t *testing.T) {
+func testArgs(t *testing.T, q jobqueue.JobQueue) {
 	type argument struct {
 		I int
 		S string
 	}
-
-	q, dir := newTemporaryQueue(t)
-	defer cleanupTempDir(t, dir)
 
 	oneargs := argument{7, "🐠"}
 	one := pushTestJob(t, q, "fish", oneargs, nil)
@@ -110,46 +61,27 @@ func TestArgs(t *testing.T) {
 
 	var parsedArgs argument
 
-	id, tok, deps, typ, args, err := q.Dequeue(context.Background(), []string{"octopus"})
+	id, deps, typ, args, err := q.Dequeue(context.Background(), []string{"octopus"})
 	require.NoError(t, err)
 	require.Equal(t, two, id)
-	require.NotEmpty(t, tok)
 	require.Empty(t, deps)
 	require.Equal(t, "octopus", typ)
 	err = json.Unmarshal(args, &parsedArgs)
 	require.NoError(t, err)
 	require.Equal(t, twoargs, parsedArgs)
 
-	// Read job params after Dequeue
-	jtype, jargs, jdeps, err := q.Job(id)
-	require.NoError(t, err)
-	require.Equal(t, args, jargs)
-	require.Equal(t, deps, jdeps)
-	require.Equal(t, typ, jtype)
-
-	id, tok, deps, typ, args, err = q.Dequeue(context.Background(), []string{"fish"})
+	id, deps, typ, args, err = q.Dequeue(context.Background(), []string{"fish"})
 	require.NoError(t, err)
 	require.Equal(t, one, id)
-	require.NotEmpty(t, tok)
 	require.Empty(t, deps)
 	require.Equal(t, "fish", typ)
 	err = json.Unmarshal(args, &parsedArgs)
 	require.NoError(t, err)
 	require.Equal(t, oneargs, parsedArgs)
-
-	jtype, jargs, jdeps, err = q.Job(id)
-	require.NoError(t, err)
-	require.Equal(t, args, jargs)
-	require.Equal(t, deps, jdeps)
-	require.Equal(t, typ, jtype)
-
-	_, _, _, err = q.Job(uuid.New())
-	require.Error(t, err)
 }
 
-func TestJobTypes(t *testing.T) {
-	q, dir := newTemporaryQueue(t)
-	defer cleanupTempDir(t, dir)
+func testJobTypes(t *testing.T, q jobqueue.JobQueue) {
+	type testResult struct{}
 
 	one := pushTestJob(t, q, "octopus", nil, nil)
 	two := pushTestJob(t, q, "clownfish", nil, nil)
@@ -159,18 +91,16 @@ func TestJobTypes(t *testing.T) {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	cancel()
-	id, tok, deps, typ, args, err := q.Dequeue(ctx, []string{"zebra"})
+	id, deps, typ, args, err := q.Dequeue(ctx, []string{"zebra"})
 	require.Equal(t, err, context.Canceled)
 	require.Equal(t, uuid.Nil, id)
-	require.Equal(t, uuid.Nil, tok)
 	require.Empty(t, deps)
 	require.Equal(t, "", typ)
 	require.Nil(t, args)
 }
 
-func TestDependencies(t *testing.T) {
-	q, dir := newTemporaryQueue(t)
-	defer cleanupTempDir(t, dir)
+func testDependencies(t *testing.T, q jobqueue.JobQueue) {
+	type testResult struct{}
 
 	t.Run("done-before-pushing-dependant", func(t *testing.T) {
 		one := pushTestJob(t, q, "test", nil, nil)
@@ -239,19 +169,15 @@ func TestDependencies(t *testing.T) {
 
 // Test that a job queue allows parallel access to multiple workers, mainly to
 // verify the quirky unlocking in Dequeue().
-func TestMultipleWorkers(t *testing.T) {
-	q, dir := newTemporaryQueue(t)
-	defer cleanupTempDir(t, dir)
-
+func testMultipleWorkers(t *testing.T, q jobqueue.JobQueue) {
 	done := make(chan struct{})
 	go func() {
 		defer close(done)
 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 		defer cancel()
-		id, tok, deps, typ, args, err := q.Dequeue(ctx, []string{"octopus"})
+		id, deps, typ, args, err := q.Dequeue(ctx, []string{"octopus"})
 		require.NoError(t, err)
 		require.NotEmpty(t, id)
-		require.NotEmpty(t, tok)
 		require.Empty(t, deps)
 		require.Equal(t, "octopus", typ)
 		require.Equal(t, json.RawMessage("null"), args)
@@ -263,10 +189,9 @@ func TestMultipleWorkers(t *testing.T) {
 
 	// This call to Dequeue() should not block on the one in the goroutine.
 	id := pushTestJob(t, q, "clownfish", nil, nil)
-	r, tok, deps, typ, args, err := q.Dequeue(context.Background(), []string{"clownfish"})
+	r, deps, typ, args, err := q.Dequeue(context.Background(), []string{"clownfish"})
 	require.NoError(t, err)
 	require.Equal(t, id, r)
-	require.NotEmpty(t, tok)
 	require.Empty(t, deps)
 	require.Equal(t, "clownfish", typ)
 	require.Equal(t, json.RawMessage("null"), args)
@@ -276,9 +201,8 @@ func TestMultipleWorkers(t *testing.T) {
 	<-done
 }
 
-func TestCancel(t *testing.T) {
-	q, dir := newTemporaryQueue(t)
-	defer cleanupTempDir(t, dir)
+func testCancel(t *testing.T, q jobqueue.JobQueue) {
+	type testResult struct{}
 
 	// Cancel a non-existing job
 	err := q.CancelJob(uuid.New())
@@ -299,10 +223,9 @@ func TestCancel(t *testing.T) {
 	// Cancel a running job, which should not dequeue the canceled job from above
 	id = pushTestJob(t, q, "clownfish", nil, nil)
 	require.NotEmpty(t, id)
-	r, tok, deps, typ, args, err := q.Dequeue(context.Background(), []string{"clownfish"})
+	r, deps, typ, args, err := q.Dequeue(context.Background(), []string{"clownfish"})
 	require.NoError(t, err)
 	require.Equal(t, id, r)
-	require.NotEmpty(t, tok)
 	require.Empty(t, deps)
 	require.Equal(t, "clownfish", typ)
 	require.Equal(t, json.RawMessage("null"), args)
@@ -318,10 +241,9 @@ func TestCancel(t *testing.T) {
 	// Cancel a finished job, which is a no-op
 	id = pushTestJob(t, q, "clownfish", nil, nil)
 	require.NotEmpty(t, id)
-	r, tok, deps, typ, args, err = q.Dequeue(context.Background(), []string{"clownfish"})
+	r, deps, typ, args, err = q.Dequeue(context.Background(), []string{"clownfish"})
 	require.NoError(t, err)
 	require.Equal(t, id, r)
-	require.NotEmpty(t, tok)
 	require.Empty(t, deps)
 	require.Equal(t, "clownfish", typ)
 	require.Equal(t, json.RawMessage("null"), args)
@@ -336,32 +258,24 @@ func TestCancel(t *testing.T) {
 	require.NoError(t, err)
 }
 
-func TestHeartbeats(t *testing.T) {
-	q, dir := newTemporaryQueue(t)
-	defer cleanupTempDir(t, dir)
-
-	id := pushTestJob(t, q, "octopus", nil, nil)
-	// No heartbeats for queued job
-	require.Empty(t, q.Heartbeats(time.Second*0))
-
-	r, tok, _, _, _, err := q.Dequeue(context.Background(), []string{"octopus"})
+func pushTestJob(t *testing.T, q jobqueue.JobQueue, jobType string, args interface{}, dependencies []uuid.UUID) uuid.UUID {
+	t.Helper()
+	id, err := q.Enqueue(jobType, args, dependencies)
 	require.NoError(t, err)
-	require.Equal(t, id, r)
-	require.NotEmpty(t, tok)
+	require.NotEmpty(t, id)
+	return id
+}
 
-	tokens := q.Heartbeats(time.Second * 0)
-	require.Contains(t, tokens, tok)
-	require.Empty(t, q.Heartbeats(time.Hour*24))
-
-	id2, err := q.IdFromToken(tok)
+func finishNextTestJob(t *testing.T, q jobqueue.JobQueue, jobType string, result interface{}, deps []uuid.UUID) uuid.UUID {
+	id, d, typ, args, err := q.Dequeue(context.Background(), []string{jobType})
 	require.NoError(t, err)
-	require.Equal(t, id, id2)
+	require.NotEmpty(t, id)
+	require.ElementsMatch(t, deps, d)
+	require.Equal(t, jobType, typ)
+	require.NotNil(t, args)
 
-	err = q.FinishJob(id, &testResult{})
+	err = q.FinishJob(id, result)
 	require.NoError(t, err)
 
-	// No heartbeats for finished job
-	require.Empty(t, q.Heartbeats(time.Second*0))
-	_, err = q.IdFromToken(tok)
-	require.Equal(t, jobqueue.ErrNotExist, err)
+	return id
 }
