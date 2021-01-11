1a81489ef17d721b7c89a5b046ef4d00af8c734e
Jozef Mikovic <jmikovic@redhat.com>
2021-01-11 14:20:09 +0100

    osbuild-worker: add target for upload to vmware
    New upload target for VMWare, similar to the ones for AWS and Azure,
    allowing users to set credentials for their vSphere instance.
    Commit also includes function that performs the actual upload.

--- internal/upload/vmware/vmware.go
+++ internal/upload/vmware/vmware.go
@@ -1,10 +1,24 @@
 package vmware
 
 import (
+	"errors"
+	"fmt"
 	"os"
 	"os/exec"
 	"strings"
+
+	"github.com/vmware/govmomi/govc/cli"
+	_ "github.com/vmware/govmomi/govc/importx"
 )
+
+type Credentials struct {
+	Host       string
+	Username   string
+	Password   string
+	Datacenter string
+	Cluster    string
+	Datastore  string
+}
 
 func OpenAsStreamOptimizedVmdk(imagePath string) (*os.File, error) {
 	newPath := strings.TrimSuffix(imagePath, ".vmdk") + "-stream.vmdk"
@@ -20,4 +34,23 @@ func OpenAsStreamOptimizedVmdk(imagePath string) (*os.File, error) {
 		return nil, err
 	}
 	return f, err
+}
+
+func UploadImage(creds Credentials, imagePath, imageName string) error {
+	args := []string{
+		"import.vmdk",
+		fmt.Sprintf("-u=%s:%s@%s", creds.Username, creds.Password, creds.Host),
+		"-k=true",
+		fmt.Sprintf("-pool=%s/Resources", creds.Cluster),
+		fmt.Sprintf("-dc=%s", creds.Datacenter),
+		fmt.Sprintf("-ds=%s", creds.Datastore),
+		imagePath,
+		imageName,
+	}
+	retcode := cli.Run(args)
+
+	if retcode != 0 {
+		return errors.New("importing vmdk failed")
+	}
+	return nil
 }
