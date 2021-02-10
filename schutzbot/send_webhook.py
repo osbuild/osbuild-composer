95e6c9613b2a1b233a41d2deb8138df5047c02b9
Major Hayden <major@redhat.com>
2021-02-10 07:44:14 -0600

    Send webhooks without credentials
    Signed-off-by: Major Hayden <major@redhat.com>

--- schutzbot/send_webhook.py
+++ schutzbot/send_webhook.py
@@ -4,13 +4,21 @@ import json
 import os
 
 import boto3
+from botocore import UNSIGNED
+from botocore.client import Config
 
 WEBHOOK_PAYLOAD = os.environ.get("WEBHOOK_PAYLOAD")
 EVENT_NAME = os.environ.get("EVENT_NAME")
-SQS_REGION = os.environ.get("SQS_REGION")
 SQS_QUEUE_URL = os.environ.get("SQS_QUEUE_URL")
+SQS_REGION = os.environ.get("SQS_REGION")
 
-sqs = boto3.client('sqs', region_name=SQS_REGION)
+sqs = boto3.client(
+    'sqs',
+    region_name=SQS_REGION,
+    config=Config(
+        signature_version=UNSIGNED
+    )
+)
 
 payload = json.loads(WEBHOOK_PAYLOAD)
 message = {
