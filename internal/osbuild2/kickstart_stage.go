a0f8b8106cf9dc9a14294ccc3ba8134f02dcca86
Achilleas Koutsou <achilleas@koutsou.net>
2021-03-03 16:12:21 +0100

    osbuild2: add support for new osbuild stages
    - org.osbuild.anaconda
        Configures Anaconda.  For now, only enabling kickstart modules is
        supported.
    
    - org.osbuild.buildstamp
        Creates a buildstamp file, which is required by Anaconda.
    
    - org.osbuild.kickstart
        Creates a kickstart file.
    
    - org.osbuild.lorax-script
        Uses lorax template helpers to execute a template.
    
    - org.osbuild.bootiso
        Prepares a bootable file system tree suitable for writing on an ISO
        file system
    
    - org.osbuild.discinfo
        Creates a .discinfo file, used by the Anaconda installer.
    
    - org.osbuild.xorrisofs
        Uses the `xorrisofs` command line utility to an ISO.
    
    - org.osbuild.implantisomd5
        Uses the `implantisomd5` command to implant MD5 checksums into an
        ISO.
    
    (cherry picked from commit bec194dfff2a8c453582a767b2b7361f70ebe416)

---
+++ internal/osbuild2/kickstart_stage.go
@@ -0,0 +1,31 @@
+package osbuild2
+
+type KickstartStageOptions struct {
+	// Where to place the kickstart file
+	Path string `json:"path"`
+
+	OSTree OSTreeOptions `json:"ostree,omitempty"`
+
+	LiveIMG *LiveIMG `json:"liveimg,omitempty"`
+}
+
+type LiveIMG struct {
+	URL string `json:"url"`
+}
+
+type OSTreeOptions struct {
+	OSName string `json:"osname"`
+	URL    string `json:"url"`
+	Ref    string `json:"ref"`
+	GPG    bool   `json:"gpg"`
+}
+
+func (KickstartStageOptions) isStageOptions() {}
+
+// Creates an Anaconda kickstart file
+func NewKickstartStage(options *KickstartStageOptions) *Stage {
+	return &Stage{
+		Type:    "org.osbuild.kickstart",
+		Options: options,
+	}
+}
