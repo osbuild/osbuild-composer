812a449a87f0727a87bdba9f05330a3ff6bbf04f
Ondřej Budai <ondrej@budai.cz>
2021-03-03 12:06:18 +0100

    target/azure: document what it does
    Signed-off-by: Ondřej Budai <ondrej@budai.cz>

--- internal/target/azure_target.go
+++ internal/target/azure_target.go
@@ -9,6 +9,16 @@ type AzureTargetOptions struct {
 
 func (AzureTargetOptions) isTargetOptions() {}
 
+// NewAzureTarget creates org.osbuild.azure target
+//
+// This target uploads a Page Blob to Azure Storage.
+//
+// The target uses Azure Storage keys for authentication, see:
+// https://docs.microsoft.com/en-us/azure/storage/common/storage-account-keys-manage
+// The credentials are defined inside the target options.
+//
+// If you need to upload an Azure Image instead, see the
+// org.osbuild.azure.image target.
 func NewAzureTarget(options *AzureTargetOptions) *Target {
 	return newTarget("org.osbuild.azure", options)
 }
