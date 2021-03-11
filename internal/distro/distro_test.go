6142f6af406eb736edf8c1aea383f9cf07e0c48f
Ondřej Budai <ondrej@budai.cz>
2021-03-11 13:02:50 +0100

    distro/test: use the new default distroregistry
    This way, we don't need to add here a new distro when it's added.
    
    Signed-off-by: Ondřej Budai <ondrej@budai.cz>

--- internal/distro/distro_test.go
+++ internal/distro/distro_test.go
@@ -4,17 +4,15 @@ import (
 	"testing"
 
 	"github.com/osbuild/osbuild-composer/internal/distro/distro_test_common"
-	"github.com/osbuild/osbuild-composer/internal/distro/fedora32"
-	"github.com/osbuild/osbuild-composer/internal/distro/fedora33"
-	"github.com/osbuild/osbuild-composer/internal/distro/rhel8"
-	"github.com/osbuild/osbuild-composer/internal/distro/rhel84"
+	"github.com/osbuild/osbuild-composer/internal/distroregistry"
 )
 
 func TestDistro_Manifest(t *testing.T) {
+
 	distro_test_common.TestDistro_Manifest(
 		t,
 		"../../test/data/manifests/",
 		"*",
-		fedora32.New(), fedora33.New(), rhel8.New(), rhel84.New(), rhel84.NewCentos(),
+		distroregistry.NewDefault(),
 	)
 }
