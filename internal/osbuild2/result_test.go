4a5402133823a9a7a0170eda0086dd59f1b4dc67
Ondřej Budai <ondrej@budai.cz>
2021-03-19 16:03:16 +0100

    osbuild1/result: fix conversion of success field from v2
    When a stage is successful in a manifest v2, the success field is omitted from
    the result. In other words, the default value of the success field is true
    which is against the default value of boolean in Go. This commit implements
    a workaround.
    
    Signed-off-by: Ondřej Budai <ondrej@budai.cz>

--- internal/osbuild2/result_test.go
+++ internal/osbuild2/result_test.go
@@ -1,1 +1,28 @@
 package osbuild2
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStageResult_UnmarshalJSON(t *testing.T) {
+	cases := []struct {
+		input   string
+		success bool
+	}{
+		{input: `{}`, success: true},
+		{input: `{"success": true}`, success: true},
+		{input: `{"success": false}`, success: false},
+	}
+
+	for _, c := range cases {
+		t.Run(c.input, func(t *testing.T) {
+			var result StageResult
+			err := json.Unmarshal([]byte(c.input), &result)
+			assert.NoError(t, err)
+			assert.Equal(t, c.success, result.Success)
+		})
+	}
+}
