ea8d080c8a33a11610e6859bce9d8c58f6d219b4
Ondřej Budai <ondrej@budai.cz>
2021-03-19 16:03:16 +0100

    osbuild1/result: fix conversion of success field from v2
    When a stage is successful in a manifest v2, the success field is omitted from
    the result. In other words, the default value of the success field is true
    which is against the default value of boolean in Go. This commit implements
    a workaround.
    
    Signed-off-by: Ondřej Budai <ondrej@budai.cz>

--- internal/osbuild2/result.go
+++ internal/osbuild2/result.go
@@ -14,6 +14,32 @@ type StageResult struct {
 	Error   string `json:"string,omitempty"`
 }
 
+// UnmarshalJSON decodes json-encoded StageResult.
+//
+// This method is here only as a workaround for the default value of the
+// success field, see the comment inside the method.
+func (sr *StageResult) UnmarshalJSON(data []byte) error {
+	// Create a StageResult-like object with the Success value set to true
+	// before json.Unmarshal is called. If the success field isn't in the
+	// input JSON, the json decoder will not touch it and thus it will still
+	// be true.
+	//
+	// The type alias is needed to prevent recursive calls of this method.
+	type stageResultAlias StageResult
+	stageResultDefault := stageResultAlias{
+		Success: true,
+	}
+
+	err := json.Unmarshal(data, &stageResultDefault)
+	if err != nil {
+		return err
+	}
+
+	*sr = StageResult(stageResultDefault)
+
+	return nil
+}
+
 type Result struct {
 	Type    string                    `json:"type"`
 	Success bool                      `json:"success"`
