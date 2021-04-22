ef0105945a314daf30de30f36219cde1a4e95435
Alexander Todorov <atodorov@redhat.com>
2021-04-22 12:56:33 +0300

    tests: Revert a lorax patch
    BZ 1843704 has been fixed and shipped in 8.3. We shouldn't need this
    patch anymore!

--- test/cases/aws.sh
+++ test/cases/aws.sh
@@ -19,14 +19,6 @@ fi
 # Provision the software under tet.
 /usr/libexec/osbuild-composer-test/provision.sh
 
-# Apply lorax patch to work around pytoml issues in RHEL 8.x.
-# See BZ 1843704 or https://github.com/weldr/lorax/pull/1030 for more details.
-if [[ $ID == rhel ]]; then
-    sudo sed -r -i 's#toml.load\(args\[3\]\)#toml.load(open(args[3]))#' \
-        /usr/lib/python3.6/site-packages/composer/cli/compose.py
-    sudo rm -f /usr/lib/python3.6/site-packages/composer/cli/compose.pyc
-fi
-
 # We need awscli to talk to AWS.
 if ! hash aws; then
     greenprint "Installing awscli"
