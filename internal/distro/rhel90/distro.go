a19b883727024088efb10548a1aed94d8de73897
Ondřej Budai <ondrej@budai.cz>
2021-05-14 13:10:11 +0200

    distro/rhel90: enable cloud-init
    cloud-init was enabled explicitly in the image-factory kickstart and thus we
    need to explicitly enable it too.
    
    Fixes: rhbz#1960309
    Fixes: COMPOSER-920
    Signed-off-by: Ondřej Budai <ondrej@budai.cz>

--- internal/distro/rhel90/distro.go
+++ internal/distro/rhel90/distro.go
@@ -822,6 +822,12 @@ func newDistro(name string) distro.Distro {
 			"rng-tools",
 			"udisks2",
 		},
+		enabledServices: []string{
+			"cloud-init.service",
+			"cloud-config.service",
+			"cloud-final.service",
+			"cloud-init-local.service",
+		},
 		defaultTarget:           "multi-user.target",
 		kernelOptions:           "console=tty0 console=ttyS0,115200n8 no_timer_check net.ifnames=0 crashkernel=auto",
 		bootable:                true,
