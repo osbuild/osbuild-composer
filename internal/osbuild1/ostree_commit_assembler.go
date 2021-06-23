332d23097eb09f9aa220f2d7a0e0fa230b0a1326
Achilleas Koutsou <achilleas@koutsou.net>
2021-06-23 16:46:22 +0200

    osbuild1: convert stage metadata from v2
    While unmarshalling osbuild output and converting from v1 to v2, convert
    the metadata as well.
    
    Added checks to successful parsing and conversion test.

--- internal/osbuild1/ostree_commit_assembler.go
+++ internal/osbuild1/ostree_commit_assembler.go
@@ -23,19 +23,21 @@ func NewOSTreeCommitAssembler(options *OSTreeCommitAssemblerOptions) *Assembler 
 }
 
 type OSTreeCommitStageMetadata struct {
-	Compose struct {
-		Ref                       string `json:"ref"`
-		OSTreeNMetadataTotal      int    `json:"ostree-n-metadata-total"`
-		OSTreeNMetadataWritten    int    `json:"ostree-n-metadata-written"`
-		OSTreeNContentTotal       int    `json:"ostree-n-content-total"`
-		OSTreeNContentWritten     int    `json:"ostree-n-content-written"`
-		OSTreeNCacheHits          int    `json:"ostree-n-cache-hits"`
-		OSTreeContentBytesWritten int    `json:"ostree-content-bytes-written"`
-		OSTreeCommit              string `json:"ostree-commit"`
-		OSTreeContentChecksum     string `json:"ostree-content-checksum"`
-		OSTreeTimestamp           string `json:"ostree-timestamp"`
-		RPMOSTreeInputHash        string `json:"rpm-ostree-inputhash"`
-	} `json:"compose"`
+	Compose OSTreeCommitStageMetadataCompose `json:"compose"`
+}
+
+type OSTreeCommitStageMetadataCompose struct {
+	Ref                       string `json:"ref"`
+	OSTreeNMetadataTotal      int    `json:"ostree-n-metadata-total"`
+	OSTreeNMetadataWritten    int    `json:"ostree-n-metadata-written"`
+	OSTreeNContentTotal       int    `json:"ostree-n-content-total"`
+	OSTreeNContentWritten     int    `json:"ostree-n-content-written"`
+	OSTreeNCacheHits          int    `json:"ostree-n-cache-hits"`
+	OSTreeContentBytesWritten int    `json:"ostree-content-bytes-written"`
+	OSTreeCommit              string `json:"ostree-commit"`
+	OSTreeContentChecksum     string `json:"ostree-content-checksum"`
+	OSTreeTimestamp           string `json:"ostree-timestamp"`
+	RPMOSTreeInputHash        string `json:"rpm-ostree-inputhash"`
 }
 
 func (OSTreeCommitStageMetadata) isStageMetadata() {}
