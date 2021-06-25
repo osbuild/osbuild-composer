beead86da6bb38190becd3d6993fbd460be31a41
Jakub Rusz <jrusz@redhat.com>
2021-06-25 12:11:52 +0200

    tests: update cloud-cleaner
    In 0680214c9b20b76f82c381fb3472d83846ca0c71 the BUILD_ID was changed
    in azure.sh test but not in cloud_cleaner causing cloud-cleaner to
    not clean up properly. This fixes that.

--- schutzbot/run_cloud_cleaner.sh
+++ schutzbot/run_cloud_cleaner.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 source /etc/os-release
 DISTRO_CODE="${DISTRO_CODE:-${ID}_${VERSION_ID//./}}"
 BRANCH_NAME="${BRANCH_NAME:-${CI_COMMIT_BRANCH}}"
-BUILD_ID="${BUILD_ID:-${CI_PIPELINE_ID}}"
+BUILD_ID="${BUILD_ID:-${CI_BUILD_ID}}"
 
 CLEANER_CMD="env $(cat "${AZURE_CREDS:-/dev/null}") BRANCH_NAME=$BRANCH_NAME BUILD_ID=$BUILD_ID DISTRO_CODE=$DISTRO_CODE /usr/libexec/osbuild-composer-test/cloud-cleaner"
 
