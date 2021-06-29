685c390df770a717ebad81585b23d76e2faa4484
Tomas Hozza <thozza@redhat.com>
2021-06-29 17:52:00 +0200

    osbuild2: add support for `org.osbuild.modprobe` stage
    Add support for the `org.osbuild.modprobe` osbuild stage [1], which
    allows one to configure modprobe by creating configuration files.
    
    Add unit test cases for the newly added stage.
    
    [1] https://github.com/osbuild/osbuild/pull/670
    
    Signed-off-by: Tomas Hozza <thozza@redhat.com>

---
+++ internal/osbuild2/modprobe_stage_test.go
@@ -0,0 +1,39 @@
+package osbuild2
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewModprobeStage(t *testing.T) {
+	expectedStage := &Stage{
+		Type:    "org.osbuild.modprobe",
+		Options: &ModprobeStageOptions{},
+	}
+	actualStage := NewModprobeStage(&ModprobeStageOptions{})
+	assert.Equal(t, expectedStage, actualStage)
+}
+
+func TestModprobeStage_MarshalJSON_Invalid(t *testing.T) {
+	tests := []struct {
+		name    string
+		options ModprobeStageOptions
+	}{
+		{
+			name: "no-commands",
+			options: ModprobeStageOptions{
+				ConfigFiles: map[string]ModprobeConfigCmdList{
+					"disallow-modules.conf": {},
+				},
+			},
+		},
+	}
+	for idx, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotBytes, err := json.Marshal(tt.options)
+			assert.NotNilf(t, err, "json.Marshal() didn't return an error, but: %s [idx: %d]", string(gotBytes), idx)
+		})
+	}
+}
