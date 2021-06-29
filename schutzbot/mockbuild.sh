ff62d725278c1273af280a6ebbc65a5bfdaeccbb
Alexander Todorov <atodorov@redhat.com>
2021-06-29 16:08:02 +0300

    tests: Cleanup leftover 8.4 references

--- schutzbot/mockbuild.sh
+++ schutzbot/mockbuild.sh
@@ -84,16 +84,7 @@ greenprint "🧬 Using mock config: ${MOCK_CONFIG}"
 greenprint "📦 SHA: ${COMMIT}"
 greenprint "📤 RPMS will be uploaded to: ${REPO_URL}"
 
-# rhel 8.4 and 8.5 will run off of the internal repos and does not have a redhat subscription
-if [[ $VERSION_ID == 8.4 ]]; then
-    greenprint "📋 Updating RHEL 8 mock template for unsubscribed image"
-    sudo sed -i '/# repos/q' /etc/mock/templates/rhel-8.tpl
-    # remove the subscription check
-    sudo sed -i "s/config_opts\['redhat_subscription_required'\] = True/config_opts['redhat_subscription_required'] = False/" /etc/mock/templates/rhel-8.tpl
-    cat "$RHEL84_NIGHTLY_REPO" | sudo tee -a /etc/mock/templates/rhel-8.tpl > /dev/null
-    # We need triple quotes at the end of the template to mark the end of the repo list.
-    echo '"""' | sudo tee -a /etc/mock/templates/rhel-8.tpl
-elif [[ $VERSION_ID == 8.5 ]]; then
+if [[ $VERSION_ID == 8.5 ]]; then
     greenprint "📋 Updating RHEL 8 mock template for unsubscribed image"
     sudo sed -i '/# repos/q' /etc/mock/templates/rhel-8.tpl
     # remove the subscription check
--- tools/provision.sh
+++ tools/provision.sh
@@ -52,17 +52,10 @@ sudo mkdir -p $REPODIR
 # Copy all fedora repo overrides
 sudo cp -a /usr/share/tests/osbuild-composer/repositories/{fedora,centos}-*.json "$REPODIR"
 # Copy RHEL point relese repos
-sudo cp /usr/share/tests/osbuild-composer/repositories/rhel-84.json "$REPODIR"
 sudo cp /usr/share/tests/osbuild-composer/repositories/rhel-85.json "$REPODIR"
 
 # RHEL nightly repos need to be overridden in rhel-8.json and rhel-8-beta.json
 case "${ID}-${VERSION_ID}" in
-    "rhel-8.4")
-        # Override old rhel-8.json and rhel-8-beta.json because RHEL 8.4 test needs nightly repos
-        sudo cp /usr/share/tests/osbuild-composer/repositories/rhel-84.json "$REPODIR/rhel-8.json"
-        # If multiple tests are run and call provision.sh the symlink will need to be overridden with -f
-        sudo ln -sf /etc/osbuild-composer/repositories/rhel-8.json "$REPODIR/rhel-8-beta.json"
-        ;;
     "rhel-8.5")
         # Override old rhel-8.json and rhel-8-beta.json because RHEL 8.5 test needs nightly repos
         sudo cp /usr/share/tests/osbuild-composer/repositories/rhel-85.json "$REPODIR/rhel-8.json"
