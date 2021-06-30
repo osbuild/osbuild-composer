bef4628a0846057bf148e43d375f272ff1295535
Tomas Hozza <thozza@redhat.com>
2021-06-30 12:45:50 +0200

    osbuild2: add support for `org.osbuild.systemd-logind` stage
    Add support for the `org.osbuild.systemd-logind` osbuild stage [1],
    which allows one to configure systemd-logind by creating
    configuration drop-ins.
    
    Add unit test cases for the newly added stage.
    
    [1] https://github.com/osbuild/osbuild/pull/668
    
    Signed-off-by: Tomas Hozza <thozza@redhat.com>

---
+++ internal/osbuild2/systemd_logind_stage.go
@@ -0,0 +1,46 @@
+package osbuild2
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type SystemdLogindStageOptions struct {
+	ConfigDropins map[string]SystemdLogindConfigDropin `json:"configuration_dropins,omitempty"`
+}
+
+func (SystemdLogindStageOptions) isStageOptions() {}
+
+func NewSystemdLogindStage(options *SystemdLogindStageOptions) *Stage {
+	return &Stage{
+		Type:    "org.osbuild.systemd-logind",
+		Options: options,
+	}
+}
+
+// Drop-in configuration for systemd-logind
+type SystemdLogindConfigDropin struct {
+	Login SystemdLogindConfigLoginSection `json:"Login"`
+}
+
+// 'Login' configuration section - at least one option must be specified
+type SystemdLogindConfigLoginSection struct {
+	// Configures how many virtual terminals (VTs) to allocate by default
+	// The option is optional, but zero is a valid value
+	NAutoVT *int `json:"NAutoVT,omitempty"`
+}
+
+// Unexported struct for use in SystemdLogindConfigLoginSection's MarshalJSON() to prevent recursion
+type systemdLogindConfigLoginSection struct {
+	// Configures how many virtual terminals (VTs) to allocate by default
+	// The option is optional, but zero is a valid value
+	NAutoVT *int `json:"NAutoVT,omitempty"`
+}
+
+func (s SystemdLogindConfigLoginSection) MarshalJSON() ([]byte, error) {
+	if s.NAutoVT == nil {
+		return nil, fmt.Errorf("at least one 'Login' section option must be specified")
+	}
+	loginSection := systemdLogindConfigLoginSection(s)
+	return json.Marshal(loginSection)
+}
