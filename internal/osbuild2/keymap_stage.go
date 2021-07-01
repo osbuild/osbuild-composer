006ff98025138f01e755f53f390acc6baea7ea7a
Tomas Hozza <thozza@redhat.com>
2021-07-01 13:25:18 +0200

    osbuild2: support for `x11-keymap` option in `org.osbuild.keymap`
    Add support for the newly added `x11-keymap` option in the
    `org.osbuild.keymap` osbuild stage [1]. The option allows one to
    set layouts for the X11 keyboard.
    
    Add unit test cases for the added functionality.
    
    [1] https://github.com/osbuild/osbuild/pull/693
    
    Signed-off-by: Tomas Hozza <thozza@redhat.com>

--- internal/osbuild2/keymap_stage.go
+++ internal/osbuild2/keymap_stage.go
@@ -1,7 +1,13 @@
 package osbuild2
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type KeymapStageOptions struct {
-	Keymap string `json:"keymap"`
+	Keymap    string            `json:"keymap"`
+	X11Keymap *X11KeymapOptions `json:"x11-keymap,omitempty"`
 }
 
 func (KeymapStageOptions) isStageOptions() {}
@@ -11,4 +17,21 @@ func NewKeymapStage(options *KeymapStageOptions) *Stage {
 		Type:    "org.osbuild.keymap",
 		Options: options,
 	}
+}
+
+type X11KeymapOptions struct {
+	Layouts []string `json:"layouts"`
+}
+
+// Unexported struct for use in X11KeymapOptions's MarshalJSON() to prevent recursion
+type x11KeymapOptions struct {
+	Layouts []string `json:"layouts"`
+}
+
+func (o X11KeymapOptions) MarshalJSON() ([]byte, error) {
+	if len(o.Layouts) == 0 {
+		return nil, fmt.Errorf("at least one layout must be provided for X11 keymap")
+	}
+	keymapOptions := x11KeymapOptions(o)
+	return json.Marshal(keymapOptions)
 }
