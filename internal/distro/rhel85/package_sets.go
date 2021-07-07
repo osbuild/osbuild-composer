e5013607025f3c5ab85fb53cd1a75d715756e064
Achilleas Koutsou <achilleas@koutsou.net>
2021-07-07 17:56:21 +0200

    distro/rhel85: tidy up package sets
    - Move all package set definitions to package_sets.go file.
    - Do not append image package sets to arch and distro sets; this happens
      automatically in PackageSets() method.
    - Fix architecture-specific package sets (boot and build where
      available).

--- internal/distro/rhel85/package_sets.go
+++ internal/distro/rhel85/package_sets.go
@@ -1,4 +1,3 @@
-// nolint: deadcode,unused // Helper functions for future implementations of pipelines
 package rhel85
 
 // This file defines package sets that are used by more than one image type.
@@ -8,7 +7,7 @@ import "github.com/osbuild/osbuild-composer/internal/rpmmd"
 // BUILD PACKAGE SETS
 
 // distro-wide build package set
-func buildPackageSet() rpmmd.PackageSet {
+func distroBuildPackageSet() rpmmd.PackageSet {
 	return rpmmd.PackageSet{
 		Include: []string{
 			"dnf", "dosfstools", "e2fsprogs", "glibc", "lorax-templates-generic",
@@ -19,56 +18,60 @@ func buildPackageSet() rpmmd.PackageSet {
 	}
 }
 
+// x86_64 build package set
 func x8664BuildPackageSet() rpmmd.PackageSet {
 	return rpmmd.PackageSet{
 		Include: []string{"grub2-pc"},
 	}
 }
 
-func ppc64lePackageSet() rpmmd.PackageSet {
+// ppc64le build package set
+func ppc64leBuildPackageSet() rpmmd.PackageSet {
 	return rpmmd.PackageSet{
 		Include: []string{"grub2-ppc64le", "grub2-ppc64le-modules"},
 	}
 }
 
+// common edge image build package set
 func edgeBuildPackageSet() rpmmd.PackageSet {
-	return rpmmd.PackageSet{
-		Include: []string{
-			"dnf", "dosfstools", "e2fsprogs", "grub2-pc", "policycoreutils",
-			"python36", "python3-iniparse", "qemu-img", "rpm-ostree", "systemd",
-			"tar", "xfsprogs", "xz", "selinux-policy-targeted", "genisoimage",
-			"isomd5sum", "xorriso", "syslinux", "lorax-templates-generic",
-			"lorax-templates-rhel", "syslinux-nonlinux", "squashfs-tools",
-			"grub2-pc-modules", "grub2-tools", "grub2-efi-x64", "shim-x64",
-			"efibootmgr", "grub2-tools-minimal", "grub2-tools-extra",
-			"grub2-tools-efi", "grub2-efi-x64", "grub2-efi-x64-cdboot",
-			"shim-ia32", "grub2-efi-ia32-cdboot",
-		},
-	}
-}
-
-func installerBuildPackageSet() rpmmd.PackageSet {
 	return rpmmd.PackageSet{
 		Include: []string{
 			"efibootmgr", "genisoimage", "grub2-efi-ia32-cdboot",
 			"grub2-efi-x64", "grub2-efi-x64-cdboot", "grub2-pc",
 			"grub2-pc-modules", "grub2-tools", "grub2-tools-efi",
 			"grub2-tools-extra", "grub2-tools-minimal", "isomd5sum",
-			"lorax-templates-generic", "lorax-templates-rhel", "rpm-ostree",
-			"shim-ia32", "shim-x64", "squashfs-tools", "syslinux",
-			"syslinux-nonlinux", "xorriso",
+			"rpm-ostree", "shim-ia32", "shim-x64", "squashfs-tools",
+			"syslinux", "syslinux-nonlinux", "xorriso",
+		},
+		Exclude: nil,
+	}
+}
+
+// x86_64 installer ISO build package set
+// TODO: separate into common installer and arch specific sets
+func x8664InstallerBuildPackageSet() rpmmd.PackageSet {
+	return rpmmd.PackageSet{
+		Include: []string{
+			"efibootmgr", "genisoimage", "grub2-efi-ia32-cdboot",
+			"grub2-efi-x64", "grub2-efi-x64-cdboot", "grub2-pc",
+			"grub2-pc-modules", "grub2-tools", "grub2-tools-efi",
+			"grub2-tools-extra", "grub2-tools-minimal", "isomd5sum",
+			"rpm-ostree", "shim-ia32", "shim-x64", "squashfs-tools",
+			"syslinux", "syslinux-nonlinux", "xorriso",
 		},
 	}
 }
 
 // BOOT PACKAGE SETS
 
+// x86_64 arch-specific boot package set
 func x8664BootPackageSet() rpmmd.PackageSet {
 	return rpmmd.PackageSet{
 		Include: []string{"dracut-config-generic", "grub2-pc", "grub2-efi-x64", "shim-x64"},
 	}
 }
 
+// aarch64 arch-specific boot package set
 func aarch64BootPackageSet() rpmmd.PackageSet {
 	return rpmmd.PackageSet{
 		Include: []string{
@@ -78,6 +81,7 @@ func aarch64BootPackageSet() rpmmd.PackageSet {
 	}
 }
 
+// ppc64le arch-specific boot package set
 func ppc64leBootPackageSet() rpmmd.PackageSet {
 	return rpmmd.PackageSet{
 		Include: []string{
@@ -87,6 +91,7 @@ func ppc64leBootPackageSet() rpmmd.PackageSet {
 	}
 }
 
+// s390x arch-specific boot package set
 func s390xBootPackageSet() rpmmd.PackageSet {
 	return rpmmd.PackageSet{
 		Include: []string{"dracut-config-generic", "s390utils-base"},
@@ -95,79 +100,82 @@ func s390xBootPackageSet() rpmmd.PackageSet {
 
 // OS package sets
 
-func qcow2CommonPkgSet() rpmmd.PackageSet {
+func x8664EdgeCommitPackageSet() rpmmd.PackageSet {
+	x8664set := rpmmd.PackageSet{
+		Include: []string{
+			"grub2", "grub2-efi-x64", "efibootmgr", "shim-x64",
+			"microcode_ctl", "iwl1000-firmware", "iwl100-firmware",
+			"iwl105-firmware", "iwl135-firmware", "iwl2000-firmware",
+			"iwl2030-firmware", "iwl3160-firmware", "iwl5000-firmware",
+			"iwl5150-firmware", "iwl6000-firmware", "iwl6050-firmware",
+			"iwl7260-firmware",
+		},
+		Exclude: nil,
+	}
+	return edgeCommitCommonPackageSet().Append(x8664set)
+}
+
+func aarch64EdgeCommitPackageSet() rpmmd.PackageSet {
+	aarch64set := rpmmd.PackageSet{
+		Include: []string{"grub2-efi-aa64", "efibootmgr", "shim-aa64", "iwl7260-firmware"},
+		Exclude: nil,
+	}
+	return edgeCommitCommonPackageSet().Append(aarch64set)
+}
+
+func qcow2CommonPackageSet() rpmmd.PackageSet {
 	return rpmmd.PackageSet{
 		Include: []string{
-			"@core",
-			"authselect-compat",
-			"chrony",
-			"cloud-init",
-			"cloud-utils-growpart",
-			"cockpit-system",
-			"cockpit-ws",
-			"dhcp-client",
-			"dnf",
-			"dnf-utils",
-			"dosfstools",
-			"dracut-norescue",
-			"insights-client",
-			"NetworkManager",
-			"net-tools",
-			"nfs-utils",
-			"oddjob",
-			"oddjob-mkhomedir",
-			"psmisc",
-			"python3-jsonschema",
-			"qemu-guest-agent",
-			"redhat-release",
-			"redhat-release-eula",
-			"rsync",
-			"subscription-manager-cockpit",
-			"tar",
-			"tcpdump",
-			"yum",
+			"@core", "authselect-compat", "chrony", "cloud-init",
+			"cloud-utils-growpart", "cockpit-system", "cockpit-ws",
+			"dhcp-client", "dnf", "dnf-utils", "dosfstools", "dracut-norescue",
+			"insights-client", "NetworkManager", "net-tools", "nfs-utils",
+			"oddjob", "oddjob-mkhomedir", "psmisc", "python3-jsonschema",
+			"qemu-guest-agent", "redhat-release", "redhat-release-eula",
+			"rsync", "subscription-manager-cockpit", "tar", "tcpdump", "yum",
 		},
 		Exclude: []string{
-			"aic94xx-firmware",
-			"alsa-firmware",
-			"alsa-lib",
-			"alsa-tools-firmware",
-			"biosdevname",
-			"dnf-plugin-spacewalk",
-			"dracut-config-rescue",
-			"fedora-release",
-			"fedora-repos",
-			"firewalld",
-			"fwupd",
-			"iprutils",
-			"ivtv-firmware",
-			"iwl100-firmware",
-			"iwl1000-firmware",
-			"iwl105-firmware",
-			"iwl135-firmware",
-			"iwl2000-firmware",
-			"iwl2030-firmware",
-			"iwl3160-firmware",
-			"iwl3945-firmware",
-			"iwl4965-firmware",
-			"iwl5000-firmware",
-			"iwl5150-firmware",
-			"iwl6000-firmware",
-			"iwl6000g2a-firmware",
-			"iwl6000g2b-firmware",
-			"iwl6050-firmware",
-			"iwl7260-firmware",
-			"langpacks-*",
-			"langpacks-en",
-			"langpacks-en",
-			"libertas-sd8686-firmware",
-			"libertas-sd8787-firmware",
-			"libertas-usb8388-firmware",
-			"nss",
-			"plymouth",
-			"rng-tools",
+			"aic94xx-firmware", "alsa-firmware", "alsa-lib",
+			"alsa-tools-firmware", "biosdevname", "dnf-plugin-spacewalk",
+			"dracut-config-rescue", "fedora-release", "fedora-repos",
+			"firewalld", "fwupd", "iprutils", "ivtv-firmware",
+			"iwl100-firmware", "iwl1000-firmware", "iwl105-firmware",
+			"iwl135-firmware", "iwl2000-firmware", "iwl2030-firmware",
+			"iwl3160-firmware", "iwl3945-firmware", "iwl4965-firmware",
+			"iwl5000-firmware", "iwl5150-firmware", "iwl6000-firmware",
+			"iwl6000g2a-firmware", "iwl6000g2b-firmware", "iwl6050-firmware",
+			"iwl7260-firmware", "langpacks-*", "langpacks-en", "langpacks-en",
+			"libertas-sd8686-firmware", "libertas-sd8787-firmware",
+			"libertas-usb8388-firmware", "nss", "plymouth", "rng-tools",
 			"udisks2",
 		},
+	}
+}
+
+// edge commit OS package set
+func edgeCommitCommonPackageSet() rpmmd.PackageSet {
+	return rpmmd.PackageSet{
+		Include: []string{
+			"redhat-release", "glibc", "glibc-minimal-langpack",
+			"nss-altfiles", "dracut-config-generic", "dracut-network",
+			"basesystem", "bash", "platform-python", "shadow-utils", "chrony",
+			"setup", "shadow-utils", "sudo", "systemd", "coreutils",
+			"util-linux", "curl", "vim-minimal", "rpm", "rpm-ostree", "polkit",
+			"lvm2", "cryptsetup", "pinentry", "e2fsprogs", "dosfstools",
+			"keyutils", "gnupg2", "attr", "xz", "gzip", "firewalld",
+			"iptables", "NetworkManager", "NetworkManager-wifi",
+			"NetworkManager-wwan", "wpa_supplicant", "dnsmasq", "traceroute",
+			"hostname", "iproute", "iputils", "openssh-clients", "procps-ng",
+			"rootfiles", "openssh-server", "passwd", "policycoreutils",
+			"policycoreutils-python-utils", "selinux-policy-targeted",
+			"setools-console", "less", "tar", "rsync", "fwupd", "usbguard",
+			"bash-completion", "tmux", "ima-evm-utils", "audit", "podman",
+			"container-selinux", "skopeo", "criu", "slirp4netns",
+			"fuse-overlayfs", "clevis", "clevis-dracut", "clevis-luks",
+			"greenboot", "greenboot-grub2", "greenboot-rpm-ostree-grub2",
+			"greenboot-reboot", "greenboot-status",
+		},
+		Exclude: []string{"rng-tools"},
 	}
 }
 
@@ -223,5 +231,59 @@ func installerPackageSet() rpmmd.PackageSet {
 			"xorg-x11-drivers", "xorg-x11-fonts-misc", "xorg-x11-server-utils",
 			"xorg-x11-server-Xorg", "xorg-x11-xauth", "xz",
 		},
+	}
+}
+
+func edgeInstallerPackageSet() rpmmd.PackageSet {
+	return rpmmd.PackageSet{
+		Include: []string{
+			"aajohan-comfortaa-fonts", "abattis-cantarell-fonts",
+			"alsa-firmware", "alsa-tools-firmware", "anaconda",
+			"anaconda-dracut", "anaconda-install-env-deps", "anaconda-widgets",
+			"audit", "bind-utils", "biosdevname", "bitmap-fangsongti-fonts",
+			"bzip2", "cryptsetup", "curl", "dbus-x11", "dejavu-sans-fonts",
+			"dejavu-sans-mono-fonts", "device-mapper-persistent-data",
+			"dmidecode", "dnf", "dracut-config-generic", "dracut-network",
+			"dump", "efibootmgr", "ethtool", "ftp", "gdb-gdbserver", "gdisk",
+			"gfs2-utils", "glibc-all-langpacks",
+			"google-noto-sans-cjk-ttc-fonts", "grub2-efi-ia32-cdboot",
+			"grub2-efi-x64-cdboot", "grub2-tools", "grub2-tools-efi",
+			"grub2-tools-extra", "grub2-tools-minimal", "grubby",
+			"gsettings-desktop-schemas", "hdparm", "hexedit", "hostname",
+			"initscripts", "ipmitool", "iwl1000-firmware", "iwl100-firmware",
+			"iwl105-firmware", "iwl135-firmware", "iwl2000-firmware",
+			"iwl2030-firmware", "iwl3160-firmware", "iwl3945-firmware",
+			"iwl4965-firmware", "iwl5000-firmware", "iwl5150-firmware",
+			"iwl6000-firmware", "iwl6000g2a-firmware", "iwl6000g2b-firmware",
+			"iwl6050-firmware", "iwl7260-firmware", "jomolhari-fonts",
+			"kacst-farsi-fonts", "kacst-qurn-fonts", "kbd", "kbd-misc",
+			"kdump-anaconda-addon", "kernel", "khmeros-base-fonts", "less",
+			"libblockdev-lvm-dbus", "libertas-sd8686-firmware",
+			"libertas-sd8787-firmware", "libertas-usb8388-firmware",
+			"libertas-usb8388-olpc-firmware", "libibverbs",
+			"libreport-plugin-bugzilla", "libreport-plugin-reportuploader",
+			"libreport-rhel-anaconda-bugzilla", "librsvg2", "linux-firmware",
+			"lklug-fonts", "lohit-assamese-fonts", "lohit-bengali-fonts",
+			"lohit-devanagari-fonts", "lohit-gujarati-fonts",
+			"lohit-gurmukhi-fonts", "lohit-kannada-fonts", "lohit-odia-fonts",
+			"lohit-tamil-fonts", "lohit-telugu-fonts", "lsof", "madan-fonts",
+			"memtest86+", "metacity", "mtr", "mt-st", "net-tools", "nfs-utils",
+			"nmap-ncat", "nm-connection-editor", "nss-tools",
+			"openssh-clients", "openssh-server", "oscap-anaconda-addon",
+			"ostree", "pciutils", "perl-interpreter", "pigz", "plymouth",
+			"prefixdevname", "python3-pyatspi", "rdma-core",
+			"redhat-release-eula", "rng-tools", "rpcbind", "rpm-ostree",
+			"rsync", "rsyslog", "selinux-policy-targeted", "sg3_utils",
+			"shim-ia32", "shim-x64", "sil-abyssinica-fonts",
+			"sil-padauk-fonts", "sil-scheherazade-fonts", "smartmontools",
+			"smc-meera-fonts", "spice-vdagent", "strace", "syslinux",
+			"systemd", "system-storage-manager", "tar",
+			"thai-scalable-waree-fonts", "tigervnc-server-minimal",
+			"tigervnc-server-module", "udisks2", "udisks2-iscsi", "usbutils",
+			"vim-minimal", "volume_key", "wget", "xfsdump", "xfsprogs",
+			"xorg-x11-drivers", "xorg-x11-fonts-misc", "xorg-x11-server-utils",
+			"xorg-x11-server-Xorg", "xorg-x11-xauth", "xz",
+		},
+		Exclude: nil,
 	}
 }
