0b962c9185a6f137e772a61f6aa7a10c990f08f7
Jakub Rusz <jrusz@redhat.com>
2021-07-09 13:40:34 +0200

    ci: add slack notfications
    This sends a message to a slack channel if a scheduled pipeline is
    ran and reports either failure or success.

---
+++ schutzbot/slack_notification.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eux
+
+COMPOSE_ID=$(cat COMPOSE_ID)
+MESSAGE="\"Nightly pipeline execution on *$COMPOSE_ID* finished with status *$1* $2 \n Link to results: $CI_PIPELINE_URL \""
+
+curl \
+    -X POST \
+    -H 'Content-type: application/json' \
+    --data '{"text": "test", "blocks": [ { "type": "section", "text": {"type": "mrkdwn", "text":'"$MESSAGE"'}}]}' \
+    "$SLACK_WEBHOOK_URL"
