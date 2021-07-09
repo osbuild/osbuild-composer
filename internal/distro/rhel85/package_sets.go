ba17dea401f10ab4818656e9612d25a20f47e37c
Achilleas Koutsou <achilleas@koutsou.net>
2021-07-09 15:11:23 +0200

    distro/rhel85: update ami package set
    Based on http://git.app.eng.bos.redhat.com/git/spin-kickstarts.git/tree/rhel8/rhel-8.5-ec2.ks

--- internal/distro/rhel85/package_sets.go
+++ internal/distro/rhel85/package_sets.go
@@ -174,26 +174,26 @@ func openstackCommonPackageSet() rpmmd.PackageSet {
 func amiCommonPackageSet() rpmmd.PackageSet {
 	return rpmmd.PackageSet{
 		Include: []string{
-			"checkpolicy", "chrony", "cloud-init", "cloud-init",
-			"cloud-utils-growpart", "@core", "dhcp-client", "gdisk",
-			"insights-client", "langpacks-en", "net-tools", "NetworkManager",
-			"redhat-release", "redhat-release-eula", "rsync",
-			"selinux-policy-targeted", "tar", "yum-utils",
+			"@core", "cloud-init", "cloud-utils-growpart", "clyum-utils",
+			"dracut-config-generic", "dracut-norescue", "gdisk", "grub2",
+			"insights-client", "NetworkManager", "NetworkManager-cloud-setup",
+			"redhat-release", "redhat-release-eula", "rsync", "tar",
 
 			// TODO this doesn't exist in BaseOS or AppStream
 			// "rh-amazon-rhui-client",
+
 		},
 		Exclude: []string{
 			"aic94xx-firmware", "alsa-firmware", "alsa-lib",
-			"alsa-tools-firmware", "biosdevname", "dracut-config-rescue",
-			"firewalld", "iprutils", "ivtv-firmware", "iwl1000-firmware",
-			"iwl100-firmware", "iwl105-firmware", "iwl135-firmware",
-			"iwl2000-firmware", "iwl2030-firmware", "iwl3160-firmware",
-			"iwl3945-firmware", "iwl4965-firmware", "iwl5000-firmware",
-			"iwl5150-firmware", "iwl6000-firmware", "iwl6000g2a-firmware",
-			"iwl6000g2b-firmware", "iwl6050-firmware", "iwl7260-firmware",
-			"libertas-sd8686-firmware", "libertas-sd8787-firmware",
-			"libertas-usb8388-firmware", "plymouth", "rng-tools",
+			"alsa-tools-firmware", "biosdevname", "iprutils", "ivtv-firmware",
+			"iwl1000-firmware", "iwl100-firmware", "iwl105-firmware",
+			"iwl135-firmware", "iwl2000-firmware", "iwl2030-firmware",
+			"iwl3160-firmware", "iwl3945-firmware", "iwl4965-firmware",
+			"iwl5000-firmware", "iwl5150-firmware", "iwl6000-firmware",
+			"iwl6000g2a-firmware", "iwl6000g2b-firmware", "iwl6050-firmware",
+			"iwl7260-firmware", "libertas-sd8686-firmware",
+			"libertas-sd8787-firmware", "libertas-usb8388-firmware",
+			"plymouth",
 		},
 	}
 }
