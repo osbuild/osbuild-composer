d264c6d9c8150c8c19b7e9572067193b940280b2
Jakub Rusz <jrusz@redhat.com>
2021-07-09 13:40:34 +0200

    ci: add slack notfications
    This sends a message to a slack channel if a scheduled pipeline is
    ran and reports either failure or success.

--- tools/define-compose-url.sh
+++ tools/define-compose-url.sh
@@ -8,7 +8,7 @@ COMPOSE_ID=$(cat COMPOSE_ID)
 COMPOSE_URL="${COMPOSE_URL:-http://download.devel.redhat.com/rhel-8/nightly/RHEL-8/$COMPOSE_ID}"
 
 # in case COMPOSE_URL was defined from the outside refresh COMPOSE_ID file,
-# used for telegram messages in case of success/failure
+# used for slack messages in case of success/failure
 curl -L "$COMPOSE_URL/COMPOSE_ID" > COMPOSE_ID
 
 # Make sure the compose URL really exists
