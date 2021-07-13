daa93569058068898c36745b0db37d7395b07738
Achilleas Koutsou <achilleas@koutsou.net>
2021-07-13 14:18:14 +0200

    test: separate regression from base tests
    - Rewrote regression.sh to be like base tests with list of regression
      test scripts to be run and fail counts
    - Separate regression tests in CI to have better control of distro
      runners

--- test/cases/regression.sh
+++ test/cases/regression.sh
@@ -1,20 +1,61 @@
 #!/bin/bash
 set -euo pipefail
 
-# Get OS data.
-source /etc/os-release
+TESTS_PATH=/usr/libexec/tests/osbuild-composer/
+mkdir --parents /tmp/logs
+LOGS_DIRECTORY=$(mktemp --directory --tmpdir=/tmp/logs)
+
+PASSED_TESTS=()
+FAILED_TESTS=()
+
+TEST_CASES=(
+    "regression-excluded-dependency.sh"
+    "regression-include-excluded-packages.sh"
+)
+
+# Print out a nice test divider so we know when tests stop and start.
+test_divider () {
+    printf "%0.s-" {1..78} && echo
+}
+
+# Run a test case and store the result as passed or failed.
+run_test_case () {
+    TEST_NAME=$(basename "$1")
+    echo
+    test_divider
+    echo "🏃🏻 Running test: ${TEST_NAME}"
+    test_divider
+
+    if sudo "${1}" -test.v | tee "${LOGS_DIRECTORY}"/"${TEST_NAME}".log; then
+        PASSED_TESTS+=("$TEST_NAME")
+    else
+        FAILED_TESTS+=("$TEST_NAME")
+    fi
+
+    test_divider
+    echo
+}
+
 
 # Provision the software under tet.
 /usr/libexec/osbuild-composer-test/provision.sh
 
-# Set os-variant and boot location used by virt-install.
-case "${ID}" in
-    "fedora")
-        echo "No regression test for Fedora";;
-    "rhel")
-        /usr/libexec/tests/osbuild-composer/regression-include-excluded-packages.sh;;
-    "centos")
-        /usr/libexec/tests/osbuild-composer/regression-include-excluded-packages.sh;;
-    *)
-        echo "unsupported distro: ${ID}-${VERSION_ID}"
-esac
+# Run each test case.
+for TEST_CASE in "${TEST_CASES[@]}"; do
+    run_test_case ${TESTS_PATH}/"$TEST_CASE"
+done
+
+# Print a report of the test results.
+test_divider
+echo "😃 Passed tests:" "${PASSED_TESTS[@]}"
+echo "☹ Failed tests:" "${FAILED_TESTS[@]}"
+test_divider
+
+# Exit with a failure if tests were executed and any of them failed.
+if [ ${#PASSED_TESTS[@]} -gt 0 ] && [ ${#FAILED_TESTS[@]} -eq 0 ]; then
+    echo "🎉 All tests passed."
+    exit 0
+else
+    echo "🔥 One or more tests failed."
+    exit 1
+fi
