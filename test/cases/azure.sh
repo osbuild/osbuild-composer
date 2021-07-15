ab3234260a79b79e1963a83d5506c545cb7a7d7a
Jakub Rusz <jrusz@redhat.com>
2021-07-15 11:53:57 +0200

    tests: fix wrong default value in azure.sh test
    It's not possible to create vm with the single quotes in the name.
    Removing it to enable running locally.

--- test/cases/azure.sh
+++ test/cases/azure.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 source /etc/os-release
 DISTRO_CODE="${DISTRO_CODE:-${ID}_${VERSION_ID//./}}"
-BRANCH_NAME="${CI_COMMIT_BRANCH:-'local'}"
+BRANCH_NAME="${CI_COMMIT_BRANCH:-local}"
 BUILD_ID="${CI_BUILD_ID:-$(uuidgen)}"
 HYPER_V_GEN="${HYPER_V_GEN:-V1}"
 
