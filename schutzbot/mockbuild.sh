0ef62711cab6bc8dca836e0853ab5f4ec162bfb8
Ondřej Budai <ondrej@budai.cz>
2021-07-20 15:14:59 +0200

    mockbuild: register the instance at the beginning
    So we can check if the machine is subscribed in the script.
    
    Signed-off-by: Ondřej Budai <ondrej@budai.cz>

--- schutzbot/mockbuild.sh
+++ schutzbot/mockbuild.sh
@@ -10,6 +10,13 @@ function greenprint {
 source /etc/os-release
 ARCH=$(uname -m)
 
+# Register RHEL if we are provided with a registration script.
+if [[ $ID == "rhel" && $VERSION_ID == "8.3" && -n "${RHN_REGISTRATION_SCRIPT:-}" ]] && ! sudo subscription-manager status; then
+    greenprint "🪙 Registering RHEL instance"
+    sudo chmod +x "$RHN_REGISTRATION_SCRIPT"
+    sudo "$RHN_REGISTRATION_SCRIPT"
+fi
+
 # Mock configuration file to use for building RPMs.
 MOCK_CONFIG="${ID}-${VERSION_ID%.*}-$(uname -m)"
 
@@ -49,13 +56,6 @@ if [[ $ID == rhel || $ID == centos ]] && ! rpm -q epel-release; then
     curl -Ls --retry 5 --output /tmp/epel.rpm \
         https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
     sudo rpm -Uvh /tmp/epel.rpm
-fi
-
-# Register RHEL if we are provided with a registration script.
-if [[ $ID == "rhel" && $VERSION_ID == "8.3" && -n "${RHN_REGISTRATION_SCRIPT:-}" ]] && ! sudo subscription-manager status; then
-    greenprint "🪙 Registering RHEL instance"
-    sudo chmod +x "$RHN_REGISTRATION_SCRIPT"
-    sudo "$RHN_REGISTRATION_SCRIPT"
 fi
 
 # Install requirements for building RPMs in mock.
