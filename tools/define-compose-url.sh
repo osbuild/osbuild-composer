3f7e815a80be2cdaa00964e88da951d4c551a0e1
Ondřej Budai <ondrej@budai.cz>
2021-07-27 09:08:35 +0200

    tests: define compose url also for RHEL 9.0
    Signed-off-by: Ondřej Budai <ondrej@budai.cz>

--- tools/define-compose-url.sh
+++ tools/define-compose-url.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 set -euo pipefail
+source /etc/os-release
 
-curl -L http://download.devel.redhat.com/rhel-8/nightly/RHEL-8/latest-finished-RHEL-8.5/COMPOSE_ID > COMPOSE_ID
-COMPOSE_ID=$(cat COMPOSE_ID)
+# This isn't needed when not running on RHEL
+if [[ $ID != rhel ]]; then
+  exit 0
+fi
 
-# default to a nightly tree but respect values passed from ENV so we can test rel-eng composes as well
-COMPOSE_URL="${COMPOSE_URL:-http://download.devel.redhat.com/rhel-8/nightly/RHEL-8/$COMPOSE_ID}"
+if [[ $ID == rhel && ${VERSION_ID%.*} == 8 ]]; then
+  COMPOSE_ID=$(curl -L http://download.devel.redhat.com/rhel-8/nightly/RHEL-8/latest-finished-RHEL-8.5/COMPOSE_ID)
+
+  # default to a nightly tree but respect values passed from ENV so we can test rel-eng composes as well
+  COMPOSE_URL="${COMPOSE_URL:-http://download.devel.redhat.com/rhel-8/nightly/RHEL-8/$COMPOSE_ID}"
+
+elif [[ $ID == rhel && ${VERSION_ID%.*} == 9 ]]; then
+  COMPOSE_ID=$(curl -L http://download.devel.redhat.com/rhel-9/nightly/RHEL-9-Beta/latest-RHEL-9.0/COMPOSE_ID)
+
+  # default to a nightly tree but respect values passed from ENV so we can test rel-eng composes as well
+  COMPOSE_URL="${COMPOSE_URL:-http://download.devel.redhat.com/rhel-9/nightly/RHEL-9-Beta/$COMPOSE_ID}"
+fi
 
 # in case COMPOSE_URL was defined from the outside refresh COMPOSE_ID file,
 # used for slack messages in case of success/failure
