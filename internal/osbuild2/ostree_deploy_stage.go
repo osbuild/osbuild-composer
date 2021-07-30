68a35d1048ea786c6d4c559487ad42971f1116d5
Antonio Murdaca <runcom@linux.com>
2021-07-30 11:30:05 +0200

    internal/osbuild2: add stages for raw images and grub iso
    Signed-off-by: Antonio Murdaca <runcom@linux.com>

---
+++ internal/osbuild2/ostree_deploy_stage.go
@@ -0,0 +1,28 @@
+package osbuild2
+
+// Options for the org.osbuild.ostree.deploy stage.
+type OSTreeDeployStageOptions struct {
+	OsName string `json:"osname"`
+
+	Ref string `json:"ref"`
+
+	Mounts []string `json:"mounts,omitempty"`
+
+	Rootfs Rootfs `json:"rootfs"`
+
+	KernelOpts []string `json:"kernel_opts,omitempty"`
+}
+
+type Rootfs struct {
+	Label string `json:"label"`
+}
+
+func (OSTreeDeployStageOptions) isStageOptions() {}
+
+// A new org.osbuild.ostree.init stage to create an OSTree repository
+func NewOSTreeDeployStage(options *OSTreeDeployStageOptions) *Stage {
+	return &Stage{
+		Type:    "org.osbuild.ostree.deploy",
+		Options: options,
+	}
+}
---
+++ internal/osbuild2/ostree_fillvar_stage.go
@@ -0,0 +1,22 @@
+package osbuild2
+
+// Options for the org.osbuild.ostree.fillvar stage.
+type OSTreeFillvarStageOptions struct {
+	Deployment OSTreeDeployment `json:"deployment"`
+}
+
+type OSTreeDeployment struct {
+	OsName string `json:"osname"`
+
+	Ref string `json:"ref"`
+}
+
+func (OSTreeFillvarStageOptions) isStageOptions() {}
+
+// A new org.osbuild.ostree.init stage to create an OSTree repository
+func NewOSTreeFillvarStage(options *OSTreeFillvarStageOptions) *Stage {
+	return &Stage{
+		Type:    "org.osbuild.ostree.fillvar",
+		Options: options,
+	}
+}
