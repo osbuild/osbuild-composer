0ed4f057c2cf0b392c7bb9dcd7cf843d28799cf7
Achilleas Koutsou <achilleas@koutsou.net>
2021-08-06 01:44:19 +0200

    osbuild2: zipl install stage
    New stage: org.osbuild.zipl.inst
    
    Signed-off-by: Achilleas Koutsou <achilleas@koutsou.net>

---
+++ internal/osbuild2/zipl_inst_stage.go
@@ -0,0 +1,32 @@
+package osbuild2
+
+// Install the Z Initial Program Loader
+
+type ZiplInstStageOptions struct {
+	Kernel string `json:"kernel"`
+
+	// The offset of the partition containing /boot
+	Location uint64 `json:"location"`
+
+	SectorSize *uint64 `json:"sector-size,omitempty"`
+}
+
+func (ZiplInstStageOptions) isStageOptions() {}
+
+type ZiplInstStageDevices map[string]Device
+
+func (ZiplInstStageDevices) isStageDevices() {}
+
+type ZiplInstStageMounts []Mount
+
+func (ZiplInstStageMounts) isStageMounts() {}
+
+// Return a new zipl.inst stage. A device needs to be specified as 'disk' and root mountpoint must be provided
+func NewZiplInstStage(options *ZiplInstStageOptions, devices *CopyStageDevices, mounts *CopyStageMounts) *Stage {
+	return &Stage{
+		Type:    "org.osbuild.zipl.inst",
+		Options: options,
+		Devices: devices,
+		Mounts:  mounts,
+	}
+}
