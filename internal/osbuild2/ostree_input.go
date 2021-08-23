e28755afc0a17286fb52ac73ae716ec18bfa6fc7
Christian Kellner <christian@kellner.me>
2021-08-23 22:07:19 +0000

    osbuild2: new org.osbuild.ostree.passwd stage
    Add support for the `org.osbuild.ostree.passwd` and with it also
    support for the `org.osbuild.ostree.checkout` input.
    This stage can be used to pre-load the user and groups database
    from an existing commit to ensure that uids/gids are stable.

--- internal/osbuild2/ostree_input.go
+++ internal/osbuild2/ostree_input.go
@@ -13,3 +13,30 @@ func NewOSTreeInput() *OSTreeInput {
 	input.Origin = "org.osbuild.source"
 	return input
 }
+
+// Inputs of type org.osbuild.ostree.checkout
+type OSTreeCheckoutInput struct {
+	inputCommon
+	References OSTreeCheckoutReferences `json:"references"`
+}
+
+func (OSTreeCheckoutInput) isStageInput() {}
+
+type OSTreeCheckoutReferences []string
+
+func (OSTreeCheckoutReferences) isReferences() {}
+
+// NewOSTreeCommitsInput creates a new OSTreeCommitsInputs
+// where `origin` is either "org.osbuild.source" or "org.osbuild.pipeline
+// `name` is the id of the commit, i.e. its digest or the pipeline name that
+// produced it)
+func NewOSTreeCheckoutInput(origin, name string) *OSTreeCheckoutInput {
+	input := new(OSTreeCheckoutInput)
+	input.Type = "org.osbuild.ostree.checkout"
+	input.Origin = origin
+
+	inputRefs := make([]string, 1)
+	inputRefs[0] = name
+	input.References = inputRefs
+	return input
+}
