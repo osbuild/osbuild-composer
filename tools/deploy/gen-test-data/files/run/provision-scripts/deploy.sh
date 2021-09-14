94f93a5df2c364a7c138796362a13309c2ee724f
Tomas Hozza <thozza@redhat.com>
2021-09-14 19:54:04 +0200

    generate-all-test-cases: don't use cloud-init to install RPMs
    Previously cloud-init was used to install necessary packages on the
    runner. This would not be practical in the future with other types of
    runners, which would not use cloud-init. Install all necessary RPMs by
    directly running DNF command on the runner.
    
    Signed-off-by: Tomas Hozza <thozza@redhat.com>

--- tools/deploy/gen-test-data/files/run/provision-scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-dnf -y install  osbuild \
-                osbuild-selinux \
-                osbuild-ostree \
-                osbuild-composer \
-                golang
