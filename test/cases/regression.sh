85147a33457e8f9c275b177c380891ded4db02cf
Juan Abia <jabia@redhat.com>
2021-10-29 10:25:14 +0200

    debugging  purposes
    find out the root cause of the failed test

--- test/cases/regression.sh
+++ test/cases/regression.sh
@@ -14,7 +14,7 @@ TEST_CASES=(
     "regression-excluded-dependency.sh"
     "regression-include-excluded-packages.sh"
 # see https://github.com/osbuild/osbuild-composer/issues/1834
-#    "regression-composer-works-behind-satellite.sh"
+    "regression-composer-works-behind-satellite.sh"
 )
 
 # Print out a nice test divider so we know when tests stop and start.
