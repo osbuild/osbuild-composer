82ee9661a9f55aa414e5264cd6fe898bd2471e5e
Thomas Lavocat <tlavocat@redhat.com>
2021-11-05 17:38:44 +0100

    CI: Journal-log is accessible and encrypted

--- schutzbot/save_journal.sh
+++ schutzbot/save_journal.sh
@@ -2,3 +2,8 @@
 
 # use tee, otherwise shellcheck complains
 sudo journalctl --boot | tee journal-log >/dev/null
+
+# As it might contain sensitive information and is important for debugging
+# purposes, encrypt journal-log with a symmetric passphrase.
+gpg --batch --yes --passphrase "$GPG_SYMMETRIC_PASSPHRASE" -o journal-log.gpg --symmetric journal-log
+rm journal-log
