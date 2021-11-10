2d422add7e68809c8e3fefbe7c63ebf0548005d6
Martin Sehnoutka <msehnout@redhat.com>
2021-11-10 13:20:27 +0100

    osbuild2: new stage pwquality conf
    This stage was introduced in osbuild 41. Add support into
    osbuild-composer and a test using test data from osbuild repo.

--- internal/osbuild2/stage_test.go
+++ internal/osbuild2/stage_test.go
@@ -637,6 +637,16 @@ func TestStage_UnmarshalJSON(t *testing.T) {
 				data: []byte(`{"type":"org.osbuild.users","options":{"users":null}}`),
 			},
 		},
+		{
+			name: "pwquality.conf",
+			fields: fields{
+				Type:    "org.osbuild.pwquality.conf",
+				Options: &PwqualityConfStageOptions{},
+			},
+			args: args{
+				data: []byte(`{"type":"org.osbuild.pwquality.conf","options":{"config":{}}}`),
+			},
+		},
 	}
 	for idx, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
