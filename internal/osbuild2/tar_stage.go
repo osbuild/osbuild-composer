ef366a91771768d4d82f236066ba4d607f8f5cff
Tomas Hozza <thozza@redhat.com>
2021-12-21 14:10:11 +0100

    osbuild2: support 'format' and 'root-node' Tar stage options
    Bring the Tar stage implementation on par with the current osbuild
    schema. Specifically add the 'format' and 'root-node' options to the
    stage options structure.
    
    Add stage options validation along with appropriate unit tests.
    
    Signed-off-by: Tomas Hozza <thozza@redhat.com>

--- internal/osbuild2/tar_stage.go
+++ internal/osbuild2/tar_stage.go
@@ -1,8 +1,32 @@
 package osbuild2
 
+import "fmt"
+
+type TarArchiveFormat string
+
+// valid values for the 'format' Tar stage option
+const (
+	TarArchiveFormatGnu    TarArchiveFormat = "gnu"
+	TarArchiveFormatOldgnu TarArchiveFormat = "oldngu"
+	TarArchiveFormatPosix  TarArchiveFormat = "posix"
+	TarArchiveFormatUstar  TarArchiveFormat = "ustar"
+	TarArchiveFormatV7     TarArchiveFormat = "v7"
+)
+
+type TarRootNode string
+
+// valid values for the 'root-node' Tar stage option
+const (
+	TarRootNodeInclude TarRootNode = "include"
+	TarRootNodeOmit    TarRootNode = "omit"
+)
+
 type TarStageOptions struct {
 	// Filename for tar archive
 	Filename string `json:"filename"`
+
+	// Archive format to use
+	Format TarArchiveFormat `json:"format,omitempty"`
 
 	// Enable support for POSIX ACLs
 	ACLs bool `json:"acls,omitempty"`
@@ -12,9 +36,53 @@ type TarStageOptions struct {
 
 	// Enable support for extended attributes
 	Xattrs bool `json:"xattrs,omitempty"`
+
+	// How to handle the root node: include or omit
+	RootNode TarRootNode `json:"root-node,omitempty"`
 }
 
 func (TarStageOptions) isStageOptions() {}
+
+func (o TarStageOptions) validate() error {
+	if o.Format != "" {
+		allowedArchiveFormatValues := []TarArchiveFormat{
+			TarArchiveFormatGnu,
+			TarArchiveFormatOldgnu,
+			TarArchiveFormatPosix,
+			TarArchiveFormatUstar,
+			TarArchiveFormatV7,
+		}
+		valid := false
+		for _, value := range allowedArchiveFormatValues {
+			if o.Format == value {
+				valid = true
+				break
+			}
+		}
+		if !valid {
+			return fmt.Errorf("'format' option does not allow %q as a value", o.Format)
+		}
+	}
+
+	if o.RootNode != "" {
+		allowedRootNodeValues := []TarRootNode{
+			TarRootNodeInclude,
+			TarRootNodeOmit,
+		}
+		valid := false
+		for _, value := range allowedRootNodeValues {
+			if o.RootNode == value {
+				valid = true
+				break
+			}
+		}
+		if !valid {
+			return fmt.Errorf("'root-node' option does not allow %q as a value", o.RootNode)
+		}
+	}
+
+	return nil
+}
 
 type TarStageInput struct {
 	inputCommon
@@ -36,6 +104,10 @@ func (TarStageReferences) isReferences() {}
 // Assembles a tree into a tar archive. Compression is determined by the suffix
 // (i.e., --auto-compress is used).
 func NewTarStage(options *TarStageOptions, inputs *TarStageInputs) *Stage {
+	if err := options.validate(); err != nil {
+		panic(err)
+	}
+
 	return &Stage{
 		Type:    "org.osbuild.tar",
 		Options: options,
