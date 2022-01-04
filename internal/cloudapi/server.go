d967790ea5e9f4dd40f4ec95ae19d3bb595bafc7
Ondřej Budai <ondrej@budai.cz>
2022-01-04 13:14:48 +0100

    api/cloud: drop v1 API
    It's deprecated and not used anywhere, let's just drop it.
    
    Signed-off-by: Ondřej Budai <ondrej@budai.cz>

--- internal/cloudapi/server.go
+++ internal/cloudapi/server.go
@@ -7,25 +7,18 @@ import (
 	"github.com/osbuild/osbuild-composer/internal/rpmmd"
 	"github.com/osbuild/osbuild-composer/internal/worker"
 
-	v1 "github.com/osbuild/osbuild-composer/internal/cloudapi/v1"
 	v2 "github.com/osbuild/osbuild-composer/internal/cloudapi/v2"
 )
 
 type Server struct {
-	v1 *v1.Server
 	v2 *v2.Server
 }
 
 func NewServer(workers *worker.Server, rpmMetadata rpmmd.RPMMD, distros *distroregistry.Registry, awsBucket string) *Server {
 	server := &Server{
-		v1: v1.NewServer(workers, rpmMetadata, distros),
 		v2: v2.NewServer(workers, rpmMetadata, distros, awsBucket),
 	}
 	return server
-}
-
-func (server *Server) V1(path string) http.Handler {
-	return server.v1.Handler(path)
 }
 
 func (server *Server) V2(path string) http.Handler {
