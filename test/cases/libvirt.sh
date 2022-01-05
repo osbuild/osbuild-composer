79941a6d043b1dede8f6c506c54199b2e81e6604
Achilleas Koutsou <achilleas@koutsou.net>
2022-01-05 17:29:08 +0100

    tests/libvirt: test uefi on cs9

--- test/cases/libvirt.sh
+++ test/cases/libvirt.sh
@@ -12,8 +12,8 @@ source /usr/libexec/osbuild-composer-test/set-env-variables.sh
 
 /usr/libexec/osbuild-composer-test/libvirt_test.sh openstack
 
-# RHEL 8.4 and Centos Stream 8 images also supports uefi, check that
-if [[ "$DISTRO_CODE" == "rhel-84" || "$DISTRO_CODE" == "rhel-85" || "$DISTRO_CODE" == "centos-8" || "$DISTRO_CODE" == "rhel-90" ]]; then
+# RHEL 8.4+ and Centos Stream 8+ images also supports uefi, check that
+if [[ "$DISTRO_CODE" == "rhel-84" || "$DISTRO_CODE" == "rhel-85" || "$DISTRO_CODE" == "centos-8" || "$DISTRO_CODE" == "rhel-90" || "$DISTRO_CODE" == "centos-9" ]]; then
   echo "🐄 Booting qcow2 image in UEFI mode on RHEL/Centos Stream"
   /usr/libexec/osbuild-composer-test/libvirt_test.sh qcow2 uefi
 fi
