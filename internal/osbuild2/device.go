de6b18019746052dd36a722a42a60a649dda7a00
Achilleas Koutsou <achilleas@koutsou.net>
2022-02-08 22:49:30 +0100

    osbuild2/device: add Parent field to Device
    osbuild, starting with version 31, supports for nesting of devices,
    i.e. any device can have a parent device. See osbuild commit 45d0594
    
    Co-Authored-By: Christian Kellner <christian@kellner.me>

--- internal/osbuild2/device.go
+++ internal/osbuild2/device.go
@@ -4,6 +4,7 @@ type Devices map[string]Device
 
 type Device struct {
 	Type    string        `json:"type"`
+	Parent  string        `json:"parent,omitempty"`
 	Options DeviceOptions `json:"options,omitempty"`
 }
 
