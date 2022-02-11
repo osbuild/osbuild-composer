54a4616a7db78cc28b31a145deae310130a4bed6
Christian Kellner <christian@kellner.me>
2022-02-11 12:05:46 +0000

    distro: remove unused sfdiskStageOptions
    This was replaced by the previous switch to `GenImage*Stages`.
    
    Co-Authored-By: Achilleas Koutsou <achilleas@koutsou.net>

--- internal/distro/rhel90beta/stage_options.go
+++ internal/distro/rhel90beta/stage_options.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/osbuild/osbuild-composer/internal/blueprint"
 	"github.com/osbuild/osbuild-composer/internal/crypt"
-	"github.com/osbuild/osbuild-composer/internal/disk"
 	osbuild "github.com/osbuild/osbuild-composer/internal/osbuild2"
 	"github.com/osbuild/osbuild-composer/internal/rpmmd"
 )
@@ -304,28 +303,6 @@ func xorrisofsStageOptions(filename string, arch string) *osbuild.XorrisofsStage
 		EFI:          "images/efiboot.img",
 		IsohybridMBR: "/usr/share/syslinux/isohdpfx.bin",
 	}
-}
-
-// sfdiskStageOptions creates the options and devices properties for an
-// org.osbuild.sfdisk stage based on a partition table description
-func sfdiskStageOptions(pt *disk.PartitionTable) *osbuild.SfdiskStageOptions {
-	partitions := make([]osbuild.Partition, len(pt.Partitions))
-	for idx, p := range pt.Partitions {
-		partitions[idx] = osbuild.Partition{
-			Bootable: p.Bootable,
-			Start:    pt.BytesToSectors(p.Start),
-			Size:     pt.BytesToSectors(p.Size),
-			Type:     p.Type,
-			UUID:     p.UUID,
-		}
-	}
-	stageOptions := &osbuild.SfdiskStageOptions{
-		Label:      pt.Type,
-		UUID:       pt.UUID,
-		Partitions: partitions,
-	}
-
-	return stageOptions
 }
 
 func qemuStageOptions(filename, format, compat string) *osbuild.QEMUStageOptions {
