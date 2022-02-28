8459b0463247edc4a1ede96a9c1bb2e923c64f66
Achilleas Koutsou <achilleas@koutsou.net>
2022-02-28 22:19:28 +0100

    obsuild2: deduplicate *kickstartStageOptions()
    Use single NewKickstartStageOptions() and replace image-type-specific
    implementations from each distro.
    
    (cherry picked from commit cb186df208d80074ff8db9e4cfa8bf42b5a3937d)

--- internal/distro/rhel90/stage_options.go
+++ internal/distro/rhel90/stage_options.go
@@ -189,27 +189,6 @@ func dracutStageOptions(kernelVer, arch string, additionalModules []string) *osb
 	}
 }
 
-func tarKickstartStageOptions(tarURL string) *osbuild.KickstartStageOptions {
-	return &osbuild.KickstartStageOptions{
-		Path: kspath,
-		LiveIMG: &osbuild.LiveIMG{
-			URL: tarURL,
-		},
-	}
-}
-
-func ostreeKickstartStageOptions(ostreeURL, ostreeRef string) *osbuild.KickstartStageOptions {
-	return &osbuild.KickstartStageOptions{
-		Path: kspath,
-		OSTree: &osbuild.OSTreeOptions{
-			OSName: "rhel",
-			URL:    ostreeURL,
-			Ref:    ostreeRef,
-			GPG:    false,
-		},
-	}
-}
-
 func bootISOMonoStageOptions(kernelVer, arch, vendor, product, osVersion, isolabel string) *osbuild.BootISOMonoStageOptions {
 	comprOptions := new(osbuild.FSCompressionOptions)
 	if bcj := osbuild.BCJOption(arch); bcj != "" {
