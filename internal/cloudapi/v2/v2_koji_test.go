7b7a97019dd53073c0372065d69072504ba5c585
Ondřej Budai <ondrej@budai.cz>
2022-03-07 11:10:52 +0100

    cloudapi/test: add option to enable JWT
    Signed-off-by: Ondřej Budai <ondrej@budai.cz>

--- internal/cloudapi/v2/v2_koji_test.go
+++ internal/cloudapi/v2/v2_koji_test.go
@@ -31,7 +31,7 @@ func TestKojiCompose(t *testing.T) {
 	dir, err := ioutil.TempDir("", "osbuild-composer-test-api-v2-")
 	require.NoError(t, err)
 	defer os.RemoveAll(dir)
-	kojiServer, workerServer, cancel := newV2Server(t, dir, []string{""})
+	kojiServer, workerServer, cancel := newV2Server(t, dir, []string{""}, false)
 	handler := kojiServer.Handler("/api/image-builder-composer/v2")
 	workerHandler := workerServer.Handler()
 	defer cancel()
@@ -464,7 +464,7 @@ func TestKojiRequest(t *testing.T) {
 	dir, err := ioutil.TempDir("", "osbuild-composer-test-api-v2-")
 	require.NoError(t, err)
 	defer os.RemoveAll(dir)
-	server, _, cancel := newV2Server(t, dir, []string{""})
+	server, _, cancel := newV2Server(t, dir, []string{""}, false)
 	handler := server.Handler("/api/image-builder-composer/v2")
 	defer cancel()
 
@@ -496,7 +496,7 @@ func TestKojiJobTypeValidation(t *testing.T) {
 	dir, err := ioutil.TempDir("", "osbuild-composer-test-api-v2-")
 	require.NoError(t, err)
 	defer os.RemoveAll(dir)
-	server, workers, cancel := newV2Server(t, dir, []string{""})
+	server, workers, cancel := newV2Server(t, dir, []string{""}, false)
 	handler := server.Handler("/api/image-builder-composer/v2")
 	defer cancel()
 
