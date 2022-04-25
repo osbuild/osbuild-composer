5d62dec25e38ee2fc9b05074a25a74a8f5f4020d
Tomas Hozza <thozza@redhat.com>
2022-04-25 09:53:36 +0200

    kojiapi: use `DepsolvePackageSets` instead of `Depsolve`

--- internal/kojiapi/server.go
+++ internal/kojiapi/server.go
@@ -123,13 +123,17 @@ func (h *apiHandlers) PostCompose(ctx echo.Context) error {
 		}
 
 		packageSets := imageType.PackageSets(*bp)
-		packageSpecSets := make(map[string][]rpmmd.PackageSpec)
-		for name, packages := range packageSets {
-			packageSpecs, _, err := h.server.rpmMetadata.Depsolve(packages, repositories, d.ModulePlatformID(), arch.Name(), d.Releasever())
-			if err != nil {
-				return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Failed to depsolve base base packages for %s/%s/%s: %s", ir.ImageType, ir.Architecture, request.Distribution, err))
-			}
-			packageSpecSets[name] = packageSpecs
+		packageSpecSets, err := h.server.rpmMetadata.DepsolvePackageSets(
+			imageType.PackageSetsChains(),
+			packageSets,
+			repositories,
+			nil,
+			d.ModulePlatformID(),
+			arch.Name(),
+			d.Releasever(),
+		)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Failed to depsolve base base packages for %s/%s/%s: %s", ir.ImageType, ir.Architecture, request.Distribution, err))
 		}
 
 		manifest, err := imageType.Manifest(nil, distro.ImageOptions{Size: imageType.Size(0)}, repositories, packageSpecSets, manifestSeed)
