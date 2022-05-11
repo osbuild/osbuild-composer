da78a767519642c8a7237e1f1fae17b1f220fdbe
Tomas Hozza <thozza@redhat.com>
2022-05-11 17:07:30 +0200

    target/koji: delete unused fields from `KojiTargetOptions`
    Delete unused fields from the `KojiTargetOptions` structure.

--- internal/target/koji_target.go
+++ internal/target/koji_target.go
@@ -1,16 +1,9 @@
 package target
 
 type KojiTargetOptions struct {
-	BuildID         uint64 `json:"build_id"`
-	TaskID          uint64 `json:"task_id"`
-	Token           string `json:"token"`
-	Name            string `json:"name"`
-	Version         string `json:"version"`
-	Release         string `json:"release"`
 	Filename        string `json:"filename"`
 	UploadDirectory string `json:"upload_directory"`
 	Server          string `json:"server"`
-	KojiFilename    string `json:"koji_filename"`
 }
 
 func (KojiTargetOptions) isTargetOptions() {}
