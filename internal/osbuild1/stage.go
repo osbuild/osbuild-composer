91758daa4fb5e357c0b26ba146cc72676d4d6768
Achilleas Koutsou <achilleas@koutsou.net>
2022-06-13 18:48:16 +0200

    osbuild1: remove UnmarshalJSON for Stage and StageOptions
    OSBuild used to return the stage options as part of the result object
    for v1 manifests.  We didn't use this information anywhere.  Currently
    we convert v1 results to the v2 format while parsing the results of jobs
    from old manifests (old distro definitions), but the StageOptions are
    ignored and we only care about the StageMetadata.

--- internal/osbuild1/stage.go
+++ internal/osbuild1/stage.go
@@ -1,10 +1,5 @@
 package osbuild1
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
 // A Stage transforms a filesystem tree.
 type Stage struct {
 	// Well-known name in reverse domain-name notation, uniquely identifying
@@ -18,72 +13,4 @@ type Stage struct {
 // StageOptions specify the operations of a given stage-type.
 type StageOptions interface {
 	isStageOptions()
-}
-
-type rawStage struct {
-	Name    string          `json:"name"`
-	Options json.RawMessage `json:"options"`
-}
-
-// UnmarshalJSON unmarshals JSON into a Stage object. Each type of stage has
-// a custom unmarshaller for its options, selected based on the stage name.
-func (stage *Stage) UnmarshalJSON(data []byte) error {
-	var rawStage rawStage
-	err := json.Unmarshal(data, &rawStage)
-	if err != nil {
-		return err
-	}
-	var options StageOptions
-	switch rawStage.Name {
-	case "org.osbuild.fix-bls":
-		// TODO: verify that we can unmarshall this also if "options" is omitted
-		options = new(FixBLSStageOptions)
-	case "org.osbuild.fstab":
-		options = new(FSTabStageOptions)
-	case "org.osbuild.grub2":
-		options = new(GRUB2StageOptions)
-	case "org.osbuild.locale":
-		options = new(LocaleStageOptions)
-	case "org.osbuild.selinux":
-		options = new(SELinuxStageOptions)
-	case "org.osbuild.hostname":
-		options = new(HostnameStageOptions)
-	case "org.osbuild.users":
-		options = new(UsersStageOptions)
-	case "org.osbuild.groups":
-		options = new(GroupsStageOptions)
-	case "org.osbuild.timezone":
-		options = new(TimezoneStageOptions)
-	case "org.osbuild.chrony":
-		options = new(ChronyStageOptions)
-	case "org.osbuild.keymap":
-		options = new(KeymapStageOptions)
-	case "org.osbuild.firewall":
-		options = new(FirewallStageOptions)
-	case "org.osbuild.rhsm":
-		options = new(RHSMStageOptions)
-	case "org.osbuild.rpm":
-		options = new(RPMStageOptions)
-	case "org.osbuild.rpm-ostree":
-		options = new(RPMOSTreeStageOptions)
-	case "org.osbuild.systemd":
-		options = new(SystemdStageOptions)
-	case "org.osbuild.script":
-		options = new(ScriptStageOptions)
-	case "org.osbuild.sysconfig":
-		options = new(SysconfigStageOptions)
-	case "org.osbuild.kernel-cmdline":
-		options = new(KernelCmdlineStageOptions)
-	default:
-		return fmt.Errorf("unexpected stage name: %s", rawStage.Name)
-	}
-	err = json.Unmarshal(rawStage.Options, options)
-	if err != nil {
-		return err
-	}
-
-	stage.Name = rawStage.Name
-	stage.Options = options
-
-	return nil
 }
