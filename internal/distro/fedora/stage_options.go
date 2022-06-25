284eb9af595927b551cffedb6f22c2f779e6bbd5
Tom Gundersen <teg@jklm.no>
2022-06-25 22:26:09 +0100

    distro/fedora: drop dead code

--- internal/distro/fedora/stage_options.go
+++ internal/distro/fedora/stage_options.go
@@ -2,10 +2,7 @@ package fedora
 
 import (
 	"fmt"
-	"path/filepath"
 
-	"github.com/osbuild/osbuild-composer/internal/blueprint"
-	"github.com/osbuild/osbuild-composer/internal/common"
 	"github.com/osbuild/osbuild-composer/internal/distro"
 	osbuild "github.com/osbuild/osbuild-composer/internal/osbuild2"
 )
@@ -13,81 +10,6 @@ import (
 const (
 	kspath = "/osbuild.ks"
 )
-
-// selinuxStageOptions returns the options for the org.osbuild.selinux stage.
-// Setting the argument to 'true' relabels the '/usr/bin/cp'
-// binariy with 'install_exec_t'. This should be set in the build root.
-func selinuxStageOptions(labelcp bool) *osbuild.SELinuxStageOptions {
-	options := &osbuild.SELinuxStageOptions{
-		FileContexts: "etc/selinux/targeted/contexts/files/file_contexts",
-	}
-	if labelcp {
-		options.Labels = map[string]string{
-			"/usr/bin/cp": "system_u:object_r:install_exec_t:s0",
-		}
-	}
-	return options
-}
-
-func usersFirstBootOptions(usersStageOptions *osbuild.UsersStageOptions) *osbuild.FirstBootStageOptions {
-	cmds := make([]string, 0, 3*len(usersStageOptions.Users)+2)
-	// workaround for creating authorized_keys file for user
-	// need to special case the root user, which has its home in a different place
-	varhome := filepath.Join("/var", "home")
-	roothome := filepath.Join("/var", "roothome")
-
-	for name, user := range usersStageOptions.Users {
-		if user.Key != nil {
-			var home string
-
-			if name == "root" {
-				home = roothome
-			} else {
-				home = filepath.Join(varhome, name)
-			}
-
-			sshdir := filepath.Join(home, ".ssh")
-
-			cmds = append(cmds, fmt.Sprintf("mkdir -p %s", sshdir))
-			cmds = append(cmds, fmt.Sprintf("sh -c 'echo %q >> %q'", *user.Key, filepath.Join(sshdir, "authorized_keys")))
-			cmds = append(cmds, fmt.Sprintf("chown %s:%s -Rc %s", name, name, sshdir))
-		}
-	}
-	cmds = append(cmds, fmt.Sprintf("restorecon -rvF %s", varhome))
-	cmds = append(cmds, fmt.Sprintf("restorecon -rvF %s", roothome))
-
-	options := &osbuild.FirstBootStageOptions{
-		Commands:       cmds,
-		WaitForNetwork: false,
-	}
-
-	return options
-}
-
-func firewallStageOptions(firewall *blueprint.FirewallCustomization) *osbuild.FirewallStageOptions {
-	options := osbuild.FirewallStageOptions{
-		Ports: firewall.Ports,
-	}
-
-	if firewall.Services != nil {
-		options.EnabledServices = firewall.Services.Enabled
-		options.DisabledServices = firewall.Services.Disabled
-	}
-
-	return &options
-}
-
-func systemdStageOptions(enabledServices, disabledServices []string, s *blueprint.ServicesCustomization, target string) *osbuild.SystemdStageOptions {
-	if s != nil {
-		enabledServices = append(enabledServices, s.Enabled...)
-		disabledServices = append(disabledServices, s.Disabled...)
-	}
-	return &osbuild.SystemdStageOptions{
-		EnabledServices:  enabledServices,
-		DisabledServices: disabledServices,
-		DefaultTarget:    target,
-	}
-}
 
 func buildStampStageOptions(arch, product, osVersion, variant string) *osbuild.BuildstampStageOptions {
 	return &osbuild.BuildstampStageOptions{
@@ -228,26 +150,4 @@ func xorrisofsStageOptions(filename, isolabel, arch string, isolinux bool) *osbu
 	}
 
 	return options
-}
-
-func nginxConfigStageOptions(path, htmlRoot, listen string) *osbuild.NginxConfigStageOptions {
-	// configure nginx to work in an unprivileged container
-	cfg := &osbuild.NginxConfig{
-		Listen: listen,
-		Root:   htmlRoot,
-		Daemon: common.BoolToPtr(false),
-		PID:    "/tmp/nginx.pid",
-	}
-	return &osbuild.NginxConfigStageOptions{
-		Path:   path,
-		Config: cfg,
-	}
-}
-
-func chmodStageOptions(path, mode string, recursive bool) *osbuild.ChmodStageOptions {
-	return &osbuild.ChmodStageOptions{
-		Items: map[string]osbuild.ChmodStagePathOptions{
-			path: {Mode: mode, Recursive: recursive},
-		},
-	}
 }
