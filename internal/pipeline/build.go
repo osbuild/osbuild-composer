e5ef7933acea447d2ace034fda91d1c69e277d12
Tom Gundersen <teg@jklm.no>
2022-06-26 19:58:51 +0100

    pipeline: drop stage_options.go
    This was barely used. Making it explicit seemed more readable.

--- internal/pipeline/build.go
+++ internal/pipeline/build.go
@@ -25,7 +25,13 @@ func (p BuildPipeline) Serialize() osbuild2.Pipeline {
 	pipeline := p.Pipeline.Serialize()
 
 	pipeline.AddStage(osbuild2.NewRPMStage(osbuild2.NewRPMStageOptions(p.repos), osbuild2.NewRpmStageSourceFilesInputs(p.packageSpecs)))
-	pipeline.AddStage(osbuild2.NewSELinuxStage(selinuxStageOptions(true)))
+	pipeline.AddStage(osbuild2.NewSELinuxStage(&osbuild2.SELinuxStageOptions{
+		FileContexts: "etc/selinux/targeted/contexts/files/file_contexts",
+		Labels: map[string]string{
+			"/usr/bin/cp": "system_u:object_r:install_exec_t:s0",
+		},
+	},
+	))
 
 	return pipeline
 }
