95046d5feed799cdbb02914a35296c51d263e602
Gianluca Zuccarelli <gzuccare@redhat.com>
2022-06-29 17:14:08 +0100

    pipelines: add oscap config for os pipeline
    Add config options for oscap in the build pipeline
    and add a new stage if the config is not nil.

--- internal/manifest/os.go
+++ internal/manifest/os.go
@@ -70,23 +70,24 @@ type OSCustomizations struct {
 	Users    []blueprint.UserCustomization
 	Firewall *blueprint.FirewallCustomization
 	// TODO: drop osbuild types from the API
-	Grub2Config   *osbuild.GRUB2Config
-	Sysconfig     []*osbuild.SysconfigStageOptions
-	SystemdLogind []*osbuild.SystemdLogindStageOptions
-	CloudInit     []*osbuild.CloudInitStageOptions
-	Modprobe      []*osbuild.ModprobeStageOptions
-	DracutConf    []*osbuild.DracutConfStageOptions
-	SystemdUnit   []*osbuild.SystemdUnitStageOptions
-	Authselect    *osbuild.AuthselectStageOptions
-	SELinuxConfig *osbuild.SELinuxConfigStageOptions
-	Tuned         *osbuild.TunedStageOptions
-	Tmpfilesd     []*osbuild.TmpfilesdStageOptions
-	PamLimitsConf []*osbuild.PamLimitsConfStageOptions
-	Sysctld       []*osbuild.SysctldStageOptions
-	DNFConfig     []*osbuild.DNFConfigStageOptions
-	SshdConfig    *osbuild.SshdConfigStageOptions
-	AuthConfig    *osbuild.AuthconfigStageOptions
-	PwQuality     *osbuild.PwqualityConfStageOptions
+	Grub2Config    *osbuild.GRUB2Config
+	Sysconfig      []*osbuild.SysconfigStageOptions
+	SystemdLogind  []*osbuild.SystemdLogindStageOptions
+	CloudInit      []*osbuild.CloudInitStageOptions
+	Modprobe       []*osbuild.ModprobeStageOptions
+	DracutConf     []*osbuild.DracutConfStageOptions
+	SystemdUnit    []*osbuild.SystemdUnitStageOptions
+	Authselect     *osbuild.AuthselectStageOptions
+	SELinuxConfig  *osbuild.SELinuxConfigStageOptions
+	Tuned          *osbuild.TunedStageOptions
+	Tmpfilesd      []*osbuild.TmpfilesdStageOptions
+	PamLimitsConf  []*osbuild.PamLimitsConfStageOptions
+	Sysctld        []*osbuild.SysctldStageOptions
+	DNFConfig      []*osbuild.DNFConfigStageOptions
+	SshdConfig     *osbuild.SshdConfigStageOptions
+	AuthConfig     *osbuild.AuthconfigStageOptions
+	PwQuality      *osbuild.PwqualityConfStageOptions
+	OpenSCAPConfig *osbuild.OscapRemediationStageOptions
 }
 
 // OS represents the filesystem tree of the target image. This roughly
@@ -416,6 +417,10 @@ func (p *OS) serialize() osbuild.Pipeline {
 		}
 
 		pipeline.AddStage(bootloader)
+	}
+
+	if p.OpenSCAPConfig != nil {
+		pipeline.AddStage(osbuild.NewOscapRemediationStage(p.OpenSCAPConfig))
 	}
 
 	if p.SElinux != "" {
