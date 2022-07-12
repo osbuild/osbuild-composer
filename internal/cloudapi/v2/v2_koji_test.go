74eb3860df1b02635170ad8f0efd80203f2cd23b
Ondřej Budai <ondrej@budai.cz>
2022-07-12 10:02:52 +0200

    internal: remove kojiapi
    We no longer use it, let's remove it. If you are wondering what to use instead,
    use Cloud API. It supports everything that Koji API supported and more.
    
    Signed-off-by: Ondřej Budai <ondrej@budai.cz>

--- internal/cloudapi/v2/v2_koji_test.go
+++ internal/cloudapi/v2/v2_koji_test.go
@@ -13,7 +13,6 @@ import (
 
 	v2 "github.com/osbuild/osbuild-composer/internal/cloudapi/v2"
 	"github.com/osbuild/osbuild-composer/internal/distro/test_distro"
-	"github.com/osbuild/osbuild-composer/internal/kojiapi/api"
 	"github.com/osbuild/osbuild-composer/internal/osbuild"
 	"github.com/osbuild/osbuild-composer/internal/target"
 	"github.com/osbuild/osbuild-composer/internal/test"
