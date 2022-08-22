c85e6628472ef84641a8e8ec68498bee087c7b0d
Sayan Paul <saypaul@redhat.com>
2022-08-22 13:54:14 +0000

    added greenboot stage test

---
+++ internal/osbuild/greenboot_stage_test.go
@@ -0,0 +1,20 @@
+package osbuild
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGreenbootConfig(t *testing.T) {
+	config := &GreenbootConfig{
+		MonitorService: []string{"sshd", "NetworkManager"},
+	}
+	expectedConfig := &Stage{
+		Type:    "org.osbuild.greenboot",
+		Options: config,
+	}
+
+	actualConfig := NewGreenbootConfig(config)
+	assert.Equal(t, expectedConfig, actualConfig)
+}
