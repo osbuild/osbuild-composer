c48b63b2701492dd6b73788a4cc925f3439d53fe
Achilleas Koutsou <achilleas@koutsou.net>
2022-09-20 21:53:48 +0200

    osbuild: add isolinux stage
    Add support for the org.osbuild.isolinux stage.

---
+++ internal/osbuild/isolinux_stage.go
@@ -0,0 +1,27 @@
+package osbuild
+
+type ISOLinuxStageOptions struct {
+	Product ISOLinuxProduct `json:"product"`
+	Kernel  ISOLinuxKernel  `json:"kernel"`
+}
+
+func (ISOLinuxStageOptions) isStageOptions() {}
+
+type ISOLinuxProduct struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
+
+type ISOLinuxKernel struct {
+	Dir string `json:"dir"`
+
+	Opts []string `json:"opts,omitempty"`
+}
+
+func NewISOLinuxStage(options *ISOLinuxStageOptions, inputPipeline string) *Stage {
+	return &Stage{
+		Type:    "org.osbuild.isolinux",
+		Options: options,
+		Inputs:  NewPipelineTreeInputs("data", inputPipeline),
+	}
+}
