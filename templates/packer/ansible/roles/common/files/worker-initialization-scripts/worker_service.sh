5ffb9e693e0f8dab93f155d7c59328c9b570be1e
Diaa Sami <disami@redhat.com>
2022-09-27 12:30:04 +0200

    tools/appsre: remove monit setup code & scripts
    Since it doesn't not work since we moved workers to app-sre

--- templates/packer/ansible/roles/common/files/worker-initialization-scripts/worker_service.sh
+++ templates/packer/ansible/roles/common/files/worker-initialization-scripts/worker_service.sh
@@ -2,10 +2,7 @@
 set -euo pipefail
 source /tmp/cloud_init_vars
 
-echo "Starting worker service and monit."
+echo "Starting worker service."
 
 # Prepare osbuild-composer's remote worker services and sockets.
 systemctl enable --now "osbuild-remote-worker@${COMPOSER_HOST}:${COMPOSER_PORT}"
-
-# Now that everything is configured, ensure monit is monitoring everything.
-systemctl enable --now monit
