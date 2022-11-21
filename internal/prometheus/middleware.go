8756ea717d6aeca0e071e7f566bebe81eeffe376
Gianluca Zuccarelli <gzuccare@redhat.com>
2022-11-21 17:04:39 +0000

    prometheus: middleware to record 5xx errors
    Create a custom middleware function
    to measure 5xx requests for all composer
    & worker routes and not just the `/composer`
    endpoint. The result is a prometheus metric
    that contains info on the request status code,
    path & method.
    
    A helper function has been added to clean the
    dynamic parameters in the path routes to reduce
    metric cardinality

--- internal/prometheus/middleware.go
+++ internal/prometheus/middleware.go
@@ -1,6 +1,8 @@
 package prometheus
 
 import (
+	"errors"
+	"strconv"
 	"strings"
 
 	"github.com/labstack/echo/v4"
@@ -16,5 +18,37 @@ func MetricsMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 		timer := prometheus.NewTimer(httpDuration.WithLabelValues(ctx.Path()))
 		defer timer.ObserveDuration()
 		return next(ctx)
+	}
+}
+
+func StatusMiddleware(subsystem string) func(next echo.HandlerFunc) echo.HandlerFunc {
+	counter := StatusRequestsCounter(subsystem)
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(ctx echo.Context) error {
+
+			// call the next handler to see if
+			// an error occurred, see:
+			// - https://github.com/labstack/echo/issues/1837#issuecomment-816399630
+			// - https://github.com/labstack/echo/discussions/1820#discussioncomment-529428
+			err := next(ctx)
+
+			path := pathLabel(ctx.Path())
+			method := ctx.Request().Method
+			status := ctx.Response().Status
+
+			httpErr := new(echo.HTTPError)
+			if errors.As(err, &httpErr) {
+				status = httpErr.Code
+			}
+
+			counter.WithLabelValues(
+				method,
+				path,
+				strconv.Itoa(status),
+				subsystem,
+			).Inc()
+
+			return err
+		}
 	}
 }
