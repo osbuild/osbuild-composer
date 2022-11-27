2cd27c40191c15f05acebbaa45006286eda2a916
Tom Gundersen <teg@jklm.no>
2022-11-27 21:16:56 +0000

    worker/client: rework tests
    Rework tests to use the updated API. This tests the job event-loop
    as a whole and also adds tests for cancellation and interruption.

--- internal/worker/client_test.go
+++ internal/worker/client_test.go
@@ -1,11 +1,14 @@
 package worker_test
 
 import (
+	"context"
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
@@ -13,13 +16,19 @@ import (
 	"github.com/osbuild/osbuild-composer/internal/worker"
 )
 
-// newTestWorkerServer returns 3 strings:
-// - base URL of the worker server
-// - base URL of the OAuth server
-// - offline token for OAuth
-//
-// The worker server has one pending osbuild job of arch "arch" ready to be dequeued.
-func newTestWorkerServer(t *testing.T) (string, string, string) {
+type RunFunc func(context.Context, *worker.Job) (interface{}, error)
+type TestJobImpl struct {
+	testRun RunFunc
+}
+
+func (impl *TestJobImpl) Run(ctx context.Context, job *worker.Job) (interface{}, error) {
+	if impl.testRun == nil {
+		panic("no testRun() function defined")
+	}
+	return impl.testRun(ctx, job)
+}
+
+func newTestWorkerServerClientPair(t *testing.T, heartbeat time.Duration, proxy *proxy) (*worker.Server, *worker.Client) {
 	tempdir := t.TempDir()
 
 	q, err := fsjobqueue.New(tempdir)
@@ -29,8 +38,6 @@ func newTestWorkerServer(t *testing.T) (string, string, string) {
 		BasePath:     "/api/image-builder-worker/v1",
 	}
 	workerServer := worker.NewServer(nil, q, config)
-	_, err = workerServer.EnqueueOSBuild("arch", &worker.OSBuildJob{}, "")
-	require.NoError(t, err)
 
 	handler := workerServer.Handler()
 
@@ -42,7 +49,7 @@ func newTestWorkerServer(t *testing.T) (string, string, string) {
 
 	/* Check that the worker supplies the access token  */
 	calls := 0
-	proxySrv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	gatewaySrv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if calls == 0 {
 			require.Equal(t, "Bearer", r.Header.Get("Authorization"))
 			w.WriteHeader(http.StatusUnauthorized)
@@ -51,7 +58,7 @@ func newTestWorkerServer(t *testing.T) (string, string, string) {
 		require.Equal(t, "Bearer "+accessToken, r.Header.Get("Authorization"))
 		handler.ServeHTTP(w, r)
 	}))
-	t.Cleanup(proxySrv.Close)
+	t.Cleanup(gatewaySrv.Close)
 
 	/* Start oauth srv supplying the bearer token */
 	oauthSrv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -77,62 +84,214 @@ func newTestWorkerServer(t *testing.T) (string, string, string) {
 	}))
 	t.Cleanup(oauthSrv.Close)
 
-	return proxySrv.URL, oauthSrv.URL, offlineToken
-}
+	var proxyURL string
+	if proxy != nil {
+		// initialize a test proxy server
+		proxySrv := httptest.NewServer(proxy)
+		t.Cleanup(proxySrv.Close)
+		proxyURL = proxySrv.URL
+	}
 
-func TestOAuth(t *testing.T) {
-	workerURL, oauthURL, offlineToken := newTestWorkerServer(t)
-
-	client, err := worker.NewClient(worker.ClientConfig{
-		BaseURL:      workerURL,
-		TlsConfig:    nil,
+	workerClient, err := worker.NewClient(worker.ClientConfig{
+		BaseURL:      gatewaySrv.URL,
+		Heartbeat:    heartbeat,
 		ClientId:     "rhsm-api",
 		OfflineToken: offlineToken,
-		OAuthURL:     oauthURL,
+		OAuthURL:     oauthSrv.URL,
 		BasePath:     "/api/image-builder-worker/v1",
+		ProxyURL:     proxyURL,
 	})
 	require.NoError(t, err)
-	job, err := client.RequestJob([]string{worker.JobTypeOSBuild}, "arch")
-	require.NoError(t, err)
-	r := strings.NewReader("artifact contents")
-	require.NoError(t, job.UploadArtifact("some-artifact", r))
-	c, err := job.Canceled()
-	require.False(t, c)
-	require.NoError(t, err)
+
+	return workerServer, workerClient
 }
 
-func TestProxy(t *testing.T) {
-	workerURL, oauthURL, offlineToken := newTestWorkerServer(t)
+func TestClient(t *testing.T) {
+	server, client := newTestWorkerServerClientPair(t, 0, nil)
 
-	// initialize a test proxy server
-	proxy := &proxy{}
-	proxySrv := httptest.NewServer(proxy)
-	t.Cleanup(proxySrv.Close)
+	barrier := make(chan bool)
+	testRun := func(ctx context.Context, job *worker.Job) (interface{}, error) {
+		if <-barrier {
+			return struct{}{}, nil
+		} else {
+			return nil, nil
+		}
+	}
+	jobImpls := map[string]worker.JobImplementation{
+		worker.JobTypeOSBuild: &TestJobImpl{testRun},
+	}
+	requestCtx, requestCtxCancel := context.WithCancel(context.Background())
+	go client.Start(requestCtx, time.Duration(10*time.Microsecond), nil, "arch", jobImpls)
+	t.Cleanup(requestCtxCancel)
 
-	client, err := worker.NewClient(worker.ClientConfig{
-		BaseURL:      workerURL,
-		TlsConfig:    nil,
-		ClientId:     "rhsm-api",
-		OfflineToken: offlineToken,
-		OAuthURL:     oauthURL,
-		BasePath:     "/api/image-builder-worker/v1",
-		ProxyURL:     proxySrv.URL,
-	})
+	// schedule two jobs
+	// we only really care about the first one, and use the second one
+	// only as a barrier: once it is being processed we know the first one
+	// has been finished.
+	jobID, err := server.EnqueueOSBuild("arch", &worker.OSBuildJob{}, "")
+	require.NoError(t, err)
+	_, err = server.EnqueueOSBuild("arch", &worker.OSBuildJob{}, "")
+	require.NoError(t, err)
 
+	barrier <- true  // processing first job - run the test
+	barrier <- false // processing second job - only a barrier, don't run the test
+
+	var result worker.OSBuildJobResult
+	info, err := server.OSBuildJobInfo(jobID, &result)
 	require.NoError(t, err)
-	job, err := client.RequestJob([]string{worker.JobTypeOSBuild}, "arch")
+	require.NotZero(t, info.JobStatus.Finished)
+}
+
+func TestClientCancel(t *testing.T) {
+	barrier := make(chan bool)
+	testRun := func(ctx context.Context, job *worker.Job) (interface{}, error) {
+		if <-barrier {
+			<-ctx.Done() // wait to be cancelled
+			return struct{}{}, nil
+		} else {
+			return nil, nil
+		}
+	}
+	jobImpls := map[string]worker.JobImplementation{
+		worker.JobTypeOSBuild: &TestJobImpl{testRun},
+	}
+
+	server, client := newTestWorkerServerClientPair(t, 2*time.Millisecond, nil)
+
+	requestCtx, requestCtxCancel := context.WithCancel(context.Background())
+	go client.Start(requestCtx, time.Duration(10*time.Microsecond), nil, "arch", jobImpls)
+	t.Cleanup(requestCtxCancel)
+
+	// schedule two jobs
+	// we only really care about the first one, and use the second one
+	// only as a barrier: once it is being processed we know the first one
+	// has been finished.
+	jobID, err := server.EnqueueOSBuild("arch", &worker.OSBuildJob{}, "")
 	require.NoError(t, err)
-	r := strings.NewReader("artifact contents")
-	require.NoError(t, job.UploadArtifact("some-artifact", r))
-	c, err := job.Canceled()
-	require.False(t, c)
+	_, err = server.EnqueueOSBuild("arch", &worker.OSBuildJob{}, "")
 	require.NoError(t, err)
+
+	barrier <- true // processing first job - run the test
+	err = server.Cancel(jobID)
+	require.NoError(t, err)
+	barrier <- false // processing second job - only a barrier, don't run the test
+}
+
+func TestClientUpload(t *testing.T) {
+	server, client := newTestWorkerServerClientPair(t, 0, nil)
+
+	barrier := make(chan bool)
+	artifactName := "some-artifact"
+	artifactContents := "artifact contents"
+	testRun := func(ctx context.Context, job *worker.Job) (interface{}, error) {
+		if <-barrier {
+			r := strings.NewReader(artifactContents)
+			require.NoError(t, job.UploadArtifact(ctx, artifactName, r))
+			return struct{}{}, nil
+		} else {
+			return nil, nil
+		}
+	}
+	jobImpls := map[string]worker.JobImplementation{
+		worker.JobTypeOSBuild: &TestJobImpl{testRun},
+	}
+	requestCtx, requestCtxCancel := context.WithCancel(context.Background())
+	go client.Start(requestCtx, time.Duration(10*time.Microsecond), nil, "arch", jobImpls)
+	t.Cleanup(requestCtxCancel)
+
+	// schedule two jobs
+	jobID, err := server.EnqueueOSBuild("arch", &worker.OSBuildJob{}, "")
+	require.NoError(t, err)
+	_, err = server.EnqueueOSBuild("arch", &worker.OSBuildJob{}, "")
+	require.NoError(t, err)
+
+	barrier <- true  // processing first job - run the test
+	barrier <- false // processing second job - only a barrier, don't run the test
+
+	var result worker.OSBuildJobResult
+	info, err := server.OSBuildJobInfo(jobID, &result)
+	require.NoError(t, err)
+	require.NotZero(t, info.JobStatus.Finished)
+	reader, size, err := server.JobArtifact(jobID, artifactName)
+	require.NoError(t, err)
+	require.Equal(t, int64(len(artifactContents)), size)
+	artifact, err := ioutil.ReadAll(reader)
+	require.NoError(t, err)
+	require.Equal(t, artifactContents, string(artifact))
+}
+
+func TestClientProxy(t *testing.T) {
+	proxy := proxy{}
+	server, client := newTestWorkerServerClientPair(t, 0, &proxy)
+
+	barrier := make(chan bool)
+	testRun := func(ctx context.Context, job *worker.Job) (interface{}, error) {
+		if <-barrier {
+			return struct{}{}, nil
+		} else {
+			return nil, nil
+		}
+	}
+	jobImpls := map[string]worker.JobImplementation{
+		worker.JobTypeOSBuild: &TestJobImpl{testRun},
+	}
+	requestCtx, requestCtxCancel := context.WithCancel(context.Background())
+	go client.Start(requestCtx, time.Duration(10*time.Microsecond), nil, "arch", jobImpls)
+	t.Cleanup(requestCtxCancel)
+
+	// schedule two jobs
+	_, err := server.EnqueueOSBuild("arch", &worker.OSBuildJob{}, "")
+	require.NoError(t, err)
+	_, err = server.EnqueueOSBuild("arch", &worker.OSBuildJob{}, "")
+	require.NoError(t, err)
+
+	barrier <- true  // processing first job - run the test
+	barrier <- false // processing second job - only a barrier, don't run the test
 
 	// we expect 5 calls to go through the proxy:
 	// - request job (fails, no oauth token)
 	// - oauth call
 	// - request job (succeeds)
-	// - upload artifact
-	// - cancel
+	// - finish
+	// - request next job (hangs)
 	require.Equal(t, 5, proxy.calls)
+}
+
+func TestClientInterrupt(t *testing.T) {
+	server, client := newTestWorkerServerClientPair(t, 0, nil)
+
+	_, err := server.EnqueueOSBuild("arch", &worker.OSBuildJob{}, "")
+	require.NoError(t, err)
+
+	barrier1 := make(chan struct{})
+	testRun1 := func(ctx context.Context, job *worker.Job) (interface{}, error) {
+		barrier1 <- struct{}{}
+		<-ctx.Done()
+		return struct{}{}, nil
+	}
+	jobImpls1 := map[string]worker.JobImplementation{
+		worker.JobTypeOSBuild: &TestJobImpl{testRun1},
+	}
+
+	clientCtx1, clientCtxCancel1 := context.WithCancel(context.Background())
+	go client.Start(clientCtx1, time.Duration(10*time.Microsecond), nil, "arch", jobImpls1)
+
+	<-barrier1         // processing first job - will hang to wait for cancellation
+	clientCtxCancel1() // cancel the client - this will cause the job to get rescheduled
+
+	// start a second client to pick up the job
+	barrier2 := make(chan struct{})
+	testRun2 := func(ctx context.Context, job *worker.Job) (interface{}, error) {
+		barrier2 <- struct{}{}
+		return struct{}{}, nil
+	}
+	jobImpls2 := map[string]worker.JobImplementation{
+		worker.JobTypeOSBuild: &TestJobImpl{testRun2},
+	}
+
+	clientCtx2, clientCtxCancel2 := context.WithCancel(context.Background())
+	go client.Start(clientCtx2, time.Duration(10*time.Microsecond), nil, "arch", jobImpls2)
+	t.Cleanup(clientCtxCancel2)
+
+	<-barrier2 // the job got picked up again - interruption worked as expected
 }
