2e445b33ab4da53023ecdebb11d1e55a3e287ace
Antonio Murdaca <antoniomurdaca@gmail.com>
2022-11-29 13:28:42 +0100

    add ign to rhel92+
    Signed-off-by: Antonio Murdaca <antoniomurdaca@gmail.com>

--- internal/distro/rhel9/edge.go
+++ internal/distro/rhel9/edge.go
@@ -366,9 +366,6 @@ func edgeCommitPackageSet(t *imageType) rpmmd.PackageSet {
 			"greenboot-default-health-checks",
 			"fdo-client",
 			"fdo-owner-cli",
-			"ignition",
-			"ignition-edge",
-			"ssh-key-dir",
 		},
 		Exclude: []string{
 			"rng-tools",
@@ -383,6 +380,10 @@ func edgeCommitPackageSet(t *imageType) rpmmd.PackageSet {
 
 	case distro.Aarch64ArchName:
 		ps = ps.Append(aarch64EdgeCommitPackageSet(t))
+	}
+
+	if !common.VersionLessThan(t.arch.distro.osVersion, "9.2") {
+		ps.Include = append(ps.Include, "ignition", "ignition-edge", "ssh-key-dir")
 	}
 
 	return ps
