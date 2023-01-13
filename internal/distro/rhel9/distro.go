0b380b70a0f5f31710306b8268bbddf8ecb5590c
Achilleas Koutsou <achilleas@koutsou.net>
2023-01-13 17:36:41 +0100

    distro/rhel9: make rhel-9 point to rhel-91
    Make RHEL 9 without a minor version point to RHEL 9.1, the current GA
    version.

--- internal/distro/rhel9/distro.go
+++ internal/distro/rhel9/distro.go
@@ -126,7 +126,7 @@ func (d *distribution) getDefaultImageConfig() *distro.ImageConfig {
 
 func New() distro.Distro {
 	// default minor: create default minor version (current GA) and rename it
-	d := newDistro("rhel", 0)
+	d := newDistro("rhel", 1)
 	d.name = "rhel-9"
 	return d
 }
