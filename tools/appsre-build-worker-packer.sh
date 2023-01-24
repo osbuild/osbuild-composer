f7e8ce1b941ade5d39f17c4a9053f9353531f01f
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2023-01-24 12:59:44 +0100

    packer: workaround missing authselect
    The AWS RHUI AppStream repository is missing
    authselect-compat-1.2.5-2.el9_1.x86_64.

--- tools/appsre-build-worker-packer.sh
+++ tools/appsre-build-worker-packer.sh
@@ -26,7 +26,7 @@ if [ "$ON_JENKINS" = false ]; then
     # see https://bugzilla.redhat.com/show_bug.cgi?id=2143282
     # TODO: Remove me when the bug is fixed or we switch to 9.1
     sudo dnf remove -y python-unversioned-command
-    sudo dnf upgrade -y
+    sudo dnf upgrade -y --allowerasing authselect
 
     sudo dnf install -y podman jq
 fi
