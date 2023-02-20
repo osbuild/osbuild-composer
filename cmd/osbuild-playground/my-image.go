8c6fba93053bec95fe902dbd359b12ef2f1da35d
Pavel Odvody <pavel@redhat.com>
2023-02-20 11:23:35 +0100

    Add support for Aarch64, update necessary customizations
    Signed-off-by: Pavel Odvody <pavel@redhat.com>

--- cmd/osbuild-playground/my-image.go
+++ cmd/osbuild-playground/my-image.go
@@ -4,6 +4,7 @@ import (
 	"math/rand"
 
 	"github.com/osbuild/osbuild-composer/internal/artifact"
+	"github.com/osbuild/osbuild-composer/internal/common"
 	"github.com/osbuild/osbuild-composer/internal/disk"
 	"github.com/osbuild/osbuild-composer/internal/manifest"
 	"github.com/osbuild/osbuild-composer/internal/platform"
@@ -33,9 +34,14 @@ func (img *MyImage) InstantiateManifest(m *manifest.Manifest,
 	build := manifest.NewBuild(m, runner, repos)
 	build.Checkpoint()
 
-	// create an x86_64 platform with bios boot
-	platform := &platform.X86{
-		BIOS: true,
+	var build_platform platform.Platform
+	switch common.CurrentArch() {
+	case "aarch64":
+		build_platform = &platform.Aarch64{}
+	default:
+		build_platform = &platform.X86{
+			BIOS: true,
+		}
 	}
 
 	// TODO: add helper
@@ -45,9 +51,12 @@ func (img *MyImage) InstantiateManifest(m *manifest.Manifest,
 	}
 
 	// create a minimal bootable OS tree
-	os := manifest.NewOS(m, build, platform, repos)
+	os := manifest.NewOS(m, build, build_platform, repos)
 	os.PartitionTable = pt   // we need a partition table
 	os.KernelName = "kernel" // use the default fedora kernel
+	os.OSCustomizations.Language = "en_US.UTF-8"
+	os.OSCustomizations.Hostname = "my-host"
+	os.OSCustomizations.Timezone = "UTC"
 
 	// create a raw image containing the OS tree created above
 	raw := manifest.NewRawImage(m, build, os)
