0269293ae00fc8f47018d286b0e564a857a63cea
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2023-02-22 16:39:03 +0100

    test/vmware: boot VMs with EFI and use SCSI as a disk controller
    Since we use streamOptimized images everywhere, we no longer need to use
    IDE and boot with bios. Let's test a more realistic scenario.

--- test/cases/vmware.sh
+++ test/cases/vmware.sh
@@ -182,9 +182,9 @@ $GOVC_CMD vm.create -u "${GOVMOMI_USERNAME}":"${GOVMOMI_PASSWORD}"@"${GOVMOMI_UR
     -folder="${GOVMOMI_FOLDER}" \
     -net="${GOVMOMI_NETWORK}" \
     -net.adapter=vmxnet3 \
-    -m=4096 -c=2 -g=rhel8_64Guest -on=true -firmware=bios \
+    -m=4096 -c=2 -g=rhel8_64Guest -on=true -firmware=efi \
     -disk="${IMAGE_KEY}"/"${IMAGE_KEY}".vmdk \
-    --disk.controller=ide \
+    --disk.controller=scsi \
     "${IMAGE_KEY}"
 
 # tagging vm as testing object
