ab9e612c9960dbcdd09706bee3e443ddfa23d830
Achilleas Koutsou <achilleas@koutsou.net>
2023-03-17 17:41:30 +0100

    containers: update tests
    Update tests to include the list digests of the test images.

--- internal/container/client_test.go
+++ internal/container/client_test.go
@@ -18,7 +18,7 @@ func TestClientResolve(t *testing.T) {
 	defer registry.Close()
 
 	repo := registry.AddRepo("library/osbuild")
-	repo.AddImage(
+	listDigest := repo.AddImage(
 		[]Blob{NewDataBlobFromBase64(rootLayer)},
 		[]string{"amd64", "ppc64le"},
 		"cool container",
@@ -39,11 +39,12 @@ func TestClientResolve(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, container.Spec{
-		Source:    ref,
-		Digest:    "sha256:f29b6cd42a94a574583439addcd6694e6224f0e4b32044c9e3aee4c4856c2a50",
-		ImageID:   "sha256:c2ecf25cf190e76b12b07436ad5140d4ba53d8a136d498705e57a006837a720f",
-		TLSVerify: client.GetTLSVerify(),
-		LocalName: ref,
+		Source:     ref,
+		Digest:     "sha256:f29b6cd42a94a574583439addcd6694e6224f0e4b32044c9e3aee4c4856c2a50",
+		ImageID:    "sha256:c2ecf25cf190e76b12b07436ad5140d4ba53d8a136d498705e57a006837a720f",
+		TLSVerify:  client.GetTLSVerify(),
+		LocalName:  ref,
+		ListDigest: listDigest,
 	}, spec)
 
 	client.SetArchitectureChoice("ppc64le")
@@ -51,11 +52,12 @@ func TestClientResolve(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, container.Spec{
-		Source:    ref,
-		Digest:    "sha256:d49eebefb6c7ce5505594bef652bd4adc36f413861bd44209d9b9486310b1264",
-		ImageID:   "sha256:d2ab8fea7f08a22f03b30c13c6ea443121f25e87202a7496e93736efa6fe345a",
-		TLSVerify: client.GetTLSVerify(),
-		LocalName: ref,
+		Source:     ref,
+		Digest:     "sha256:d49eebefb6c7ce5505594bef652bd4adc36f413861bd44209d9b9486310b1264",
+		ImageID:    "sha256:d2ab8fea7f08a22f03b30c13c6ea443121f25e87202a7496e93736efa6fe345a",
+		TLSVerify:  client.GetTLSVerify(),
+		LocalName:  ref,
+		ListDigest: listDigest,
 	}, spec)
 
 	// don't have that architecture
