e1d879691eb74bde29746e3dfa20447efad70b35
Antonio Murdaca <antoniomurdaca@gmail.com>
2023-06-28 10:39:03 +0200

    fedora-iot: enable fdo-client-linuxapp via presets
    Signed-off-by: Antonio Murdaca <antoniomurdaca@gmail.com>

--- internal/distro/fedora/images.go
+++ internal/distro/fedora/images.go
@@ -348,6 +348,10 @@ func iotCommitImage(workload workload.Workload,
 				Name:  "coreos-ignition-write-issues.service",
 				State: osbuild.StateEnable,
 			},
+			{
+				Name:  "fdo-client-linuxapp.service",
+				State: osbuild.StateEnable,
+			},
 		}
 	}
 	img.Environment = t.environment
@@ -383,6 +387,10 @@ func iotContainerImage(workload workload.Workload,
 			},
 			{
 				Name:  "coreos-ignition-write-issues.service",
+				State: osbuild.StateEnable,
+			},
+			{
+				Name:  "fdo-client-linuxapp.service",
 				State: osbuild.StateEnable,
 			},
 		}
