7fa81a514f088ca409159596e78ae62e3c16e754
Alexander Todorov <atodorov@redhat.com>
2023-07-03 12:35:19 +0300

    Schedule a nightly CI pipeline against each PR
    - don't report to Slack if SLACK_WEBHOOK_URL is not defined
    - report GitHub statuses separately for regular pipeline & nightly
      pipeline

--- schutzbot/slack_notification.sh
+++ schutzbot/slack_notification.sh
@@ -2,6 +2,11 @@
 
 set -eux
 
+if [ -z "$SLACK_WEBHOOK_URL" ]; then
+    echo "INFO: Variable SLACK_WEBHOOK_URL is undefined"
+    exit 0
+fi
+
 COMPOSE_ID=$(cat COMPOSE_ID)
 COMPOSER_NVR=$(cat COMPOSER_NVR)
 MESSAGE="\"Nightly pipeline execution on *$COMPOSE_ID* with *$COMPOSER_NVR* finished with status *$1* $2 \n QE: @atodorov, @jrusz, @jabia\n Link to results: $CI_PIPELINE_URL\n For edge testing status please see https://url.corp.redhat.com/edge-pipelines \""
--- schutzbot/update_github_status.sh
+++ schutzbot/update_github_status.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-
-# Don't overwrite CI statuses on GitHub branches for nightly pipelines
-if [[ "$CI_PIPELINE_SOURCE" == "schedule" ]]; then
-    exit 0
-fi
-
 # if a user is logged in to the runner, wait until they're done
 while (( $(who -s | wc -l)  > 0 )); do
     echo "Waiting for user(s) to log off"
@@ -33,12 +27,17 @@ else
   exit 1
 fi
 
+CONTEXT="Schutzbot on GitLab"
+if [[ "$CI_PIPELINE_SOURCE" == "schedule" ]]; then
+    CONTEXT="$CONTEXT, RHEL-${RHEL_MAJOR:-}-nightly"
+fi
+
 curl \
     -u "${SCHUTZBOT_LOGIN}" \
     -X POST \
     -H "Accept: application/vnd.github.v3+json" \
     "https://api.github.com/repos/osbuild/osbuild-composer/statuses/${CI_COMMIT_SHA}" \
-    -d '{"state":"'"${GITHUB_NEW_STATE}"'", "description": "'"${GITHUB_NEW_DESC}"'", "context": "Schutzbot on GitLab", "target_url": "'"${CI_PIPELINE_URL}"'"}'
+    -d '{"state":"'"${GITHUB_NEW_STATE}"'", "description": "'"${GITHUB_NEW_DESC}"'", "context": "'"${CONTEXT}"'", "target_url": "'"${CI_PIPELINE_URL}"'"}'
 
 # ff release branch on github if this ran on main
 if [ "$CI_COMMIT_BRANCH" = "main" ] && [ "$GITHUB_NEW_STATE" = "success" ]; then
