be64684d7f6bdf3d11ab553c5ebe2b35ebaf446c
Jakub Rusz <jrusz@redhat.com>
2023-07-27 14:17:55 +0200

    Juan is no longer with Red Hat

--- schutzbot/slack_notification.sh
+++ schutzbot/slack_notification.sh
@@ -9,7 +9,7 @@ fi
 
 COMPOSE_ID=$(cat COMPOSE_ID)
 COMPOSER_NVR=$(cat COMPOSER_NVR)
-MESSAGE="\"Nightly pipeline execution on *$COMPOSE_ID* with *$COMPOSER_NVR* finished with status *$1* $2 \n QE: @atodorov, @jrusz, @jabia\n Link to results: $CI_PIPELINE_URL\n For edge testing status please see https://url.corp.redhat.com/edge-pipelines \""
+MESSAGE="\"Nightly pipeline execution on *$COMPOSE_ID* with *$COMPOSER_NVR* finished with status *$1* $2 \n QE: @atodorov, @jrusz\n Link to results: $CI_PIPELINE_URL\n For edge testing status please see https://url.corp.redhat.com/edge-pipelines \""
 
 curl \
     -X POST \
