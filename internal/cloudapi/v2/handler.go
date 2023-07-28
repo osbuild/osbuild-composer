0936fddfcb92672f8f87de3a16d995ba43549f9d
Brian C. Lane <bcl@redhat.com>
2023-07-28 14:35:15 -0700

    cloudapi: Add optional image size support
    This adds a 'size' parameter to the image_request object. It can be used
    to specify the minimum image size in bytes.
    
    This behaves in the same way as the size parameter of the weldr API.
    
    For raw images the root partition is grown to fill the available space.
    For LVM images the PV uses the available space, but the LV does not,
    leaving space available for other LVs to be created after boot.
    
    See COMPOSER-1883

--- internal/cloudapi/v2/handler.go
+++ internal/cloudapi/v2/handler.go
@@ -447,7 +447,19 @@ func (h *apiHandlers) PostCompose(ctx echo.Context) error {
 			return err
 		}
 
-		imageOptions := distro.ImageOptions{Size: imageType.Size(0)}
+		// check if filesytem customizations have been set.
+		// if compose size parameter is set, take the larger of
+		// the two values.
+		// NOTE: The size is in bytes
+		var size uint64
+		if ir.Size == nil {
+			size = imageType.Size(0)
+		} else if minSize := bp.Customizations.GetFilesystemsMinSize(); bp.Customizations != nil && minSize > 0 && minSize > *ir.Size {
+			size = imageType.Size(minSize)
+		} else {
+			size = imageType.Size(*ir.Size)
+		}
+		imageOptions := distro.ImageOptions{Size: size}
 
 		if request.Koji == nil {
 			imageOptions.Facts = &facts.ImageOptions{
