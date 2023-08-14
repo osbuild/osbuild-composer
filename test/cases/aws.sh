39cd32756ed3dd096b49da4db2c10a9454773d49
Alexander Todorov <atodorov@redhat.com>
2023-08-14 12:18:25 +0300

    DEBUG: use cloud-image-val:pr-266 container
    in order to exercise test for RHBZ#2221269. See:
    https://github.com/osbuild/cloud-image-val/pull/266
    
    NOTE: changing more than necessary in order to satisfy CI tools

--- test/cases/aws.sh
+++ test/cases/aws.sh
@@ -215,15 +215,7 @@ fi
 
 greenprint "Pulling cloud-image-val container"
 
-if [[ "$CI_PROJECT_NAME" =~ "cloud-image-val" ]]; then
-  # If running on CIV, get dev container
-  TAG=${CI_COMMIT_REF_SLUG}
-else
-  # If not, get prod container
-  TAG="prod"
-fi
-
-CONTAINER_CLOUD_IMAGE_VAL="quay.io/cloudexperience/cloud-image-val:$TAG"
+CONTAINER_CLOUD_IMAGE_VAL="quay.io/cloudexperience/cloud-image-val:pr-266"
 
 sudo "${CONTAINER_RUNTIME}" pull "${CONTAINER_CLOUD_IMAGE_VAL}"
 
