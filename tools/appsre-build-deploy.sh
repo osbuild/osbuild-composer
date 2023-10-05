134955352c296a32146d622a359896bd7d71bee1
Tomáš Hozza <thozza@redhat.com>
2023-10-05 16:58:27 +0200

    Container: set the composer git revision during the build
    When the container with osbuild-composer gets built in our CI or by
    AppSRE, we do not set the composer version to any value (as we do when
    we built RPMs). As a result, the version reported by composer is always
    "devel". This is not useful for debugging and determining the used
    version of composer. In addition, this information now gets exposed in
    Koji builds, therefore it makes sense to make it useful.
    
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

--- tools/appsre-build-deploy.sh
+++ tools/appsre-build-deploy.sh
@@ -4,6 +4,7 @@ set -exv
 
 IMAGE_NAME="quay.io/app-sre/composer"
 IMAGE_TAG=$(git rev-parse --short=7 HEAD)
+COMMIT=$(git rev-parse HEAD)
 
 if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
     echo "QUAY_USER and QUAY_TOKEN must be set"
@@ -13,7 +14,7 @@ fi
 DOCKER_CONF="$PWD/.docker"
 mkdir -p "$DOCKER_CONF"
 docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
-docker --config="$DOCKER_CONF" build -f distribution/Dockerfile-ubi -t "${IMAGE_NAME}:${IMAGE_TAG}" .
+docker --config="$DOCKER_CONF" build -f distribution/Dockerfile-ubi --build-arg="COMMIT=${COMMIT}" -t "${IMAGE_NAME}:${IMAGE_TAG}" .
 docker --config="$DOCKER_CONF" push "${IMAGE_NAME}:${IMAGE_TAG}"
 
 # Maintenance image
