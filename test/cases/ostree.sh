3d7b01b18d248890f4717e48d827eda8b7d70a60
yih <yih@redhat.com>
2023-10-07 16:43:13 +0800

    update pulp test case

--- test/cases/ostree.sh
+++ test/cases/ostree.sh
@@ -13,7 +13,6 @@ fi
 # Set up variables.
 FIREWALL_FEATURE="false"
 SYSROOT_RO="false"
-SKIP_PULP_TEST="true"
 
 # Provision the software under test.
 /usr/libexec/osbuild-composer-test/provision.sh none
@@ -60,6 +59,7 @@ case "${ID}-${VERSION_ID}" in
         FIREWALL_FEATURE="true"
         DIRS_FILES_CUSTOMIZATION="true"
 
+
         # Use a stable installer image unless it's the nightly pipeline
         BOOT_LOCATION="http://download.devel.redhat.com/released/rhel-8/RHEL-8/8.8.0/BaseOS/x86_64/os/"
         if [ "${NIGHTLY:=false}" == "true" ]; then
@@ -74,6 +74,7 @@ case "${ID}-${VERSION_ID}" in
         EMBEDED_CONTAINER="true"
         FIREWALL_FEATURE="true"
         DIRS_FILES_CUSTOMIZATION="true"
+
 
         # Use a stable installer image unless it's the nightly pipeline
         BOOT_LOCATION="http://download.devel.redhat.com/released/rhel-8/RHEL-8/8.8.0/BaseOS/x86_64/os/"
@@ -116,7 +117,7 @@ case "${ID}-${VERSION_ID}" in
         FIREWALL_FEATURE="true"
         DIRS_FILES_CUSTOMIZATION="true"
         SYSROOT_RO="true"
-        SKIP_PULP_TEST="false"
+
         # Use a stable installer image unless it's the nightly pipeline
         BOOT_LOCATION="http://download.devel.redhat.com/released/rhel-9/RHEL-9/9.2.0/BaseOS/x86_64/os/"
         if [ "${NIGHTLY:=false}" == "true" ]; then
@@ -223,21 +224,12 @@ COMPOSE_INFO=${TEMPDIR}/compose-info-${IMAGE_KEY}.json
 FEDORA_IMAGE_DIGEST="sha256:4d76a7480ce1861c95975945633dc9d03807ffb45c64b664ef22e673798d414b"
 FEDORA_LOCAL_NAME="localhost/fedora-minimal:v1"
 PROD_REPO_URL=http://192.168.100.1/repo
-PROD_REPO=/var/www/html/repo
 
 # SSH setup.
 SSH_OPTIONS=(-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=5)
 SSH_DATA_DIR=$(/usr/libexec/osbuild-composer-test/gen-ssh.sh)
 SSH_KEY=${SSH_DATA_DIR}/id_rsa
 SSH_KEY_PUB="$(cat "${SSH_KEY}".pub)"
-
-# Pulp setup
-PULP_CONFIG_FILE="$(pwd)/pulp.toml"
-PULP_SERVER="http://192.168.100.1:9090"
-PULP_USERNAME="admin"
-PULP_PASSWORD="foobar"
-PULP_REPO="commit"
-PULP_BASEPATH="commit"
 
 # Get the compose log.
 get_compose_log () {
@@ -267,6 +259,9 @@ get_compose_metadata () {
 
 # Build ostree image.
 build_image() {
+    blueprint_name=$1
+    image_type=$2
+
     # Get worker unit file so we can watch the journal.
     WORKER_UNIT=$(sudo systemctl list-units | grep -o -E "osbuild.*worker.*\.service")
     sudo journalctl -af -n 1 -u "${WORKER_UNIT}" &
@@ -276,33 +271,12 @@ build_image() {
 
     # Start the compose.
     greenprint "🚀 Starting compose"
-    pulp_enabled=$1
-    blueprint_name=$2
-    image_type=$3
-    if [ "$pulp_enabled" == "nopulp" ]; then
-        # for nopulp first build
-        if [ $# -eq 3 ]; then
-            sudo composer-cli --json compose start-ostree --ref "$OSTREE_REF" "$blueprint_name" "$image_type" | tee "$COMPOSE_START"
-        # for nopulp upgrade build
-        else
-            repo_url=$4
-            parent_ref=$5
-            sudo composer-cli --json compose start-ostree --ref "$OSTREE_REF" --parent "$parent_ref" --url "$repo_url" "$blueprint_name" "$image_type" | tee "$COMPOSE_START"
-        fi
+    if [ $# -eq 4 ]; then
+        repo_url=$3
+        parent_ref=$4
+        sudo composer-cli --json compose start-ostree --ref "$OSTREE_REF" --parent "$parent_ref" --url "$repo_url" "$blueprint_name" "$image_type" | tee "$COMPOSE_START"
     else
-        # for pulp first build
-        if [ $# -eq 5 ]; then
-            image_key=$4
-            pulp_config=$5
-            sudo composer-cli --json compose start-ostree --ref "$OSTREE_REF" "$blueprint_name" "$image_type" "$image_key" "$pulp_config" | tee "$COMPOSE_START"
-        # for pulp upgrade build
-        else
-            image_key=$4
-            pulp_config=$5
-            repo_url=$6
-            parent_ref=$7
-            sudo composer-cli --json compose start-ostree --ref "$OSTREE_REF" --parent "$parent_ref" --url "$repo_url" "$blueprint_name" "$image_type" "$image_key" "$pulp_config" | tee "$COMPOSE_START"
-        fi
+        sudo composer-cli --json compose start-ostree --ref "$OSTREE_REF" "$blueprint_name" "$image_type" | tee "$COMPOSE_START"
     fi
     COMPOSE_ID=$(get_build_info ".build_id" "$COMPOSE_START")
 
@@ -467,7 +441,7 @@ sudo composer-cli blueprints push "$BLUEPRINT_FILE"
 sudo composer-cli blueprints depsolve ostree
 
 # Build installation image.
-build_image nopulp ostree "$IMAGE_TYPE"
+build_image ostree "$IMAGE_TYPE"
 
 # Start httpd to serve ostree repo.
 greenprint "🚀 Starting httpd daemon"
@@ -724,7 +698,7 @@ greenprint "🕹 Get ostree installed commit value"
 PARENT_HASH=$(curl "${PROD_REPO_URL}/refs/heads/${OSTREE_REF}")
 
 # Build upgrade image.
-build_image nopulp upgrade "$IMAGE_TYPE" "$PROD_REPO_URL" "$PARENT_HASH"
+build_image upgrade "$IMAGE_TYPE" "$PROD_REPO_URL" "$PARENT_HASH"
 
 # Download the image and extract tar into web server root folder.
 greenprint "📥 Downloading and extracting the image"
@@ -749,426 +723,6 @@ sudo ostree summary --update --repo "${HTTPD_PATH}/repo"
 # Ensure SELinux is happy with all objects files.
 greenprint "👿 Running restorecon on web server root folder"
 sudo restorecon -Rv "${HTTPD_PATH}/repo" > /dev/null
-
-# Get ostree commit value.
-greenprint "🕹 Get ostree upgrade commit value"
-UPGRADE_HASH=$(curl "${PROD_REPO_URL}/refs/heads/${OSTREE_REF}")
-
-# Upgrade image/commit.
-greenprint "Upgrade ostree image/commit"
-sudo ssh "${SSH_OPTIONS[@]}" -i "${SSH_KEY}" "${SSH_USER}@${GUEST_ADDRESS}" 'sudo rpm-ostree upgrade || { sudo rpm-ostree status; sudo journalctl -b -r -u rpm-ostreed; exit 1; }'
-sudo ssh "${SSH_OPTIONS[@]}" -i "${SSH_KEY}" "${SSH_USER}@${GUEST_ADDRESS}" 'nohup sudo systemctl reboot &>/dev/null & exit'
-# Sleep 10 seconds here to make sure vm restarted already
-sleep 10
-
-# Check for ssh ready to go.
-greenprint "🛃 Checking for SSH is ready to go"
-# shellcheck disable=SC2034  # Unused variables left for readability
-for LOOP_COUNTER in $(seq 0 30); do
-    RESULTS="$(wait_for_ssh_up $GUEST_ADDRESS)"
-    if [[ $RESULTS == 1 ]]; then
-        echo "SSH is ready now! 🥳"
-        break
-    fi
-    sleep 10
-done
-
-# Check ostree upgrade result
-check_result
-
-# Add instance IP address into /etc/ansible/hosts
-sudo tee "${TEMPDIR}"/inventory > /dev/null << EOF
-[ostree_guest]
-${GUEST_ADDRESS}
-
-[ostree_guest:vars]
-ansible_python_interpreter=/usr/bin/python3
-ansible_user=${SSH_USER}
-ansible_private_key_file=${SSH_KEY}
-ansible_ssh_common_args="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
-EOF
-
-# Test IoT/Edge OS
-sudo ansible-playbook -v -i "${TEMPDIR}"/inventory \
-    -e image_type=${IMAGE_TYPE} \
-    -e ostree_commit="${UPGRADE_HASH}" \
-    -e embeded_container="${EMBEDED_CONTAINER}" \
-    -e firewall_feature="${FIREWALL_FEATURE}" \
-    -e test_custom_dirs_files="${DIRS_FILES_CUSTOMIZATION}" \
-    -e sysroot_ro="$SYSROOT_RO" \
-    /usr/share/tests/osbuild-composer/ansible/check_ostree.yaml || RESULTS=0
-check_result
-
-##################################################
-##
-## Upload ostree commit to pulp test
-##
-##################################################
-
-# Pulp test is only for rhel-9.4 and later, as there is no 9.4 in CI, run it on 9.3 first
-if [[ "${SKIP_PULP_TEST}" == "true" ]]; then
-    greenprint "📋 Current OS does not support pulp, skip pulp test and exit"
-    clean_up
-    exit 0
-fi
-
-# Clean up VM and resources
-greenprint "🧹 Clean up VM"
-if [[ $(sudo virsh domstate "${IMAGE_KEY}") == "running" ]]; then
-    sudo virsh destroy "${IMAGE_KEY}"
-fi
-sudo virsh undefine "${IMAGE_KEY}" --nvram
-sudo virsh vol-delete --pool images "$LIBVIRT_IMAGE_PATH"
-
-# Setup pulp server
-greenprint "📄 Setup pulp server with one container"
-mkdir -p settings pulp_storage pgsql containers
-echo "CONTENT_ORIGIN='http://$(hostname):8080'
-ANSIBLE_API_HOSTNAME='http://$(hostname):8080'
-ANSIBLE_CONTENT_HOSTNAME='http://$(hostname):8080/pulp/content'
-CACHE_ENABLED=True" >> settings/settings.py
-sudo podman run --detach \
-            --publish 9090:80 \
-            --name pulp \
-            --volume "$(pwd)/settings":/etc/pulp:Z \
-            --volume "$(pwd)/pulp_storage":/var/lib/pulp:Z \
-            --volume "$(pwd)/pgsql":/var/lib/pgsql:Z \
-            --volume "$(pwd)/containers":/var/lib/containers:Z \
-            --device /dev/fuse \
-            docker.io/pulp/pulp:nightly
-
-# Wait until pulp service is fully functional
-sleep 120
-
-# Rotate pulp admin password
-greenprint "📄 Rotate pulp admin password"
-/usr/bin/expect <<-EOF
-spawn sudo podman exec -it pulp bash -c "pulpcore-manager reset-admin-password"
-expect {
-"*password" { send "${PULP_PASSWORD}\r"; exp_continue }
-"*again" { send "${PULP_PASSWORD}\r" }
-}
-expect eof
-EOF
-
-# Write a pulp config file.
-greenprint "📄 Prepare pulp config file"
-tee "$PULP_CONFIG_FILE" > /dev/null << EOF
-provider = "pulp.ostree"
-
-[settings]
-server_address = "${PULP_SERVER}"
-repository = "${PULP_REPO}"
-basepath = "${PULP_BASEPATH}"
-username = "${PULP_USERNAME}"
-password = "${PULP_PASSWORD}"
-EOF
-
-greenprint "📄 pulp config file:"
-cat "$PULP_CONFIG_FILE"
-
-# Write a blueprint for ostree image.
-tee "$BLUEPRINT_FILE" > /dev/null << EOF
-name = "ostree"
-description = "A base ostree image"
-version = "0.0.1"
-modules = []
-groups = []
-
-[[packages]]
-name = "python3"
-version = "*"
-
-[[packages]]
-name = "sssd"
-version = "*"
-
-[[customizations.user]]
-name = "${SSH_USER}"
-description = "Administrator account"
-password = "\$6\$GRmb7S0p8vsYmXzH\$o0E020S.9JQGaHkszoog4ha4AQVs3sk8q0DvLjSMxoxHBKnB2FBXGQ/OkwZQfW/76ktHd0NX5nls2LPxPuUdl."
-key = "${SSH_KEY_PUB}"
-home = "/home/${SSH_USER}/"
-groups = ["wheel"]
-
-[[containers]]
-source = "quay.io/fedora/fedora:latest"
-
-[[containers]]
-source = "registry.gitlab.com/redhat/services/products/image-builder/ci/osbuild-composer/fedora-minimal@${FEDORA_IMAGE_DIGEST}"
-name = "${FEDORA_LOCAL_NAME}"
-
-[[customizations.directories]]
-path = "/etc/custom_dir/dir1"
-user = 1020
-group = 1020
-mode = "0770"
-ensure_parents = true
-
-[[customizations.files]]
-path = "/etc/systemd/system/custom.service"
-data = "[Unit]\nDescription=Custom service\n[Service]\nType=oneshot\nRemainAfterExit=yes\nExecStart=/usr/bin/false\n[Install]\nWantedBy=multi-user.target\n"
-
-[[customizations.files]]
-path = "/etc/custom_file.txt"
-data = "image builder is the best\n"
-
-[[customizations.directories]]
-path = "/etc/systemd/system/custom.service.d"
-
-[[customizations.files]]
-path = "/etc/systemd/system/custom.service.d/override.conf"
-data = "[Service]\nExecStart=\nExecStart=/usr/bin/cat /etc/custom_file.txt\n"
-
-[customizations.services]
-enabled = ["custom.service"]
-EOF
-
-greenprint "📄 ostree blueprint"
-cat "$BLUEPRINT_FILE"
-
-# Prepare the blueprint for the compose.
-greenprint "📋 Preparing blueprint"
-sudo composer-cli blueprints push "$BLUEPRINT_FILE"
-sudo composer-cli blueprints depsolve ostree
-
-# Build commit image
-build_image pulp ostree "$IMAGE_TYPE" test "$PULP_CONFIG_FILE"
-
-# Pull commit from pulp to local repo
-greenprint "Pull commit from pulp to production repo"
-sudo rm -fr "$PROD_REPO"
-sudo mkdir -p "$PROD_REPO"
-sudo ostree --repo="$PROD_REPO" init --mode=archive
-sudo ostree --repo="$PROD_REPO" remote add --no-gpg-verify edge-pulp http://localhost:9090/pulp/content/${PULP_REPO}/
-sudo ostree --repo="$PROD_REPO" pull --mirror edge-pulp "$OSTREE_REF"
-
-# Clean compose and blueprints.
-greenprint "Clean up osbuild-composer"
-sudo composer-cli compose delete "${COMPOSE_ID}" > /dev/null
-sudo composer-cli blueprints delete ostree > /dev/null
-
-# Ensure SELinux is happy with our new images.
-greenprint "👿 Running restorecon on image directory"
-sudo restorecon -Rv /var/lib/libvirt/images/
-
-# Ensure SELinux is happy with all objects files.
-greenprint "👿 Running restorecon on web server root folder"
-sudo restorecon -Rv "${PROD_REPO}" > /dev/null
-
-# Create qcow2 file for virt install.
-greenprint "Create qcow2 file for virt install"
-LIBVIRT_IMAGE_PATH=/var/lib/libvirt/images/${IMAGE_KEY}-pulp.qcow2
-sudo qemu-img create -f qcow2 "${LIBVIRT_IMAGE_PATH}" 20G
-
-# Write kickstart file for ostree image installation.
-greenprint "Generate kickstart file"
-tee "$KS_FILE" > /dev/null << STOPHERE
-text
-lang en_US.UTF-8
-keyboard us
-timezone --utc Etc/UTC
-
-selinux --enforcing
-rootpw --lock --iscrypted locked
-user --name=${SSH_USER} --groups=wheel --iscrypted --password=\$6\$1LgwKw9aOoAi/Zy9\$Pn3ErY1E8/yEanJ98evqKEW.DZp24HTuqXPJl6GYCm8uuobAmwxLv7rGCvTRZhxtcYdmC0.XnYRSR9Sh6de3p0
-sshkey --username=${SSH_USER} "${SSH_KEY_PUB}"
-
-bootloader --timeout=1 --append="net.ifnames=0 modprobe.blacklist=vc4"
-
-network --bootproto=dhcp --device=link --activate --onboot=on
-
-zerombr
-clearpart --all --initlabel --disklabel=msdos
-autopart --nohome --noswap --type=plain
-ostreesetup --nogpg --osname=${IMAGE_TYPE} --remote=${IMAGE_TYPE} --url=${PROD_REPO_URL} --ref=${OSTREE_REF}
-poweroff
-
-%post --log=/var/log/anaconda/post-install.log --erroronfail
-
-# no sudo password for SSH user
-echo -e '${SSH_USER}\tALL=(ALL)\tNOPASSWD: ALL' >> /etc/sudoers
-
-# Remove any persistent NIC rules generated by udev
-rm -vf /etc/udev/rules.d/*persistent-net*.rules
-# And ensure that we will do DHCP on eth0 on startup
-cat > /etc/sysconfig/network-scripts/ifcfg-eth0 << EOF
-DEVICE="eth0"
-BOOTPROTO="dhcp"
-ONBOOT="yes"
-TYPE="Ethernet"
-PERSISTENT_DHCLIENT="yes"
-EOF
-
-echo "Packages within this iot or edge image:"
-echo "-----------------------------------------------------------------------"
-rpm -qa | sort
-echo "-----------------------------------------------------------------------"
-# Note that running rpm recreates the rpm db files which aren't needed/wanted
-rm -f /var/lib/rpm/__db*
-
-echo "Zeroing out empty space."
-# This forces the filesystem to reclaim space from deleted files
-dd bs=1M if=/dev/zero of=/var/tmp/zeros || :
-rm -f /var/tmp/zeros
-echo "(Don't worry -- that out-of-space error was expected.)"
-
-%end
-STOPHERE
-
-sudo sed -i '/^user\|^sshkey/d' "${KS_FILE}"
-
-# Install ostree image via anaconda.
-greenprint "Install ostree image via anaconda"
-sudo virt-install  --initrd-inject="${KS_FILE}" \
-                   --extra-args="inst.ks=file:/ks.cfg console=ttyS0,115200" \
-                   --name="${IMAGE_KEY}"\
-                   --disk path="${LIBVIRT_IMAGE_PATH}",format=qcow2 \
-                   --ram 3072 \
-                   --vcpus 2 \
-                   --network network=integration,mac=34:49:22:B0:83:30 \
-                   --os-variant ${OS_VARIANT} \
-                   --location ${location_arg} \
-                   --nographics \
-                   --noautoconsole \
-                   --wait=-1 \
-                   --noreboot
-
-# Start VM.
-greenprint "Start VM"
-sudo virsh start "${IMAGE_KEY}"
-
-# Check for ssh ready to go.
-greenprint "🛃 Checking for SSH is ready to go"
-for LOOP_COUNTER in $(seq 0 30); do
-    RESULTS="$(wait_for_ssh_up $GUEST_ADDRESS)"
-    if [[ $RESULTS == 1 ]]; then
-        echo "SSH is ready now! 🥳"
-        break
-    fi
-    sleep 10
-done
-
-# With new ostree-libs-2022.6-3, edge vm needs to reboot twice to make the /sysroot readonly
-sudo ssh "${SSH_OPTIONS[@]}" -i "${SSH_KEY}" "${SSH_USER}@${GUEST_ADDRESS}" 'nohup sudo systemctl reboot &>/dev/null & exit'
-# Sleep 10 seconds here to make sure vm restarted already
-sleep 10
-
-# Check for ssh ready to go.
-greenprint "🛃 Checking for SSH is ready to go"
-for LOOP_COUNTER in $(seq 0 30); do
-    RESULTS="$(wait_for_ssh_up $GUEST_ADDRESS)"
-    if [[ $RESULTS == 1 ]]; then
-        echo "SSH is ready now! 🥳"
-        break
-    fi
-    sleep 10
-done
-
-# Check image installation result
-check_result
-
-##################################################
-##
-## Upload upgrade ostree commit to pulp test
-##
-##################################################
-
-# Write a blueprint for ostree image.
-tee "$BLUEPRINT_FILE" > /dev/null << EOF
-name = "upgrade"
-description = "An upgrade ostree image"
-version = "0.0.2"
-modules = []
-groups = []
-
-[[packages]]
-name = "python3"
-version = "*"
-
-[[packages]]
-name = "sssd"
-version = "*"
-
-[[packages]]
-name = "wget"
-version = "*"
-
-[[customizations.user]]
-name = "${SSH_USER}"
-description = "Administrator account"
-password = "\$6\$GRmb7S0p8vsYmXzH\$o0E020S.9JQGaHkszoog4ha4AQVs3sk8q0DvLjSMxoxHBKnB2FBXGQ/OkwZQfW/76ktHd0NX5nls2LPxPuUdl."
-key = "${SSH_KEY_PUB}"
-home = "/home/${SSH_USER}/"
-groups = ["wheel"]
-
-[[containers]]
-source = "quay.io/fedora/fedora:latest"
-
-[[containers]]
-source = "registry.gitlab.com/redhat/services/products/image-builder/ci/osbuild-composer/fedora-minimal@${FEDORA_IMAGE_DIGEST}"
-name = "${FEDORA_LOCAL_NAME}"
-
-[[customizations.firewall.zones]]
-name = "trusted"
-sources = ["192.168.100.51"]
-[[customizations.firewall.zones]]
-name = "work"
-sources = ["192.168.100.52"]
-
-[[customizations.directories]]
-path = "/etc/custom_dir/dir1"
-user = 1020
-group = 1020
-mode = "0770"
-ensure_parents = true
-
-[[customizations.files]]
-path = "/etc/systemd/system/custom.service"
-data = "[Unit]\nDescription=Custom service\n[Service]\nType=oneshot\nRemainAfterExit=yes\nExecStart=/usr/bin/false\n[Install]\nWantedBy=multi-user.target\n"
-
-[[customizations.files]]
-path = "/etc/custom_file.txt"
-data = "image builder is the best\n"
-
-[[customizations.directories]]
-path = "/etc/systemd/system/custom.service.d"
-
-[[customizations.files]]
-path = "/etc/systemd/system/custom.service.d/override.conf"
-data = "[Service]\nExecStart=\nExecStart=/usr/bin/cat /etc/custom_file.txt\n"
-
-[customizations.services]
-enabled = ["custom.service"]
-EOF
-
-greenprint "📄 upgrade blueprint"
-cat "$BLUEPRINT_FILE"
-
-# Prepare the blueprint for the compose.
-greenprint "📋 Preparing blueprint"
-sudo composer-cli blueprints push "$BLUEPRINT_FILE"
-sudo composer-cli blueprints depsolve upgrade
-
-greenprint "🕹 Get ostree installed commit value"
-PARENT_HASH=$(curl "${PROD_REPO_URL}/refs/heads/${OSTREE_REF}")
-
-# Build upgrade image.
-build_image pulp upgrade "$IMAGE_TYPE" test "$PULP_CONFIG_FILE" "$PROD_REPO_URL" "$PARENT_HASH"
-
-# Clean compose and blueprints.
-greenprint "Clean up osbuild-composer again"
-sudo composer-cli compose delete "${COMPOSE_ID}" > /dev/null
-sudo composer-cli blueprints delete upgrade > /dev/null
-
-# Pull content from pulp to local repo
-greenprint "Pull upgrade commit from pulp to local repo"
-sudo ostree --repo="$PROD_REPO" pull --mirror edge-pulp "$OSTREE_REF"
-sudo ostree --repo="$PROD_REPO" summary -u
-
-# Ensure SELinux is happy with all objects files.
-greenprint "👿 Running restorecon on web server root folder"
-sudo restorecon -Rv "${PROD_REPO}" > /dev/null
 
 # Get ostree commit value.
 greenprint "🕹 Get ostree upgrade commit value"
