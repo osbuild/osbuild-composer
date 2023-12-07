da0b668de277b2adae9a1247c02c50598714f700
Alexander Todorov <atodorov@redhat.com>
2023-12-07 17:00:32 +0200

    Fix a boolean condition in filesystem.sh

--- test/cases/filesystem.sh
+++ test/cases/filesystem.sh
@@ -181,7 +181,7 @@ mountpoint = "/boot"
 size = 131072000
 EOF
 
-if not nvrGreaterOrEqual "osbuild-composer" "94"; then
+if ! nvrGreaterOrEqual "osbuild-composer" "94"; then
     tee -a "$BLUEPRINT_FILE" > /dev/null << EOF
 
 [[customizations.filesystem]]
@@ -289,7 +289,7 @@ mountpoint = "/sysroot"
 size = 131072000
 EOF
 
-if not nvrGreaterOrEqual "osbuild-composer" "94"; then
+if ! nvrGreaterOrEqual "osbuild-composer" "94"; then
     tee -a "$BLUEPRINT_FILE" > /dev/null << EOF
 
 [[customizations.filesystem]]
@@ -319,7 +319,7 @@ for MOUNTPOINT in '/etc' '/sys' '/proc' '/dev' '/run' '/bin' '/sbin' '/lib' '/li
   fi
 done
 
-if not nvrGreaterOrEqual "osbuild-composer" "94"; then
+if ! nvrGreaterOrEqual "osbuild-composer" "94"; then
   for MOUNTPOINT in '/usr/bin' '/var/run' '/var/lock'; do
     if ! [[ $ERROR_MSG == *"$MOUNTPOINT"* ]]; then
       FAILED_MOUNTPOINTS+=("$MOUNTPOINT")
