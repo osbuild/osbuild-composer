9f5f468e7c70d2767c4508158ea2c6fcbed0dc18
Tomáš Hozza <thozza@redhat.com>
2024-01-10 22:10:22 +0100

    composer: use distro name aliases from configuration
    Register the distro name aliases from the configuration on the distro
    factory.
    
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

--- cmd/osbuild-composer/composer.go
+++ cmd/osbuild-composer/composer.go
@@ -69,9 +69,10 @@ func NewComposer(config *ComposerConfigFile, stateDir, cacheDir string) (*Compos
 	}
 
 	c.distros = distrofactory.NewDefault()
-	// TODO: set the c.config.DistroAliases to the distrofactory
-	// More work is needed to make the distro aliases behavior consistent for Weldr API,
-	// specifically for picking the correct repositories definition.
+	err = c.distros.RegisterAliases(config.DistroAliases)
+	if err != nil {
+		return nil, fmt.Errorf("failed to configure distro aliases: %v", err)
+	}
 
 	c.solver = dnfjson.NewBaseSolver(path.Join(c.cacheDir, "rpmmd"))
 	c.solver.SetDNFJSONPath(c.config.DNFJson)
