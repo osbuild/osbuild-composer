6a0c893a4dd78e7c6535871a1e243df154015b66
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2024-01-18 13:16:48 +0100

    tools/appsre-packer: add script to build fedora worker images

---
+++ tools/appsre-build-fedora-worker-packer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# AppSRE runs this script to build an ami and share it with an account
+set -exv
+
+export SKIP_CREATE_AMI=false
+# Use prebuilt rpms for the fedora images
+export BUILD_RPMS=false
+export SKIP_TAGS="rpmcopy,subscribe"
+export PACKER_ONLY_EXCEPT=--except=amazon-ebs.fedora-38-x86_64,amazon-ebs.fedora-38-aarch64
+
+tools/appsre-build-worker-packer.sh
