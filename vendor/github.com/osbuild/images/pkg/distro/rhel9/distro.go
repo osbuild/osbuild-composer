9a8a936327172b1327af41648a23ea832eabb35e
Tomáš Hozza <thozza@redhat.com>
2024-02-05 14:58:31 +0100

    Update osbuild/images to v0.35.0
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

--- vendor/github.com/osbuild/images/pkg/distro/rhel9/distro.go
+++ vendor/github.com/osbuild/images/pkg/distro/rhel9/distro.go
@@ -474,14 +474,17 @@ func ParseID(idStr string) (*distro.ID, error) {
 		return nil, fmt.Errorf("invalid distro name: %s", id.Name)
 	}
 
-	// Backward compatibility layer for "rhel-93" and friends
+	// Backward compatibility layer for "rhel-93" or "rhel-910"
 	if id.Name == "rhel" && id.MinorVersion == -1 {
 		if id.MajorVersion/10 == 9 {
 			// handle single digit minor version
 			id.MinorVersion = id.MajorVersion % 10
 			id.MajorVersion = 9
+		} else if id.MajorVersion/100 == 9 {
+			// handle two digit minor version
+			id.MinorVersion = id.MajorVersion % 100
+			id.MajorVersion = 9
 		}
-		// two digit minor versions in the old format are not supported
 	}
 
 	if id.MajorVersion != 9 {
