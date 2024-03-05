f02fe86366ad0108b5fb88e0ad14e3b33d856db5
Tomáš Hozza <thozza@redhat.com>
2024-03-05 14:09:59 +0100

    Packer: make osbuild-executor worker config more configurable
    We need to use custom IAM policy name used by the worker for
    osbuild-executor on Fedora workers (in prod vs. stage). And we have the
    same requirement for the CloudWatch log group used by the
    osbuild-executor.
    
    Modify the Ansible playbook used by Packer to use the values from
    /tmp/cloud_init_vars if set and defaulting to the current values if not
    set.
    
    Signed-off-by: Tomáš Hozza <thozza@redhat.com>

--- templates/packer/ansible/roles/common/files/worker-initialization-scripts/worker_executor.sh
+++ templates/packer/ansible/roles/common/files/worker-initialization-scripts/worker_executor.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-# Don't subscribe on fedora
 source /etc/os-release
+source /tmp/cloud_init_vars
+
+# Don't subscribe on fedora
 if [ "$ID" != fedora ]; then
   /usr/local/bin/aws secretsmanager get-secret-value \
     --secret-id executor-subscription-manager-command | jq -r ".SecretString" > /tmp/subscription_manager_command.json
@@ -14,6 +16,8 @@ echo "Writing vector config."
 REGION=$(curl -Ls http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
 HOSTNAME=$(hostname)
 CLOUDWATCH_ENDPOINT="https://logs.$REGION.amazonaws.com"
+OSBUILD_EXECUTOR_CLOUDWATCH_GROUP=${OSBUILD_EXECUTOR_CLOUDWATCH_GROUP:-osbuild-executor-log-group}
+
 sudo mkdir -p /etc/vector
 sudo tee /etc/vector/vector.toml > /dev/null << EOF
 [sources.journald]
@@ -25,7 +29,7 @@ type = "aws_cloudwatch_logs"
 inputs = [ "journald" ]
 region = "${REGION}"
 endpoint = "${CLOUDWATCH_ENDPOINT}"
-group_name = "osbuild-executor-log-group"
+group_name = "${OSBUILD_EXECUTOR_CLOUDWATCH_GROUP}"
 stream_name = "osbuild_executor_syslog_${HOSTNAME}"
 encoding.codec = "json"
 EOF
