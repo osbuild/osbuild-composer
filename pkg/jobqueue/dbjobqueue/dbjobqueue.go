d77c25a6ee3dbb0420110f5191d095ae8bed01f3
Brian C. Lane <bcl@redhat.com>
2024-04-10 11:47:41 -0700

    jobqueue: Add AllRootJobIDs function to jobqueue
    This lists the root job UUIDs (the jobs with no dependants).
    Currently only implemented by fsjobqueue. The function for
    dbjobqueue currently returns nil.
    
    Related: RHEL-60120

--- pkg/jobqueue/dbjobqueue/dbjobqueue.go
+++ pkg/jobqueue/dbjobqueue/dbjobqueue.go
@@ -871,3 +871,10 @@ func (q *DBJobQueue) jobDependents(ctx context.Context, conn connection, id uuid
 
 	return dependents, nil
 }
+
+// AllRootJobIDs returns a list of top level job UUIDs that the worker knows about
+func (q *DBJobQueue) AllRootJobIDs() ([]uuid.UUID, error) {
+	// TODO write this
+
+	return nil, nil
+}
