5121859ef659297b5d005911bde62cb939e4eb0f
Brian C. Lane <bcl@redhat.com>
2024-04-16 10:46:28 -0700

    jobqueue: Add RemoveJob function
    This allows jobs to be removed from the database.
    Currently only implemented by fsjobqueue. The function for
    dbjobqueue currently returns nil.
    
    This will remove all the job files used by the root job UUID as long as
    no other job depends on them. ie. It starts at the top, and moves down
    the dependency tree until it finds a job that is also used by another
    job.

--- internal/jobqueue/fsjobqueue/fsjobqueue_test.go
+++ internal/jobqueue/fsjobqueue/fsjobqueue_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/osbuild/osbuild-composer/internal/jobqueue/fsjobqueue"
@@ -118,4 +119,53 @@ func TestAllJobIDs(t *testing.T) {
 	require.Greater(t, len(jobs), 0)
 	sortUUIDs(jobs)
 	require.Equal(t, allJobs, jobs)
+}
+
+func TestRemoveJob(t *testing.T) {
+	dir := t.TempDir()
+	q, err := fsjobqueue.New(dir)
+	require.Nil(t, err)
+	require.NotNil(t, q)
+
+	// root with no dependencies
+	jidRoot1, err := q.Enqueue("oneRoot", nil, nil, "OneRootJob")
+	require.Nil(t, err)
+
+	err = q.RemoveJob(jidRoot1)
+	require.Nil(t, err)
+	jobs, err := q.AllJobIDs()
+	require.Nil(t, err)
+	require.Equal(t, 0, len(jobs))
+
+	// root with 2 dependencies
+	jid1, err := q.Enqueue("twoDeps", nil, nil, "TwoDepJobs")
+	require.Nil(t, err)
+	jid2, err := q.Enqueue("twoDeps", nil, nil, "TwoDepJobs")
+	require.Nil(t, err)
+	jidRoot2, err := q.Enqueue("twoDeps", nil, []uuid.UUID{jid1, jid2}, "TwoDepJobs")
+	require.Nil(t, err)
+
+	// root with 2 dependencies, one shared with the previous root
+	jid3, err := q.Enqueue("sharedDeps", nil, nil, "SharedDepJobs")
+	require.Nil(t, err)
+	jidRoot3, err := q.Enqueue("sharedDeps", nil, []uuid.UUID{jid1, jid3}, "SharedDepJobs")
+	require.Nil(t, err)
+
+	// This should only remove jidRoot2 and jid2, leaving jidRoot3, jid1, jid3
+	err = q.RemoveJob(jidRoot2)
+	require.Nil(t, err)
+	jobs, err = q.AllJobIDs()
+	require.Nil(t, err)
+	require.Equal(t, 3, len(jobs))
+	sortUUIDs(jobs)
+	expected := []uuid.UUID{jidRoot3, jid1, jid3}
+	sortUUIDs(expected)
+	assert.Equal(t, expected, jobs)
+
+	// This should remove the rest
+	err = q.RemoveJob(jidRoot3)
+	require.Nil(t, err)
+	jobs, err = q.AllJobIDs()
+	require.Nil(t, err)
+	require.Equal(t, 0, len(jobs))
 }
