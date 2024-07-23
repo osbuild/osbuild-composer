0be8764a799609c0c9f052e2dc6743e0191f5177
Tom Koscielniak <tkosciel@redhat.com>
2024-07-23 09:29:17 +0200

    Fix slack GA pipeline notification job failure

--- schutzbot/slack_notification.sh
+++ schutzbot/slack_notification.sh
@@ -7,11 +7,11 @@ if [ -z "${SLACK_WEBHOOK_URL:-}" ]; then
     exit 0
 fi
 
-COMPOSE_ID=$(cat COMPOSE_ID)
-COMPOSER_NVR=$(cat COMPOSER_NVR)
 if [ "$3" == "ga" ]; then
     MESSAGE="\"GA composes pipeline execution finished with status *$1* $2 \n QE: @atodorov, @jrusz, @tkosciel\n Link to results: $CI_PIPELINE_URL \""
 else
+    COMPOSE_ID=$(cat COMPOSE_ID)
+    COMPOSER_NVR=$(cat COMPOSER_NVR)
     MESSAGE="\"Nightly pipeline execution on *$COMPOSE_ID* with *$COMPOSER_NVR* finished with status *$1* $2 \n QE: @atodorov, @jrusz, @tkosciel\n Link to results: $CI_PIPELINE_URL\n For edge testing status please see https://url.corp.redhat.com/edge-pipelines \""
 fi
 
