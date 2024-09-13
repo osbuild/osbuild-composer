bec66607b40447e9f35b8e0b996b188208911c03
Achilleas Koutsou <achilleas@koutsou.net>
2024-09-13 17:03:56 +0200

    blueprint: copy new partitioning configs from osbuild/images
    Add the new partitioning configs introduced in osbuild/images to the
    internal blueprint.

--- internal/blueprint/filesystem_customizations_test.go
+++ internal/blueprint/filesystem_customizations_test.go
@@ -2,6 +2,7 @@ package blueprint
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/BurntSushi/toml"
@@ -269,4 +270,389 @@ path "/foo" is not allowed
 path "/boot/" must be canonical`
 	err := blueprint.CheckMountpointsPolicy(mps, policy)
 	assert.EqualError(t, err, expectedErr)
+}
+
+func TestPartitioningValidation(t *testing.T) {
+	type testCase struct {
+		partitioning *blueprint.PartitioningCustomization
+		expected     error
+	}
+
+	testCases := map[string]testCase{
+		"null": {
+			partitioning: nil,
+			expected:     nil,
+		},
+		"happy-plain": {
+			partitioning: &blueprint.PartitioningCustomization{
+				Plain: &blueprint.PlainFilesystemCustomization{
+					Filesystems: []blueprint.FilesystemCustomization{
+						{
+							Mountpoint: "/data",
+						},
+					},
+				},
+			},
+			expected: nil,
+		},
+		"happy-plain+btrfs": {
+			partitioning: &blueprint.PartitioningCustomization{
+				Plain: &blueprint.PlainFilesystemCustomization{
+					Filesystems: []blueprint.FilesystemCustomization{
+						{
+							Mountpoint: "/data",
+						},
+					},
+				},
+				Btrfs: &blueprint.BtrfsCustomization{
+					Volumes: []blueprint.BtrfsVolumeCustomization{
+						{
+							Subvolumes: []blueprint.BtrfsSubvolumeCustomization{
+								{
+									Name:       "root",
+									Mountpoint: "/",
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: nil,
+		},
+		"happy-plain+lvm": {
+			partitioning: &blueprint.PartitioningCustomization{
+				Plain: &blueprint.PlainFilesystemCustomization{
+					Filesystems: []blueprint.FilesystemCustomization{
+						{
+							Mountpoint: "/data",
+						},
+					},
+				},
+				LVM: &blueprint.LVMCustomization{
+					VolumeGroups: []blueprint.VGCustomization{
+						{
+							LogicalVolumes: []blueprint.LVCustomization{
+								{
+									FilesystemCustomization: blueprint.FilesystemCustomization{Mountpoint: "/"},
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: nil,
+		},
+		"unhappy-btrfs+lvm": {
+			partitioning: &blueprint.PartitioningCustomization{
+				Plain: &blueprint.PlainFilesystemCustomization{
+					Filesystems: []blueprint.FilesystemCustomization{
+						{
+							Mountpoint: "/data",
+						},
+					},
+				},
+				Btrfs: &blueprint.BtrfsCustomization{
+					Volumes: []blueprint.BtrfsVolumeCustomization{
+						{
+							Subvolumes: []blueprint.BtrfsSubvolumeCustomization{
+								{
+									Mountpoint: "/backup",
+								},
+							},
+						},
+					},
+				},
+				LVM: &blueprint.LVMCustomization{
+					VolumeGroups: []blueprint.VGCustomization{
+						{
+							LogicalVolumes: []blueprint.LVCustomization{
+								{
+									FilesystemCustomization: blueprint.FilesystemCustomization{Mountpoint: "/"},
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: fmt.Errorf("btrfs and lvm partitioning cannot be combined"),
+		},
+		"unhappy-plain-dupes": {
+			partitioning: &blueprint.PartitioningCustomization{
+				Plain: &blueprint.PlainFilesystemCustomization{
+					Filesystems: []blueprint.FilesystemCustomization{
+						{
+							Mountpoint: "/data",
+						},
+						{
+							Mountpoint: "/",
+						},
+						{
+							Mountpoint: "/home",
+						},
+						{
+							Mountpoint: "/data",
+						},
+					},
+				},
+			},
+			expected: fmt.Errorf("duplicate mountpoint \"/data\" in partitioning customizations"),
+		},
+		"unhappy-plain+btrfs-dupes": {
+			partitioning: &blueprint.PartitioningCustomization{
+				Plain: &blueprint.PlainFilesystemCustomization{
+					Filesystems: []blueprint.FilesystemCustomization{
+						{
+							Mountpoint: "/data",
+						},
+					},
+				},
+				Btrfs: &blueprint.BtrfsCustomization{
+					Volumes: []blueprint.BtrfsVolumeCustomization{
+						{
+							Subvolumes: []blueprint.BtrfsSubvolumeCustomization{
+								{
+									Name:       "root",
+									Mountpoint: "/",
+								},
+								{
+									Name:       "home",
+									Mountpoint: "/home",
+								},
+								{
+									Name:       "data",
+									Mountpoint: "/data",
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: fmt.Errorf("duplicate mountpoint \"/data\" in partitioning customizations"),
+		},
+		"unhappy-plain+lvm-dupes": {
+			partitioning: &blueprint.PartitioningCustomization{
+				Plain: &blueprint.PlainFilesystemCustomization{
+					Filesystems: []blueprint.FilesystemCustomization{
+						{
+							Mountpoint: "/dupydupe",
+						},
+						{
+							Mountpoint: "/data",
+						},
+					},
+				},
+				LVM: &blueprint.LVMCustomization{
+					VolumeGroups: []blueprint.VGCustomization{
+						{
+							LogicalVolumes: []blueprint.LVCustomization{
+								{
+									FilesystemCustomization: blueprint.FilesystemCustomization{Mountpoint: "/"},
+								},
+								{
+									FilesystemCustomization: blueprint.FilesystemCustomization{Mountpoint: "/home"},
+								},
+								{
+									FilesystemCustomization: blueprint.FilesystemCustomization{Mountpoint: "/dupydupe"},
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: fmt.Errorf("duplicate mountpoint \"/dupydupe\" in partitioning customizations"),
+		},
+		"unhappy-multibtrfs": {
+			partitioning: &blueprint.PartitioningCustomization{
+				Plain: &blueprint.PlainFilesystemCustomization{
+					Filesystems: []blueprint.FilesystemCustomization{
+						{
+							Mountpoint: "/data",
+						},
+					},
+				},
+				Btrfs: &blueprint.BtrfsCustomization{
+					Volumes: []blueprint.BtrfsVolumeCustomization{
+						{
+							Subvolumes: []blueprint.BtrfsSubvolumeCustomization{
+								{
+									Name:       "root",
+									Mountpoint: "/",
+								},
+							},
+						},
+						{
+							Subvolumes: []blueprint.BtrfsSubvolumeCustomization{
+								{
+									Name:       "home",
+									Mountpoint: "/home",
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: fmt.Errorf("multiple btrfs volumes are not yet supported"),
+		},
+		"unhappy-multivg": {
+			partitioning: &blueprint.PartitioningCustomization{
+				Plain: &blueprint.PlainFilesystemCustomization{
+					Filesystems: []blueprint.FilesystemCustomization{
+						{
+							Mountpoint: "/data",
+						},
+					},
+				},
+				LVM: &blueprint.LVMCustomization{
+					VolumeGroups: []blueprint.VGCustomization{
+						{
+							LogicalVolumes: []blueprint.LVCustomization{
+								{
+									FilesystemCustomization: blueprint.FilesystemCustomization{Mountpoint: "/"},
+								},
+							},
+						},
+						{
+							LogicalVolumes: []blueprint.LVCustomization{
+								{
+									FilesystemCustomization: blueprint.FilesystemCustomization{Mountpoint: "/var/log"},
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: fmt.Errorf("multiple LVM volume groups are not yet supported"),
+		},
+		"unhappy-emptymp": {
+			partitioning: &blueprint.PartitioningCustomization{
+				Plain: &blueprint.PlainFilesystemCustomization{
+					Filesystems: []blueprint.FilesystemCustomization{
+						{},
+					},
+				},
+			},
+			expected: fmt.Errorf("filesystem with empty mountpoint in partitioning customizations"),
+		},
+		"unhappy-emptymp-btrfs": {
+			partitioning: &blueprint.PartitioningCustomization{
+				Btrfs: &blueprint.BtrfsCustomization{
+					Volumes: []blueprint.BtrfsVolumeCustomization{
+						{
+							Subvolumes: []blueprint.BtrfsSubvolumeCustomization{
+								{
+									Name:       "test",
+									Mountpoint: "/test",
+								},
+								{
+									Name:       "test2",
+									Mountpoint: "",
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: fmt.Errorf("btrfs subvolume with empty mountpoint in partitioning customizations"),
+		},
+		"unhappy-emptymp-lvm": {
+			partitioning: &blueprint.PartitioningCustomization{
+				LVM: &blueprint.LVMCustomization{
+					VolumeGroups: []blueprint.VGCustomization{
+						{
+							LogicalVolumes: []blueprint.LVCustomization{
+								{
+									Name: "testlv",
+									FilesystemCustomization: blueprint.FilesystemCustomization{
+										Mountpoint: "/stuff",
+									},
+								},
+								{
+									Name: "testlv2",
+									FilesystemCustomization: blueprint.FilesystemCustomization{
+										Mountpoint: "",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: fmt.Errorf("logical volume with empty mountpoint in partitioning customizations"),
+		},
+		"unhappy-dupesubvolname": {
+			partitioning: &blueprint.PartitioningCustomization{
+				Btrfs: &blueprint.BtrfsCustomization{
+					Volumes: []blueprint.BtrfsVolumeCustomization{
+						{
+							Subvolumes: []blueprint.BtrfsSubvolumeCustomization{
+								{
+									Name:       "root",
+									Mountpoint: "/",
+								},
+								{
+									Name:       "root",
+									Mountpoint: "/root",
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: fmt.Errorf("duplicate btrfs subvolume name \"root\" in partitioning customizations"),
+		},
+		"unhappy-dupelvname": {
+			partitioning: &blueprint.PartitioningCustomization{
+				LVM: &blueprint.LVMCustomization{
+					VolumeGroups: []blueprint.VGCustomization{
+						{
+							LogicalVolumes: []blueprint.LVCustomization{
+								{
+									Name: "testlv",
+									FilesystemCustomization: blueprint.FilesystemCustomization{
+										Mountpoint: "/stuff",
+									},
+								},
+								{
+									Name: "testlv",
+									FilesystemCustomization: blueprint.FilesystemCustomization{
+										Mountpoint: "/stuff2",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: fmt.Errorf("duplicate lvm logical volume name \"testlv\" in volume group \"\" in partitioning customizations"),
+		},
+		"unhappy-emptyname-btrfs": {
+			partitioning: &blueprint.PartitioningCustomization{
+				Btrfs: &blueprint.BtrfsCustomization{
+					Volumes: []blueprint.BtrfsVolumeCustomization{
+						{
+							Subvolumes: []blueprint.BtrfsSubvolumeCustomization{
+								{
+									Name:       "test",
+									Mountpoint: "/test",
+								},
+								{
+									Name:       "",
+									Mountpoint: "/test2",
+								},
+							},
+						},
+					},
+				},
+			},
+			expected: fmt.Errorf("btrfs subvolume with empty name in partitioning customizations"),
+		},
+	}
+
+	for name := range testCases {
+		tc := testCases[name]
+		t.Run(name, func(t *testing.T) {
+			err := tc.partitioning.Validate()
+			assert.Equal(t, tc.expected, err)
+		})
+	}
 }
