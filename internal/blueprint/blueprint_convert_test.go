009f8b72a43c3f9de136b0e4dea0f5e59724bc16
Achilleas Koutsou <achilleas@koutsou.net>
2024-10-21 18:35:49 +0200

    blueprint: add kickstart contents to conversion test
    The option was added in f5c6cdd9cfc6d38a375b2f38d868d520399b668d but a
    value was never added to the conversion test.

--- internal/blueprint/blueprint_convert_test.go
+++ internal/blueprint/blueprint_convert_test.go
@@ -173,6 +173,9 @@ func TestConvert(t *testing.T) {
 					Installer: &InstallerCustomization{
 						Unattended:   true,
 						SudoNopasswd: []string{"%group", "user"},
+						Kickstart: &Kickstart{
+							Contents: "# test kickstart addition created by osbuild-composer",
+						},
 						Modules: &AnacondaModules{
 							Enable: []string{
 								"org.fedoraproject.Anaconda.Modules.Localization",
@@ -360,6 +363,9 @@ func TestConvert(t *testing.T) {
 					Installer: &iblueprint.InstallerCustomization{
 						Unattended:   true,
 						SudoNopasswd: []string{"%group", "user"},
+						Kickstart: &iblueprint.Kickstart{
+							Contents: "# test kickstart addition created by osbuild-composer",
+						},
 						Modules: &iblueprint.AnacondaModules{
 							Enable: []string{
 								"org.fedoraproject.Anaconda.Modules.Localization",
