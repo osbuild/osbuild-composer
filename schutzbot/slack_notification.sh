f37acdd60f6274df993c961b2bfbca3f0fc2b09c
Jakub Rusz <jrusz@redhat.com>
2024-12-02 12:18:02 +0100

    CI: remove jrusz from notifications
    Removing myself from the slack notifications as I'm no longer a QE
    member.

--- schutzbot/slack_notification.sh
+++ schutzbot/slack_notification.sh
@@ -8,11 +8,11 @@ if [ -z "${SLACK_WEBHOOK_URL:-}" ]; then
 fi
 
 if [ "$3" == "ga" ]; then
-    MESSAGE="\"<$CI_PIPELINE_URL|GA composes pipeline>: *$1* $2, cc <@U01CUGX9L68>, <@U01Q07AHZ9C>, <@U04PYMDRV5H>\""
+    MESSAGE="\"<$CI_PIPELINE_URL|GA composes pipeline>: *$1* $2, cc <@U01CUGX9L68>, <@U04PYMDRV5H>\""
 else
     COMPOSE_ID=$(cat COMPOSE_ID)
     COMPOSER_NVR=$(cat COMPOSER_NVR)
-    MESSAGE="\"<$CI_PIPELINE_URL|Nightly pipeline> ($COMPOSE_ID: $COMPOSER_NVR): *$1* $2, cc <@U01CUGX9L68>, <@U01Q07AHZ9C>, <@U04PYMDRV5H>\""
+    MESSAGE="\"<$CI_PIPELINE_URL|Nightly pipeline> ($COMPOSE_ID: $COMPOSER_NVR): *$1* $2, cc <@U01CUGX9L68>, <@U04PYMDRV5H>\""
 fi
 
 curl \
