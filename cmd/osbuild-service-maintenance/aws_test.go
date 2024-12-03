a96ea533c08b32c763e0136a2f505f83f2140f80
Florian Schüller <florian.schueller@redhat.com>
2024-12-03 17:55:59 +0100

    osbuild-service-maintenance: implement removal of security groups
    Security groups of instances that are terminated should be removed.
    HMS-3632

--- cmd/osbuild-service-maintenance/aws_test.go
+++ cmd/osbuild-service-maintenance/aws_test.go
@@ -17,7 +17,7 @@ func TestFilterReservations(t *testing.T) {
 			Instances: []ec2types.Instance{
 				{
 					LaunchTime: common.ToPtr(time.Now().Add(-time.Hour * 24)),
-					InstanceId: common.ToPtr("not filtered"),
+					InstanceId: common.ToPtr("not filtered 1"),
 				},
 			},
 		},
@@ -25,7 +25,7 @@ func TestFilterReservations(t *testing.T) {
 			Instances: []ec2types.Instance{
 				{
 					LaunchTime: common.ToPtr(time.Now().Add(-time.Minute * 121)),
-					InstanceId: common.ToPtr("not filtered"),
+					InstanceId: common.ToPtr("not filtered 2"),
 				},
 			},
 		},
@@ -33,7 +33,7 @@ func TestFilterReservations(t *testing.T) {
 			Instances: []ec2types.Instance{
 				{
 					LaunchTime: common.ToPtr(time.Now().Add(-time.Minute * 119)),
-					InstanceId: common.ToPtr("filtered"),
+					InstanceId: common.ToPtr("filtered 1"),
 				},
 			},
 		},
@@ -41,12 +41,87 @@ func TestFilterReservations(t *testing.T) {
 			Instances: []ec2types.Instance{
 				{
 					LaunchTime: common.ToPtr(time.Now()),
-					InstanceId: common.ToPtr("filtered"),
+					InstanceId: common.ToPtr("filtered 2"),
 				},
 			},
 		},
 	}
 
-	instanceIDs := main.FilterReservations(reservations)
-	require.Equal(t, []string{"not filtered", "not filtered"}, instanceIDs)
+	instanceIDs := main.FilterOnTooOld([]string{}, reservations)
+	require.Equal(t, []string{"not filtered 1", "not filtered 2"}, instanceIDs)
+}
+
+func TestCheckValidParent(t *testing.T) {
+	testInstanceID := "TestInstance"
+	tests :=
+		[]struct {
+			parent []ec2types.Reservation
+			result bool
+		}{
+			// no parent
+			{
+				parent: []ec2types.Reservation{},
+				result: false,
+			},
+			// many parents - "valid" to leave as is
+			{
+				parent: []ec2types.Reservation{
+					{}, {},
+				},
+				result: true,
+			},
+			// no parent instance
+			{
+				parent: []ec2types.Reservation{
+					{Instances: []ec2types.Instance{}},
+				},
+				result: false,
+			},
+			// many parent instances - "valid" to leave as is
+			{
+				parent: []ec2types.Reservation{
+					{Instances: []ec2types.Instance{{}, {}}},
+				},
+				result: true,
+			},
+			// pending parent
+			{
+				parent: []ec2types.Reservation{
+					{Instances: []ec2types.Instance{{
+						InstanceId: &testInstanceID,
+						State: &ec2types.InstanceState{
+							Name: ec2types.InstanceStateNamePending,
+						},
+					}}},
+				},
+				result: true,
+			},
+			// running parent
+			{
+				parent: []ec2types.Reservation{
+					{Instances: []ec2types.Instance{{
+						InstanceId: &testInstanceID,
+						State: &ec2types.InstanceState{
+							Name: ec2types.InstanceStateNameRunning,
+						},
+					}}},
+				},
+				result: true,
+			},
+			// terminated parent - not valid instance
+			{
+				parent: []ec2types.Reservation{
+					{Instances: []ec2types.Instance{{
+						InstanceId: &testInstanceID,
+						State: &ec2types.InstanceState{
+							Name: ec2types.InstanceStateNameTerminated,
+						},
+					}}},
+				},
+				result: false,
+			},
+		}
+	for _, tc := range tests {
+		require.Equal(t, tc.result, main.CheckValidParent("testChildId", tc.parent))
+	}
 }
