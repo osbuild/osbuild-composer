fbf1635e64b9ff09d90f3c0b2c3aa2aaca7dea3a
Brian C. Lane <bcl@redhat.com>
2025-01-16 11:36:04 -0800

    Fix non-constant log strings
    Newer versions of the go compiler (1.24 in this case) fail when running
    go test during a mock rebuild of the srpm created by 'make srpm' on
    Fedora 42.
    
    Even though we currently don't support go1.24, fix these so they don't
    become an issue when we do.

--- cmd/osbuild-worker-executor/handler_build.go
+++ cmd/osbuild-worker-executor/handler_build.go
@@ -94,7 +94,7 @@ func runOsbuild(logger *logrus.Logger, buildDir string, control *controlJSON, ou
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		err = fmt.Errorf("cannot tar output directory: %w, output:\n%s", err, out)
-		logger.Errorf(err.Error())
+		logger.Errorf("%v", err)
 		_, _ = mw.Write([]byte(err.Error()))
 		return "", err
 	}
