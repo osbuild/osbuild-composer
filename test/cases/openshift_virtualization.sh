6b49842a546d487fddc51d3a093cac4e1022acf1
Ondřej Budai <ondrej@budai.cz>
2025-03-13 23:21:29 +0100

    fix the URL

--- test/cases/openshift_virtualization.sh
+++ test/cases/openshift_virtualization.sh
@@ -167,7 +167,7 @@ chmod a+x "$VIRTCTL"
 
 # Authenticate via the gitlab-ci service account
 # oc describe secret gitab-ci-token-g7sw2
-$OC_CLI login --token="$OPENSHIFT_TOKEN" --server=https://api.ocp-virt.prod.psi.redhat.com:6443 --insecure-skip-tls-verify=true
+$OC_CLI login --token="$OPENSHIFT_TOKEN" --server=https://api.prod-scale-spoke1-aws-us-east-1.prod-scale-mgmthub1-aws-us-east-1.itup.redhat.com/ --insecure-skip-tls-verify=true
 $OC_CLI whoami
 
 OPENSHIFT_PROJECT="image-builder--runtime-int"
