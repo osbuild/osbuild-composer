2b902eb4bf39bf6867839f0e14406d3d796ab75d
Achilleas Koutsou <achilleas@koutsou.net>
2025-06-25 18:57:58 +0200

    Update and reduce testing to RHEL 8.10
    Reduce test configurations to only run on and for RHEL 8.10 and bump the
    terraform commit ID.
    Update all GitHub runners to Ubuntu 22.04 (from 20.04, deprecated).
    
    Install dnf4 in the Python Lint job for dnf-json since it runs on Fedora
    41 now.
    
    Update the Azure test cases to fix the instance definition and bring in
    any other test updates.

--- test/cases/azure.sh
+++ test/cases/azure.sh
@@ -24,9 +24,9 @@ CONTAINER_IMAGE_CLOUD_TOOLS="quay.io/osbuild/cloud-tools:latest"
 /usr/libexec/osbuild-composer-test/provision.sh none
 
 # Check available container runtime
-if which podman 2>/dev/null >&2; then
+if type -p podman 2>/dev/null >&2; then
     CONTAINER_RUNTIME=podman
-elif which docker 2>/dev/null >&2; then
+elif type -p docker 2>/dev/null >&2; then
     CONTAINER_RUNTIME=docker
 else
     echo No container runtime found, install podman or docker.
@@ -36,6 +36,8 @@ fi
 TEMPDIR=$(mktemp -d)
 function cleanup() {
     greenprint "== Script execution stopped or finished - Cleaning up =="
+    # kill dangling journalctl processes to prevent GitLab CI from hanging
+    sudo pkill journalctl || echo "Nothing killed"
     sudo rm -rf "$TEMPDIR"
 }
 trap cleanup EXIT
@@ -50,6 +52,7 @@ if ! hash az; then
     mkdir "${AZURE_CMD_CREDS_DIR}"
 
     AZURE_CMD="sudo ${CONTAINER_RUNTIME} run --rm \
+        --net=host \
         -v ${AZURE_CMD_CREDS_DIR}:/root/.azure:Z \
         -v ${TEMPDIR}:${TEMPDIR}:Z \
         ${CONTAINER_IMAGE_CLOUD_TOOLS} az"
@@ -156,8 +159,6 @@ sudo composer-cli blueprints depsolve bash
 WORKER_UNIT=$(sudo systemctl list-units | grep -o -E "osbuild.*worker.*\.service")
 sudo journalctl -af -n 1 -u "${WORKER_UNIT}" &
 WORKER_JOURNAL_PID=$!
-# Stop watching the worker journal when exiting.
-trap 'sudo pkill -P ${WORKER_JOURNAL_PID}' EXIT
 
 # Start the compose and upload to Azure.
 greenprint "🚀 Starting compose"
@@ -184,9 +185,8 @@ greenprint "💬 Getting compose log and metadata"
 get_compose_log "$COMPOSE_ID"
 get_compose_metadata "$COMPOSE_ID"
 
-# Kill the journal monitor immediately and remove the trap
+# Kill the journal monitor
 sudo pkill -P ${WORKER_JOURNAL_PID}
-trap - EXIT
 
 # Did the compose finish with success?
 if [[ $COMPOSE_STATUS != FINISHED ]]; then
@@ -220,9 +220,11 @@ tee "${TEMPDIR}/resource-file.json" <<EOF
   "instances": [
     {
       "vhd_uri": "${BLOB_URL}",
+      "arch": "${ARCH}",
       "location": "${AZURE_LOCATION}",
       "name": "${IMAGE_KEY}",
-      "hyper_v_generation": "${HYPER_V_GEN}"
+      "hyper_v_generation": "${HYPER_V_GEN}",
+      "storage_account": "${AZURE_STORAGE_ACCOUNT}"
     }
   ]
 }
@@ -239,6 +241,7 @@ cp "${CIV_CONFIG_FILE}" "${TEMPDIR}/civ_config.yml"
 # https://issues.redhat.com/browse/CLOUDX-488
 if nvrGreaterOrEqual "osbuild-composer" "83"; then
     sudo "${CONTAINER_RUNTIME}" run \
+        --net=host \
         -a stdout -a stderr \
         -e ARM_CLIENT_ID="${V2_AZURE_CLIENT_ID}" \
         -e ARM_CLIENT_SECRET="${V2_AZURE_CLIENT_SECRET}" \
