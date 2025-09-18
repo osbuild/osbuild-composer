9f475b4cb6c20f3abe519759c4a7001071ee986f
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2025-09-18 11:15:28 +0200

    templates/packer/executor: drop cloudwatch from vector config
    All logging should just be forwarded to the worker which created the
    executor.

--- templates/packer/ansible/roles/common/files/worker-initialization-scripts/worker_executor.sh
+++ templates/packer/ansible/roles/common/files/worker-initialization-scripts/worker_executor.sh
@@ -6,43 +6,25 @@ source /etc/os-release
 source /tmp/cloud_init_vars || true
 
 echo "Writing vector config."
-REGION=$(curl -Ls http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
-HOSTNAME=$(hostname)
-CLOUDWATCH_ENDPOINT="https://logs.$REGION.amazonaws.com"
-OSBUILD_EXECUTOR_CLOUDWATCH_GROUP=${OSBUILD_EXECUTOR_CLOUDWATCH_GROUP:-osbuild-executor-log-group}
 HOST_WORKER_ADDRESS=${HOST_WORKER_ADDRESS:-127.0.0.1}
 
-sudo mkdir -p /etc/vector
-sudo tee /etc/vector/vector.yaml > /dev/null << EOF
+if [ "$HOST_WORKER_ADDRESS" != "127.0.0.1" ]; then
+    sudo mkdir -p /etc/vector
+    sudo tee /etc/vector/vector.yaml > /dev/null << EOF
 sources:
   journald:
     type: journald
     exclude_units:
       - vector.service
 sinks:
-  cloudwatch:
-    type: aws_cloudwatch_logs
-    inputs:
-      - journald
-    region: ${REGION}
-    endpoint: ${CLOUDWATCH_ENDPOINT}
-    group_name: ${OSBUILD_EXECUTOR_CLOUDWATCH_GROUP}
-    stream_name: worker_syslog_{{ host }}
-    encoding:
-      codec: json
-EOF
-
-if [ "$HOST_WORKER_ADDRESS" != "127.0.0.1" ]; then
-    sudo tee -a /etc/vector/vector.yaml > /dev/null <<EOF
   host_worker:
     type: vector
     inputs:
       - journald
     address: ${HOST_WORKER_ADDRESS}:12005
 EOF
-fi
-
 sudo systemctl enable --now vector
+fi
 
 echo "Starting worker executor"
 /usr/libexec/osbuild-composer/osbuild-worker-executor -host 0.0.0.0
