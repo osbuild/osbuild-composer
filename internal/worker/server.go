45a220237225d824a2d5cad5041616b09a92c7a7
Achilleas Koutsou <achilleas@koutsou.net>
2025-10-14 13:46:06 +0200

    worker: job info function and handling for JobTypeImageBuilderManifest
    Add the JobInfo handler for the new job type and call it from the finish
    job and error functions.
    
    Also, match the job info type when reading manifests using the
    ManifestJobInfo() function.

--- internal/worker/server.go
+++ internal/worker/server.go
@@ -326,6 +326,13 @@ func (s *Server) JobDependencyChainErrors(id uuid.UUID) (*clienterrors.Error, er
 			return nil, err
 		}
 		jobResult = &ostreeResolveJR.JobResult
+	case JobTypeImageBuilderManifest:
+		var ibManifestJR ImageBuilderManifestJobResult
+		jobInfo, err = s.ImageBuilderManifestJobInfo(id, &ibManifestJR)
+		if err != nil {
+			return nil, err
+		}
+		jobResult = &ibManifestJR.JobResult
 
 	default:
 		return nil, fmt.Errorf("unexpected job type: %s", jobType)
@@ -501,11 +508,12 @@ func (s *Server) ManifestJobInfo(id uuid.UUID, result *ManifestJobByIDResult) (*
 		return nil, err
 	}
 
-	if jobInfo.JobType != JobTypeManifestIDOnly {
-		return nil, fmt.Errorf("expected %q, found %q job instead", JobTypeManifestIDOnly, jobInfo.JobType)
+	switch jobInfo.JobType {
+	case JobTypeManifestIDOnly, JobTypeImageBuilderManifest:
+		return jobInfo, nil
+	default:
+		return nil, fmt.Errorf("expected %q or %q, found %q job instead", JobTypeManifestIDOnly, JobTypeImageBuilderManifest, jobInfo.JobType)
 	}
-
-	return jobInfo, nil
 }
 
 func (s *Server) ContainerResolveJobInfo(id uuid.UUID, result *ContainerResolveJobResult) (*JobInfo, error) {
@@ -570,6 +578,19 @@ func (s *Server) AWSEC2ShareJobInfo(id uuid.UUID, result *AWSEC2ShareJobResult) 
 
 	if jobInfo.JobType != JobTypeAWSEC2Share {
 		return nil, fmt.Errorf("expected %q, found %q job instead", JobTypeAWSEC2Share, jobInfo.JobType)
+	}
+
+	return jobInfo, nil
+}
+
+func (s *Server) ImageBuilderManifestJobInfo(id uuid.UUID, result *ImageBuilderManifestJobResult) (*JobInfo, error) {
+	jobInfo, err := s.jobInfo(id, result)
+	if err != nil {
+		return nil, err
+	}
+
+	if jobInfo.JobType != JobTypeImageBuilderManifest {
+		return nil, fmt.Errorf("expected %q, found %q job instead", JobTypeImageBuilderManifest, jobInfo.JobType)
 	}
 
 	return jobInfo, nil
@@ -972,6 +993,13 @@ func (s *Server) RequeueOrFinishJob(token uuid.UUID, maxRetries uint64, result j
 			return err
 		}
 		jobResult = &ostreeResolveJR.JobResult
+	case JobTypeImageBuilderManifest:
+		var ibManifestJR ImageBuilderManifestJobResult
+		jobInfo, err = s.ImageBuilderManifestJobInfo(jobId, &ibManifestJR)
+		if err != nil {
+			return err
+		}
+		jobResult = &ibManifestJR.JobResult
 
 	default:
 		return fmt.Errorf("unexpected job type: %s", jobType)
