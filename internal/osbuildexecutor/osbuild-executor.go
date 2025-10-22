0bbb755000d8c7197f0d69956b46c4c7bfd43fb6
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2025-10-22 17:03:55 +0200

    internal/osbuildexecutor: forward progress to composer
    Uses status scanner to monitor osbuild to generate progress
    updates. Progress updates are sent every 30 seconds.
    
    Only one level of subprogress needs to be supported. Because osbuild
    manifests consist of pipelines and stages, more than 2 levels of
    progress are not expected.
    
    The result is still captured from stdout as before. The worker-executor
    will stream the progress information over http, and save the result in a
    separate file in the output archive.

--- internal/osbuildexecutor/osbuild-executor.go
+++ internal/osbuildexecutor/osbuild-executor.go
@@ -1,9 +1,18 @@
 package osbuildexecutor
 
 import (
+	"time"
+
+	"github.com/sirupsen/logrus"
+
 	"github.com/osbuild/images/pkg/osbuild"
+	"github.com/osbuild/osbuild-composer/internal/worker"
+)
+
+const (
+	MinTimeBetweenUpdates = time.Second * 30
 )
 
 type Executor interface {
-	RunOSBuild(manifest []byte, opts *osbuild.OSBuildOptions) (*osbuild.Result, error)
+	RunOSBuild(manifest []byte, logger logrus.FieldLogger, job worker.Job, opts *osbuild.OSBuildOptions) (*osbuild.Result, error)
 }
--- internal/osbuildexecutor/runner-impl-host.go
+++ internal/osbuildexecutor/runner-impl-host.go
@@ -1,13 +1,60 @@
 package osbuildexecutor
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/sirupsen/logrus"
+
 	"github.com/osbuild/images/pkg/osbuild"
+	"github.com/osbuild/osbuild-composer/internal/worker"
 )
 
 type hostExecutor struct{}
 
-func (he *hostExecutor) RunOSBuild(manifest []byte, opts *osbuild.OSBuildOptions) (*osbuild.Result, error) {
-	return osbuild.RunOSBuild(manifest, opts)
+func (he *hostExecutor) RunOSBuild(manifest []byte, logger logrus.FieldLogger, job worker.Job, opts *osbuild.OSBuildOptions) (*osbuild.Result, error) {
+	// MonitorFile needs an *os.File
+	rPipe, wPipe, err := os.Pipe()
+	if err != nil {
+		return nil, fmt.Errorf("cannot create pipe for monitor file: %w", err)
+	}
+	defer rPipe.Close()
+	defer wPipe.Close()
+	opts.Monitor = osbuild.MonitorJSONSeq
+	opts.MonitorFile = wPipe
+
+	// Capture stdout to generate osbuild result
+	var stdoutBuffer bytes.Buffer
+	opts.Stdout = &stdoutBuffer
+
+	cmd := osbuild.NewOSBuildCmd(manifest, opts)
+	osbuildStatus := osbuild.NewStatusScanner(rPipe)
+	if err := cmd.Start(); err != nil {
+		return nil, fmt.Errorf("error starting osbuild: %v", err)
+	}
+	wPipe.Close()
+
+	if err := handleProgress(osbuildStatus, logger, job); err != nil {
+		return nil, fmt.Errorf("unable to construct osbuild result: %w", err)
+	}
+
+	if err := cmd.Wait(); err != nil {
+		return nil, fmt.Errorf("error running osbuild: %w", err)
+	}
+
+	// try to decode the output even though the job could have failed
+	if stdoutBuffer.Len() == 0 {
+		return nil, fmt.Errorf("osbuild did not return any output")
+	}
+	var result osbuild.Result
+	err = json.Unmarshal(stdoutBuffer.Bytes(), &result)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding osbuild output: %w\nraw output:\n%s", err, stdoutBuffer.String())
+	}
+
+	return &result, nil
 }
 
 func NewHostExecutor() Executor {
