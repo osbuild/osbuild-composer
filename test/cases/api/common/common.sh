8ef7f3687223b7e1f81bd90952a65d0c9c276e6b
Achilleas Koutsou <achilleas@koutsou.net>
2025-12-01 17:43:19 +0100

    test/common: change expected RHSM fact value when using image-builder
    When using image-builder to generate the manifest, the value of the RHSM
    fact is "image-builder-cli".  We want to be able to keep the value
    consistent for all images built in the service, but for now the value
    isn't configurable, so let's modify the test until we can change it.
    
    See https://issues.redhat.com/browse/HMS-9819

--- test/cases/api/common/common.sh
+++ test/cases/api/common/common.sh
@@ -61,7 +61,15 @@ function _instanceCheck() {
     [[ "$subscribe_org_id" == "org ID: $API_TEST_SUBSCRIPTION_ORG_ID" ]]
 
     FACTS=$($_ssh sudo subscription-manager facts)
-    if ! grep -q "image-builder.osbuild-composer.api-type: cloudapi-v2" <<< "$FACTS"; then
+    # NOTE: workaround until fact value becomes configurable in image-builder-cli (https://issues.redhat.com/browse/HMS-9819)
+    expected_fact_value="cloudapi-v2"
+    IMAGE_BUILDER_EXPERIMENTAL="${IMAGE_BUILDER_EXPERIMENTAL:-}"
+    if [[ "${IMAGE_BUILDER_EXPERIMENTAL}" != "" ]]; then
+        if echo "${IMAGE_BUILDER_EXPERIMENTAL}" | grep -q "image-builder-manifest-generation=1"; then
+          expected_fact_value="image-builder-cli"
+        fi
+    fi
+    if ! grep -q "image-builder.osbuild-composer.api-type: ${expected_fact_value}" <<< "$FACTS"; then
         echo "System doesn't contain the expected image-builder.osbuild-composer facts"
         echo "$FACTS" | grep image-builder
         exit 1
