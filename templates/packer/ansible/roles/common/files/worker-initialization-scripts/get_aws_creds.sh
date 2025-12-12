3b999f8925e8cdb91609307fcfd15fdc2615ce50
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2025-12-12 13:54:26 +0100

    templates/packer: add support for separate S3 credentials

--- templates/packer/ansible/roles/common/files/worker-initialization-scripts/get_aws_creds.sh
+++ templates/packer/ansible/roles/common/files/worker-initialization-scripts/get_aws_creds.sh
@@ -15,24 +15,45 @@ bucket = "${WORKER_CONFIG_AWS_BUCKET:-}"
 EOF
 
 AWS_ACCOUNT_IMAGE_BUILDER_ARN=${AWS_ACCOUNT_IMAGE_BUILDER_ARN:-}
-if [[ -z "$AWS_ACCOUNT_IMAGE_BUILDER_ARN" ]]; then
-  echo "AWS_ACCOUNT_IMAGE_BUILDER_ARN not defined, skipping."
-  exit 0
-fi
+if [[ -n "$AWS_ACCOUNT_IMAGE_BUILDER_ARN" ]]; then
+  echo "AWS_ACCOUNT_IMAGE_BUILDER_ARN set, configuring aws credentials"
 
-/usr/local/bin/aws secretsmanager get-secret-value \
---endpoint-url "${SECRETS_MANAGER_ENDPOINT_URL}" \
---secret-id "${AWS_ACCOUNT_IMAGE_BUILDER_ARN}" | jq -r ".SecretString" > /tmp/aws_credentials.json
-ACCESS_KEY_ID=$(jq -r ".access_key_id" /tmp/aws_credentials.json)
-SECRET_ACCESS_KEY=$(jq -r ".secret_access_key" /tmp/aws_credentials.json)
-rm /tmp/aws_credentials.json
+  /usr/local/bin/aws secretsmanager get-secret-value \
+    --endpoint-url "${SECRETS_MANAGER_ENDPOINT_URL}" \
+    --secret-id "${AWS_ACCOUNT_IMAGE_BUILDER_ARN}" | jq -r ".SecretString" > /tmp/aws_credentials.json
+  ACCESS_KEY_ID=$(jq -r ".access_key_id" /tmp/aws_credentials.json)
+  SECRET_ACCESS_KEY=$(jq -r ".secret_access_key" /tmp/aws_credentials.json)
+  rm /tmp/aws_credentials.json
 
-sudo tee /etc/osbuild-worker/aws_credentials.toml > /dev/null << EOF
+  CREDS_FILE="/etc/osbuild-worker/aws_credentials.toml"
+  sudo tee "$CREDS_FILE" > /dev/null << EOF
 [default]
 aws_access_key_id = "$ACCESS_KEY_ID"
 aws_secret_access_key = "$SECRET_ACCESS_KEY"
 EOF
-
-sudo tee -a /etc/osbuild-worker/osbuild-worker.toml > /dev/null << EOF
-credentials = "${WORKER_CONFIG_AWS_CREDENTIALS:-}"
+  sudo tee -a /etc/osbuild-worker/osbuild-worker.toml > /dev/null << EOF
+credentials = "$CREDS_FILE"
 EOF
+fi
+
+AWS_S3_ACCOUNT_IMAGE_BUILDER_ARN=${AWS_S3_ACCOUNT_IMAGE_BUILDER_ARN:-}
+if [[ -n "$AWS_S3_ACCOUNT_IMAGE_BUILDER_ARN" ]]; then
+  echo "AWS_S3_ACCOUNT_IMAGE_BUILDER_ARN set, configuring aws credentials"
+
+  /usr/local/bin/aws secretsmanager get-secret-value \
+    --endpoint-url "${SECRETS_MANAGER_ENDPOINT_URL}" \
+    --secret-id "${AWS_S3_ACCOUNT_IMAGE_BUILDER_ARN}" | jq -r ".SecretString" > /tmp/aws_credentials.json
+  ACCESS_KEY_ID=$(jq -r ".access_key_id" /tmp/aws_credentials.json)
+  SECRET_ACCESS_KEY=$(jq -r ".secret_access_key" /tmp/aws_credentials.json)
+  rm /tmp/aws_credentials.json
+
+  CREDS_FILE="/etc/osbuild-worker/aws_s3_credentials.toml"
+  sudo tee "$CREDS_FILE" > /dev/null << EOF
+[default]
+aws_access_key_id = "$ACCESS_KEY_ID"
+aws_secret_access_key = "$SECRET_ACCESS_KEY"
+EOF
+  sudo tee -a /etc/osbuild-worker/osbuild-worker.toml > /dev/null << EOF
+s3_credentials = "$CREDS_FILE"
+EOF
+fi
